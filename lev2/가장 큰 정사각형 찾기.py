5b24b530a31de985c957e040f8076e48108482fc
daewoong lee <dw.lee832@gmail.com>
2020-11-12 07:12:33 +0900

    가장 큰 정사각형 찾기 문제풀이 02

--- lev2/가장 큰 정사각형 찾기.py
+++ lev2/가장 큰 정사각형 찾기.py
@@ -70,20 +70,51 @@ def solution(board):
 print(solution([[0, 1, 1, 1],
                 [1, 1, 0, 1],
                 [1, 1, 1, 1]]))
-print(solution([[0, 0, 0, 0],
-                [0, 0, 0, 0],
-                [1, 1, 1, 1]]))
+# print(solution([[0, 0, 0, 0],
+#                 [0, 0, 0, 0],
+#                 [1, 1, 1, 1]]))
 
 
-import timeit
-avg_time = 0.
-tests = [[[0, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [0, 0, 1, 0]],
-         [[0, 0, 1, 1], [1, 1, 1, 1]],
-         [[0, 1, 1, 1], [1, 1, 0, 1], [1, 1, 1, 1]],
-         [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1]]]
-for t in tests:
-    avg_time += timeit.timeit(lambda: solution(t), number=10000)
-print(f'avg_time: {avg_time / len(tests)}')
+# import timeit
+# avg_time = 0.
+# tests = [[[0, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [0, 0, 1, 0]],
+#          [[0, 0, 1, 1], [1, 1, 1, 1]],
+#          [[0, 1, 1, 1], [1, 1, 0, 1], [1, 1, 1, 1]],
+#          [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1]]]
+# for t in tests:
+#     avg_time += timeit.timeit(lambda: solution(t), number=10000)
+# print(f'avg_time: {avg_time / len(tests)}')
 
 
+board = [[0, 1, 1, 1],
+         [1, 1, 0, 1],
+         [1, 1, 1, 1]]
 
+answers = []
+matching = [[0] * len(board[0])]*len(board)
+print(matching)
+for i, row in enumerate(board):
+    print(f'row: {row}')
+    another_rows = board[i+1:]
+    print(f'another rows: {another_rows}')
+    for a_row in another_rows:
+        matching_row = 1
+        for r, ar in zip(row, a_row):
+            print(f'r: {r}, ar: {ar}')
+            if r == ar:
+                matching_row += 1
+            else:
+                break
+            print(f'matching_row: {matching_row}')
+        if matching_row == 0:
+            break
+        elif matching_row == 1:
+            answer = 1
+        # else:
+        #     answer = diff * cnt if diff == cnt else diff-1 * cnt if diff > cnt else diff * cnt-1
+        answers.append(answer)
+        # print(f'z row: {z_row}')
+        # if all([j_r in z_row for j_r in j_row]):
+        #     cnt += 1
+        #     print(f'cnt: {cnt}')
+    print()
