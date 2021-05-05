725d07ead044a3df222a8d6dd914e550c52da955
daewoong lee <dw.lee832@gmail.com>
2021-05-05 16:36:43 +0900

    정수 삼각형 문제풀이 01 - code refactoring

--- lev3/정수 삼각형.py
+++ lev3/정수 삼각형.py
@@ -20,20 +20,56 @@ triangle_temp	                                                result
 
 from copy import deepcopy
 def solution(triangle):
-    triangle_temp = deepcopy(triangle)
-    max_sum = triangle[0][0]
-    for i in range(1, len(triangle)):
-        for j in range(i):
-            if triangle[i][j] < triangle_temp[i][j] + triangle[i-1][j]:
-                triangle[i][j] = triangle_temp[i][j] + triangle[i-1][j]
-            if triangle[i][j+1] < triangle_temp[i][j+1] + triangle[i-1][j]:
-                triangle[i][j+1] = triangle_temp[i][j+1] + triangle[i-1][j]
-            if max_sum < triangle[i][j]:
-                max_sum = triangle[i][j]
-            if max_sum < triangle[i][j+1]:
-                max_sum = triangle[i][j+1]
-    return max_sum
+    # 0.298445245
+    # triangle_temp = deepcopy(triangle)
+    # max_sum = triangle[0][0]
+    # for i in range(1, len(triangle)):
+    #     for j in range(i):
+    #         if triangle[i][j] < triangle_temp[i][j] + triangle[i-1][j]:
+    #             triangle[i][j] = triangle_temp[i][j] + triangle[i-1][j]
+    #         if triangle[i][j+1] < triangle_temp[i][j+1] + triangle[i-1][j]:
+    #             triangle[i][j+1] = triangle_temp[i][j+1] + triangle[i-1][j]
+    #         if max_sum < triangle[i][j]:
+    #             max_sum = triangle[i][j]
+    #         if max_sum < triangle[i][j+1]:
+    #             max_sum = triangle[i][j+1]
+    # return max_sum
+
+    # code refactoring 01 - 0.260138399
+    # triangle_temp = deepcopy(triangle)
+    # for i in range(1, len(triangle)):
+    #     for j in range(i):
+    #         if triangle[i][j] < triangle_temp[i][j] + triangle[i-1][j]:
+    #             triangle[i][j] = triangle_temp[i][j] + triangle[i-1][j]
+    #         if triangle[i][j+1] < triangle_temp[i][j+1] + triangle[i-1][j]:
+    #             triangle[i][j+1] = triangle_temp[i][j+1] + triangle[i-1][j]
+    # return max(triangle[-1])
+
+    # code refactoring 02 - 0.075924312
+    # for t in range(1, len(triangle)):
+    #     for i in range(t+1):
+    #         if i == 0:
+    #             triangle[t][0] += triangle[t-1][0]
+    #         elif i == t:
+    #             triangle[t][-1] += triangle[t-1][-1]
+    #         else:
+    #             triangle[t][i] += max(triangle[t-1][i-1], triangle[t-1][i])
+    # return max(triangle[-1])
+
+    # code refactoring 03 - 0.086990964
+    l = []
+    for r in triangle:
+        l = [max(t, y) + z for t, y, z in zip([0]+l, l+[0], r)]
+    return max(l)
 
 
-# print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
-print(solution([[0]]))
+print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
+# print(solution([[0]]))
+
+
+if __name__ == '__main__':
+    from timeit import Timer
+    query = [[[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]],
+             [[0]]]
+    t = Timer(f"for t in {query}: solution(t)", "from __main__ import solution")
+    print(t.timeit(number=10000))
