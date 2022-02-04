ed7d1dc56575f6851afc883f74698cbbb8ae1e5f
daewoonglee <dw.lee832@gmail.com>
2022-02-04 10:15:49 +0900

    부족한 금액 계산하기 문제풀이 01 - code refactoring

--- lev1/부족한 금액 계산하기.py
+++ lev1/부족한 금액 계산하기.py
@@ -1,10 +1,25 @@
 def solution(price, money, count):
-    total_price = 0
-    for c in range(count):
-        total_price += (price * (c+1))
-    return total_price - money if total_price > money else 0
+    # 0.025914032012224197
+    #total_price = 0
+    #for c in range(count):
+    #    total_price += (price * (c+1))
+    #return total_price - money if total_price > money else 0
+
+    # code refactoring - 0.007152557373046875
+    #total_price = count * (2*price + (count-1)*price)/2
+    #return total_price - money if total_price > money else 0 
+
+    # code refactoring 01 - 0.008575525134801865 (callback function)
+    return max(0, count*(2*price+(count-1)*price)/2)
 
 
 print(solution(3, 20, 4))
 print(solution(1, 200, 30))
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [[3, 20, 4], [1, 200, 30]]
+    t = Timer(f"for t in {query}: solution(*t)", "from __main__ import solution")
+    print(t.timeit(number=10000))
 
