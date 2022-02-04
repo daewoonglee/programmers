5c1476b39a6511d589f2c9eec139bdd1d85bacd5
daewoonglee <dw.lee832@gmail.com>
2022-02-04 15:12:33 +0900

    나머지가 1이 되는 수 찾기 문제풀이 01 - code refactoring

--- lev1/나머지가 1이 되는 수 찾기.py
+++ lev1/나머지가 1이 되는 수 찾기.py
@@ -1,11 +1,26 @@
 def solution(n):
-    for i in range(2, n):
-        if n % i == 1: return i
-    return n
+    # 0.832480126991868
+    #for i in range(2, n):
+    #    if n % i == 1: return i
+    #return n
+
+    # code refactoring - 0.04768524132668972
+    ans = n-1
+    for i in range(2, int(n**0.5)+1):
+        if ans % i == 0: return i
+    return ans
 
 
 print(solution(10))
 print(solution(12))
 print(solution(1001))
-print(solution(100001)) 
+print(solution(1598))
+print(solution(100002)) 
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [10, 12, 1001, 1598, 100002]
+    t = Timer(f"for t in {query}: solution(t)", "from __main__ import solution")
+    print(t.timeit(number=10000))
 
