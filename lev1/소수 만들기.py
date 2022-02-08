ee67a26115e958d06b8aa6f9bbdaa861191d6033
daewoonglee <dw.lee832@gmail.com>
2022-02-08 15:07:09 +0900

    소수 만들기 문제풀이 01 - code refactoring

--- lev1/소수 만들기.py
+++ lev1/소수 만들기.py
@@ -1,14 +1,26 @@
 from itertools import combinations
 def solution(nums):
+    # 1.3407424110919237
+    #ans = 0
+    #for c in combinations(nums, 3):
+    #    total = sum(c)
+    #    flag = False
+    #    for n in range(2, int(total**0.5)+1):
+    #        if total % n == 0:
+    #            flag = True
+    #            break
+    #    if not flag:
+    #        ans += 1
+    #return ans
+
+    # code refactoring - 1.3150121178478003
     ans = 0
     for c in combinations(nums, 3):
         total = sum(c)
-        flag = False
         for n in range(2, int(total**0.5)+1):
             if total % n == 0:
-                flag = True
                 break
-        if not flag:
+        else:
             ans += 1
     return ans
 
@@ -16,4 +28,11 @@ def solution(nums):
 print(solution([1,2,3,4]))
 print(solution([1,2,7,6,4]))
 print(solution([i for i in range(1, 51)]))
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [[1,2,3,4], [1,2,7,6,4], [i for i in range(1,51)]]
+    t = Timer(f"for t in {query}: solution(t)", "from __main__ import solution")
+    print(t.timeit(number=100))
 
