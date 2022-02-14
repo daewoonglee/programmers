13d3f97886e5adf5ba60f0f91088641ea71a0ccc
daewoonglee <dw.lee832@gmail.com>
2022-02-14 20:13:33 +0900

    없는 숫자 더하기 문제풀이 01 - code refactoring

--- lev1/없는 숫자 더하기.py
+++ lev1/없는 숫자 더하기.py
@@ -1,9 +1,13 @@
 def solution(numbers):
-    ans = 0
-    for n in range(10):
-        if n not in numbers:
-            ans += n
-    return ans
+    # 0.074751241132617
+    #ans = 0
+    #for n in range(10):
+    #    if n not in numbers:
+    #        ans += n
+    #return ans
+
+    # code refactoring - 0.02044590748846531
+    return 45 - sum(numbers)
 
 
 print(solution([1,2,3,4,6,7,8,0]))
@@ -11,4 +15,11 @@ print(solution([5,8,4,0,6,7,9]))
 print(solution([0]))
 print(solution([0,1,2,3,4,5,6,7,8]))
 print(solution([1,2,3,4,5,6,7,8,9]))
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [[1,2,3,4,6,7,8,0], [5,8,4,0,6,7,9], [0], [0,1,2,3,4,5,6,7,8], [1,2,3,4,5,6,7,8,9], [1], [2], [3]]
+    t = Timer(f"for t in {query}: solution(t)", "from __main__ import solution")
+    print(t.timeit(number=10000))
 
