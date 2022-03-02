0912d730fc6aad0a21df63a983f23d051626317c
daewoonglee <dw.lee832@gmail.com>
2022-03-02 13:13:54 +0900

    전력망을 둘로 나누기 문제풀이 04 - 풀이O (테스트 13/13)

--- lev2/전력망을 둘로 나누기.py
+++ lev2/전력망을 둘로 나누기.py
@@ -9,20 +9,14 @@ def solution(n, wires):
         ad_mat[wire[0]].append(wire[1])
         ad_mat[wire[1]].append(wire[0])
     
-    max_k, max_v = wires[0][0], len(ad_mat[wires[0][0]])
-    for k, v in ad_mat.items():
-        if len(v) > max_v:
-            max_k = k
-            max_v = len(v)
-    
     visited_idx = {k: i for i, k in enumerate(set(sum(wires, [])))}
     ans = n
-    for ad_v in ad_mat[max_k]:
+    for wire in wires:
         visited = [0 for _ in visited_idx]
-        visited[visited_idx[max_k]] = 1
+        visited[visited_idx[wire[1]]] = 1
         
         cnt = 0
-        bfs = deque([ad_v])    
+        bfs = deque([wire[0]])    
         while bfs:
             node = bfs.popleft()
             visited[visited_idx[node]] = 1
@@ -37,9 +31,10 @@ def solution(n, wires):
 
 #print(solution(9, [[1,3],[2,3],[3,4],[4,5],[4,6],[4,7],[7,8],[7,9]]))
 #print(solution(4, [[1,2],[2,3],[3,4]]))
-print(solution(7, [[1,2],[1,7],[2,3],[2,6],[3,4],[3,5]])) 
+#print(solution(7, [[1,2],[1,7],[2,3],[2,6],[3,4],[3,5]])) 
 #print(solution(3, [[1,10],[10,100]]))
 
 #print(solution(11, [[1,2],[1,5],[2,3],[3,4],[5,6],[6,7],[7,8],[8,9],[8,10],[8,11]]))
 #print(solution(8, [[1,2],[1,7],[2,3],[2,6],[3,4],[3,5],[5,6]]))
+print(solution(9, [[1,2],[2,3],[2,4],[4,5],[5,6],[6,7],[7,8],[7,9]]))
 
