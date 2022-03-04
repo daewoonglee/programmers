61c19199818afabe4ef7103212d0eee39d964b2d
daewoonglee <dw.lee832@gmail.com>
2022-03-04 22:45:15 +0900

    주차 요금 계산 문제풀이 02 - 풀이O (테스트 16/16)

--- lev2/주차 요금 계산.py
+++ lev2/주차 요금 계산.py
@@ -3,40 +3,37 @@ import math
 
 
 def solution(fees, records):
-    ans = {}
+    parking_fee = {}
     stack = {}
-    fin = timedelta(minutes=59, hours=23)
+    OUT = timedelta(minutes=59, hours=23)
     for r in records:
         rt, rn, rio = r.split()
-        if rn not in stack:
-            stack[rn] = list()
         if rio == "IN":
+            if rn not in stack:
+                stack[rn] = list()
+                parking_fee[rn] = 0
             stack[rn].append(rt)
         else:
             in_h, in_m = stack[rn].pop().split(":")
             in_t = timedelta(hours=int(in_h), minutes=int(in_m))
             out_h, out_m = rt.split(":")
             out_t = timedelta(hours=int(out_h), minutes=int(out_m))
-            if rn not in ans:
-                ans[rn] = 0
-            ans[rn] += (out_t-in_t).total_seconds()
+            parking_fee[rn] += (out_t-in_t).total_seconds()
     for k, v in stack.items():
         if v:
-            if k not in ans:
-                ans[k] = 0
             h, m = v[0].split(":")
-            ans[k] += (fin-timedelta(hours=int(h), minutes=int(m))).total_seconds()
+            parking_fee[k] += (OUT-timedelta(hours=int(h), minutes=int(m))).total_seconds()
     
-    answer = []
-    basic_t, basic_c, unit_t, unit_c = fees
-    basic_t *= 60
-    unit_t *= 60
-    for k in sorted(ans, key=lambda x: int(x[0])):
-        if ans[k] <= basic_t:
-            answer.append(basic_c)
+    ans = []
+    bt, bf, ut, uf = fees
+    bt *= 60
+    ut *= 60
+    for k, v in sorted(parking_fee.items(), key=lambda x: int(x[0])):
+        if v <= bt:
+            ans.append(bf)
         else:
-            answer.append(basic_c + (math.ceil((ans[k]-basic_t)/unit_t) * unit_c))
-    return answer
+            ans.append(bf + (math.ceil((parking_fee[k]-bt)/ut) * uf))
+    return ans
             
 
 print(solution([180, 5000, 10, 600], ["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]))
