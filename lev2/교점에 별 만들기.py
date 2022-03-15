b6f997fc89e9a2ac432acd9ce28cddef9c58af01
daewoonglee <dw.lee832@gmail.com>
2022-03-15 21:11:54 +0900

    교점에 별 만들기 문제풀이 01 - 풀이X (테스트 0/29)

---
+++ lev2/교점에 별 만들기.py
@@ -0,0 +1,32 @@
+def solution(line):
+    pts = []
+    for i in range(len(line)):
+        for r in line[i:]:
+            a, b, e = line[i]
+            c, d, f = r
+            denominator = (a*d-b*c)
+            if not denominator: continue
+            x = (b*f - e*d) / denominator
+            y = (e*c - a*f) / denominator
+            if x.is_integer() and y.is_integer():
+                pts.append([int(x), int(y)])
+    
+    max_x = max([p[0] for p in pts])
+    max_y = max([p[1] for p in pts])
+
+    w = max_x - min([p[0] for p in pts]) + 1
+    h = max_y - min([p[1] for p in pts]) + 1
+    ans = [["."] * w for _ in range(h)]
+
+    for p in pts:
+        x = p[0] + max_x if p[0] < 0 else max_x + p[0]
+        y = abs(p[1]) + max_y if p[1] < 0 else max_y - p[1]
+        ans[y][x] = "*"
+
+    return ["".join(a) for a in ans]
+
+
+print(solution([[2, -1, 4], [-2, -1, 4], [0, -1, 1], [5, -8, -12], [5, 8, 12]]))
+print(solution([[0, 1, -1], [1, 0, -1], [1, 0, 1]]))
+print(solution([[1, -1, 0], [2, -1, 0]]))
+print(solution([[1, -1, 0], [2, -1, 0], [4, -1, 0]]))
