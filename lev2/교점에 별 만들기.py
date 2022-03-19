fe8563068e4616ff8259139e128f89688f162a32
daewoonglee <dw.lee832@gmail.com>
2022-03-19 23:39:38 +0900

    교점에 별 만들기 문제풀이 03 - 풀이O (테스트 29/29)

--- lev2/교점에 별 만들기.py
+++ lev2/교점에 별 만들기.py
@@ -10,39 +10,24 @@ def solution(line):
             y = (e*c - a*f) / denominator
             if x.is_integer() and y.is_integer():
                 pts.append([int(x), int(y)])
-
-    print(f"pts: {pts}")
-    
-    max_x = max([p[0] for p in pts])
+   
+    min_x = min([p[0] for p in pts]) 
     max_y = max([p[1] for p in pts])
 
-    w = max_x - min([p[0] for p in pts]) + 1
+    w = max([p[0] for p in pts]) - min_x + 1
     h = max_y - min([p[1] for p in pts]) + 1
     ans = [["."] * w for _ in range(h)]
 
-    # [[-2, 2], [-1, 1], [1, -1]]
-    # maxx: 1, maxy: 2, w: 4, h: 4
-    print(f"maxx: {max_x}, maxy: {max_y}, w: {w}, h: {h}")
-
-#maxx: 1, maxy: 1, w: 3, h: 1
-#p: [1, 1], x: 2, y:0 -> (2,0)
-#p: [-1, 1], x: 1, y:0 -> (0,0)
-
     for p in pts:
-        x = abs(p[0]) + max_x if p[0] < 0 else max_x - p[0]
-        #x = p[0] + max_x if p[0] < 0 else max_x + p[0]
-        y = abs(p[1]) + max_y if p[1] < 0 else max_y - p[1]
-        print(f"p: {p}, x: {x}, y:{y}")
+        x = p[0] - min_x
+        y = max_y - p[1]
         ans[y][x] = "*"
-
-    for r in ["".join(a) for a in ans]:
-        print(r)
 
     return ["".join(a) for a in ans]
 
 
 #print(solution([[2, -1, 4], [-2, -1, 4], [0, -1, 1], [5, -8, -12], [5, 8, 12]]))
-print(solution([[0, 1, -1], [1, 0, -1], [1, 0, 1]]))
+#print(solution([[0, 1, -1], [1, 0, -1], [1, 0, 1]]))
 #print(solution([[1, -1, 0], [2, -1, 0]]))
 #print(solution([[1, -1, 0], [2, -1, 0], [4, -1, 0]]))
 #print(solution([[1,0,-1], [0,1,-1]]))
