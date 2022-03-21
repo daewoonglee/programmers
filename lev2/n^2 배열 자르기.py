36d8f21310d229ab3971df9a068d9945ab50c14d
daewoonglee <dw.lee832@gmail.com>
2022-03-21 12:56:22 +0900

    n^2 배열 자르기 문제풀이 02 - 풀이X (테스트 11/20)

--- lev2/n^2 배열 자르기.py
+++ lev2/n^2 배열 자르기.py
@@ -1,14 +1,20 @@
 def solution(n, left, right):
-    arr = [[0]*n for _ in range(n)]
-    num = 1
+    # 0.48373288102447987
+    arr = []
+    q = right // n + 1
     for i in range(n):
-        for j in range(i+1):
-            arr[i][j] = num
-            arr[j][i] = num
-        num += 1
-    return sum(arr, [])[left: right+1]
+        arr.extend([i+1]*i + [j+1 for j in range(i,n)])
+        if i >= q: break
+    return arr[left: right+1]
 
 
 print(solution(3,2,5))
 print(solution(4,7,14))
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [[4,7,14],[40,70,140],[400,4,14]]
+    t = Timer(f"for t in {query}: solution(*t)", "from __main__ import solution")
+    print(t.timeit(number=10000))
 
