a3146c197189e6060910424f94e9a161a0fe5582
daewoonglee <dw.lee832@gmail.com>
2022-03-24 23:18:53 +0900

    거리두기 확인하기 문제풀이 01 - 풀이X (테스트 29/30)

---
+++ lev2/거리두기 확인하기.py
@@ -0,0 +1,37 @@
+from itertools import combinations
+
+
+def solution(places):
+    ans = []
+    fail = ["O", "P"]
+    for place in places:
+        pts = []
+        for i, p_line in enumerate(place):
+            for j, p in enumerate(p_line):
+                if p == "P": pts.append([i, j])
+
+        dist = []
+        for c in (list(combinations(pts, 2))):
+            if abs(c[0][0]-c[1][0]) + abs(c[0][1]-c[1][1]) <= 2:
+                dist.append(c)
+
+        for d in dist:
+            (x1, y1), (x2, y2) = d
+            if x1 == x2:
+                if abs(y2-y1) < 2 or (abs(y2-y1) == 2 and place[x1][y1+1] in fail):
+                    ans.append(0)
+                    break
+            elif x1 < x2:
+                if abs(x2-x1) < 2 and (abs(y2-y1) == 1 and place[x1][y2] in fail or place[x2][y1] in fail):
+                    ans.append(0)
+                    break
+        else: ans.append(1)
+    return ans
+
+
+# print(solution([["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]))
+
+# print(solution([["XXXXX", "XXXXX", "XXXXP", "XXXXP", "XXXXP"]])) # 0
+# print(solution([["XXXXX", "XXXXX", "XXPPO", "XXXXX", "XXXXX"]])) # 0
+print(solution([["OOPOO", "OOOPO", "OOOOP", "OOOOO", "OOOOO"]])) # 0
+
