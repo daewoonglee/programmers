b4d64787c2546d13ca3d9b4781ee3e9849cdc682
daewoonglee <dw.lee832@gmail.com>
2022-03-29 23:08:05 +0900

    양궁대회 문제풀이 02 - 풀이X (RecursionError 해결 필요)

--- lev2/양궁대회.py
+++ lev2/양궁대회.py
@@ -1,29 +1,28 @@
+global_score = 0
+global_ans = []
+
+
 def solution(n, info):
+    def shoot(stack):
+        global global_score, global_ans
+        while stack:
+            arrows, idx, score, li = stack.pop()
+            if arrows - (info[idx]+1) >= 0:
+                if idx == len(li)-1: continue
+                li[idx] = info[idx]+1
+                stack.append([arrows-info[idx]-1, idx+1, score+scores[idx], li[:]])
+                li[idx] = 0
+                stack.append([arrows, idx+1, score, li[:]])
+            else:
+                if score > global_score:
+                    global_score = score
+                    global_ans = li[:]
+        shoot(stack)
+
     scores = [i for i in range(10, -1, -1)]
-    appeach_score = sum([scores[i] for i in info if i])
-    global_score = 0
-    global_ans = [0] * 10
-    print(global_ans)
-    for i in range(10):
-        arrows = n - info[i] - 1
-        if arrows < 0: break
-        challenger = appeach_score - scores[i]
-        local_score = scores[i]
-        local_ans = [0] * 10
-        local_ans[i] = info[i]+1
-        for j in range(i+1, 10): # 재귀처리 필요
-            arrows -= (info[j]+1)
-            if arrows < 0: break
-            challenger -= scores[j]
-            local_score += scores[j]
-            local_ans[j] = info[j]+1
-        if local_score > global_score and local_score > challenger:
-            global_score = local_score
-            global_ans = local_ans[:]
-        return global_score, global_ans
-
-    return global_ans if global_ans else [-1]
+    shoot([[n, 0, 0, [0]*10]])
+    return global_ans if sum([scores[i] for i, s in enumerate(info) if s]) - global_ans *2 < 0 else [-1]
 
 
-print(solution(5, [2,1,1,1,0,0,0,0,0,0,0]))
+print(solution(5, [2,1,1,1,0,0,0,0,0,0,0])) # [0,2,2,0,1,0,0,0,0,0,0]
 
