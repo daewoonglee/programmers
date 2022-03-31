e4fb5e6986de8ea2318746ee90460977c4818cfa
daewoonglee <dw.lee832@gmail.com>
2022-03-31 21:38:10 +0900

    양궁대회 문제풀이 03 - 풀이X (테스트 23/25)

--- lev2/양궁대회.py
+++ lev2/양궁대회.py
@@ -1,28 +1,33 @@
-global_score = 0
+global_diff = 0
 global_ans = []
 
 
 def solution(n, info):
     def shoot(stack):
-        global global_score, global_ans
+        global global_diff, global_ans
         while stack:
-            arrows, idx, score, li = stack.pop()
-            if arrows - (info[idx]+1) >= 0:
-                if idx == len(li)-1: continue
-                li[idx] = info[idx]+1
-                stack.append([arrows-info[idx]-1, idx+1, score+scores[idx], li[:]])
-                li[idx] = 0
-                stack.append([arrows, idx+1, score, li[:]])
-            else:
-                if score > global_score:
-                    global_score = score
+            arrows, idx, lion_score, appeach_score, li = stack.pop()
+            if arrows == 0:
+                if lion_score > appeach_score and (lion_score - appeach_score) > global_diff:
+                    global_diff = (lion_score - appeach_score)
                     global_ans = li[:]
-        shoot(stack)
+            else:
+                if arrows < 0 or (arrows == n and len(li)-idx < arrows): continue
+                if idx == len(li)-1:
+                    li[-1] = arrows
+                    stack.append([0, len(li)-1, lion_score, appeach_score, li])
+                else:
+                    li[idx] = info[idx]+1
+                    stack.append([arrows-info[idx]-1, idx+1, lion_score+scores[idx], appeach_score-scores[idx] if info[idx] else appeach_score, li[:]])
+                    li[idx] = 0
+                    stack.append([arrows, idx+1, lion_score, appeach_score, li[:]])
 
+    global global_ans
     scores = [i for i in range(10, -1, -1)]
-    shoot([[n, 0, 0, [0]*10]])
-    return global_ans if sum([scores[i] for i, s in enumerate(info) if s]) - global_ans *2 < 0 else [-1]
+    shoot([[n, 0, 0, sum([scores[i] for i, s in enumerate(info) if s]), [0]*11]])
+    return global_ans if global_ans else [-1]
 
 
-print(solution(5, [2,1,1,1,0,0,0,0,0,0,0])) # [0,2,2,0,1,0,0,0,0,0,0]
+# print(solution(5, [2,1,1,1,0,0,0,0,0,0,0])) # [0,2,2,0,1,0,0,0,0,0,0]
+print(solution(10, [0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 3])) # [1,1,1,1,1,1,1,1,0,0,2]
 
