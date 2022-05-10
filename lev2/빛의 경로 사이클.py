f6c5cd6c4f7dc88d31c77c1026b55efe59e58d2f
daewoong lee <dw.lee832@gmail.com>
2022-05-10 18:31:16 +0900

    빛의 경로 사이클 문제풀이 02 - 풀이방법 참고

--- lev2/빛의 경로 사이클.py
+++ lev2/빛의 경로 사이클.py
@@ -1,68 +1,29 @@
 def solution(grid):
-    def go_to_right(x, y):
-        y = 0 if y == w else y+1
-        return 0, x, y
+    direction = [[-1, 0], [1, 0], [0, 1], [0, -1]] # LRDU
+    left = {0: 2, 1: 3, 2: 1, 3: 0}
+    right = {0: 3, 1: 2, 2: 0, 3: 1}
+    w, h = len(grid[0]), len(grid)
+    visited = [[[0]*4 for _ in g] for g in grid]
+    ans = []
+    for y in range(h):
+        for x in range(w):
+            for d in range(4): # LRDU
+                # print(f"x: {x}, y: {y}, d: {d}")
+                if visited[y][x][d]: continue
+                ty, tx, td = y, x, d
+                length = 0
+                while 1:
+                    length += 1
+                    visited[ty][tx][td] = 1
+                    if grid[ty][tx] == "L": td = left[td]
+                    elif grid[ty][tx] == "R": td = right[td]
+                    ty, tx = (ty+direction[td][0]) % h, (tx+direction[td][1]) % w
+                    # print(f"tx: {tx}, ty: {ty}, td: {td}")
+                    if tx == x and ty == y and td == d: break
+                ans.append(length)
+    return sorted(ans)
 
-    def go_to_left(x, y):
-        y = w if y == 0 else y-1
-        return 1, x, y
-
-    def go_to_up(x, y):
-        x = h if x == 0 else x-1
-        return 2, x, y
-
-    def go_to_down(x, y):
-        x = 0 if x == h else x+1
-        return 3, x, y
-
-    w, h = len(grid[0])-1, len(grid)-1
-    print(f"w: {w}, h:{h}")
-    info = [[[0]*4 for _ in g] for g in grid]
-    global_cycle = []
-    for d in range(4): # LRDU
-        cycle = []
-        i = j = 0
-        while 1:
-            if grid[i][j] == "S":
-                if d == 0: d, i, j = go_to_right(i, j)
-                elif d == 1: d, i, j = go_to_left(i, j)
-                elif d == 2: d, i, j = go_to_up(i, j)
-                else: d, i, j = go_to_down(i, j)
-            elif grid[i][j] == "L":
-                if d == 0: d, i, j = go_to_up(i, j)
-                elif d == 1: d, i, j = go_to_down(i, j)
-                elif d == 2: d, i, j = go_to_left(i, j)
-                else: d, i, j = go_to_right(i, j)
-            else:
-                if d == 0: d, i, j = go_to_down(i, j)
-                elif d == 1: d, i, j = go_to_up(i, j)
-                elif d == 2: d, i, j = go_to_right(i, j)
-                else: d, i, j = go_to_left(i, j)
-            # print(f"info[i][j]: {info[i][j]}")
-            if info[i][j][d]: break
-            info[i][j][d] = 1
-            print(f"info: {info}, d: {d}, grid: {grid[i][j]}, i: {i}, j: {j}")
-            cycle.append(grid[i][j])
-        print(f"cycle: {cycle}, info: {info}, global: {global_cycle}")
-        if not cycle: continue
-        elif global_cycle and len(cycle) > 1 and len(global_cycle[-1]) == len(cycle):
-            # is same cycle
-            N = len(cycle)
-            for i in range(N):
-                cnt = 0
-                idx = i
-                while cnt < N:
-                    if global_cycle[-1][idx%N] != cycle[idx%N]:
-                        global_cycle.append(cycle[:])
-                    idx += 1
-                    cnt += 1
-        else:
-            global_cycle.append(cycle[:])
-        print(f"global: {global_cycle}")
-    return [len(gc) for gc in global_cycle]
-
-
-# print(solution(["SL", "LR"]))
-# print(solution(["S"]))
-print(solution(["R", "R"]))
+print(solution(["SL", "LR"])) # 	[16]
+# print(solution(["S"])) # [1,1,1,1]
+# print(solution(["R", "R"])) # [4,4]
 
