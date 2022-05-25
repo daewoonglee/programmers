ee09cb0486feea5628e760dc7a3aca2d03024c48
daewoong lee <dw.lee832@gmail.com>
2022-05-25 11:42:41 +0900

    순위 문제풀이 02 - code refactoring

--- lev3/순위.py
+++ lev3/순위.py
@@ -1,36 +1,3 @@
-"""
-1: 2 승
-2: 5
-3: 2
-4: 3 2
-mmmmmmmm
-2: 1 3 4 패
-3: 4
-5: 2
-
-승리 그래프
-1  3 <- 4
- \ |  /
- > V <
-   2 -> 5
-
-패배 그래프
-1  3 -> 4
- < X  >
- \ | /
-   2 <- 5
-
-자기를 바라보는 엣지 + 내가 바라보는 엣지의 합이 n이 되느냐?
-
-자기 자신을 기준으로
-    1. 자기를 바라보는 엣지가 있는가? (자기가 패배, 상대가 승리)
-        나를 승리한 상대를 승리한 상대가 있는가?, 단 나를 이미 승리한 상대는 제외
-    2. 내가 바라보는 엣지가 있는가? (자기가 승리, 상대가 패배)
-        내가 승리한 상대에 대해서 상대가 패배한 상대가 있는가?, 단 내가 이미 승리한 상대는 제외
-    1+2 관계의 합이 n이 되는 경우를 ans에 추
-"""
-
-
 def solution(n, results):
     def win_search(k, visited, res=0):
         res += 1
@@ -49,26 +16,45 @@ def solution(n, results):
                 if visited[node-1]: continue
                 res = loose_search(node, visited, res)
         return res
-# 1 -> 2 -> 3, n=3
-    win, loose = {}, {}
+
+    # 0.8407392800000001
+    # win, loose = {}, {}
+    # for r in results:
+    #     w, l = r
+    #     if w not in win: win[w] = []
+    #     if l not in loose: loose[l] = []
+    #     win[w].append(l)
+    #     loose[l].append(w)
+    #
+    # ans = 0
+    # for i in range(1, n+1):
+    #     win_nodes, loose_nodes = 0, 0
+    #     loose_nodes += loose_search(i, [0 for _ in range(n)])
+    #     win_nodes += win_search(i, [0 for _ in range(n)])
+    #     if win_nodes + loose_nodes == n+1: ans += 1
+    # return ans
+
+    # code refactoring - 0.483563335
+    from collections import defaultdict
+    win, loose = defaultdict(set), defaultdict(set)
     for r in results:
-        w, l = r
-        if w not in win: win[w] = []
-        if l not in loose: loose[l] = []
-        win[w].append(l)
-        loose[l].append(w)
-    print(f"win: {win}")
-    print(f"loose: {loose}")
-    ans = 0
+        win[r[0]].add(r[1])
+        loose[r[1]].add(r[0])
     for i in range(1, n+1):
-        win_nodes, loose_nodes = 0, 0
-        loose_nodes += loose_search(i, [0 for _ in range(n)])
-        win_nodes += win_search(i, [0 for _ in range(n)])
-        print(f"i: {i}, win: {win_nodes}, loose: {loose_nodes}")
-        if win_nodes + loose_nodes == n+1: ans += 1
-    return ans
+        for looser in win[i]: loose[looser].update(loose[i])
+        for winner in loose[i]: win[winner].update(win[i])
+    return sum([1 for i in range(1, n+1) if len(loose[i]) + len(win[i]) == n-1])
 
 
-# print(solution(5, [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]))
-# print(solution(5, [[1, 2], [2, 3], [3, 4], [4, 5]]))
-print(solution(6, [[1, 2], [2, 3], [3, 4], [4, 5], [4,6]]))
+print(solution(5, [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]))    # 2
+print(solution(5, [[1, 2], [2, 3], [3, 4], [4, 5]]))            # 5
+print(solution(6, [[1, 2], [2, 3], [3, 4], [4, 5], [4,6]]))     # 4
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [[5, [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]],
+             [5, [[1, 2], [2, 3], [3, 4], [4, 5]]],
+             [6, [[1, 2], [2, 3], [3, 4], [4, 5], [4,6]]],
+             [5, [[1, 2], [1, 3], [1, 4], [1, 5], [2, 3], [2, 4], [2, 5], [3, 4], [3, 5], [4, 5]]]]
+    t = Timer(f"for t in {query}: solution(*t)", "from __main__ import solution")
+    print(t.timeit(number=10000))
