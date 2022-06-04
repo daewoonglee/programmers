e0467ce7d78a83933784c2e568f2ea1856271c82
daewoong lee <dw.lee832@gmail.com>
2022-06-04 11:49:39 +0900

    모두 0으로 만들기 문제풀이 03 - code refactoring

--- lev3/모두 0으로 만들기.py
+++ lev3/모두 0으로 만들기.py
@@ -14,6 +14,19 @@ def solution(a, edges):
             a[n] = 0
         return cnt
 
+    def search1(node, visited):
+        visited[node] = 1
+        cnt = a[node]
+        for n in relation[node]:
+            if visited[n]: continue
+            cnt += search1(n, visited)
+        ans[0] += abs(cnt)
+        return cnt
+
+    # search1
+    ans = [0]
+    if sum(a): return -1
+
     relation = {}
     for e in edges:
         l, r = e
@@ -22,14 +35,32 @@ def solution(a, edges):
         relation[l].append(r)
         relation[r].append(l)
 
-    c = search(edges[0][0], [0 for _ in a])
-    return c
+    # 0.41819209700000004
+    # c = search(edges[0][0], [0 for _ in a])
+    # return c
+
+    # code refactoring - 0.39843556199999997
+    search1(edges[0][0], [0 for _ in a])
+    return ans[0]
 
 
-# print(solution([-5,0,2,1,2], [[0,1],[3,4],[2,3],[0,3]])) # 9
+print(solution([-5,0,2,1,2], [[0,1],[3,4],[2,3],[0,3]])) # 9
 # print(solution([0,-5,2,1,2], [[0,1],[3,4],[2,3],[0,3]])) # 14
 # print(solution([-3,-2,2,1,2], [[0,1],[3,4],[2,3],[0,3]])) # 11
 # print(solution([0,-1,2,1,2,-2,-2], [[0,1],[3,4],[2,3],[0,3],[5,1],[6,1]])) # 18
 # print(solution([0,0,0,0,0,0,0], [[0,1],[3,4],[2,3],[0,3],[5,1],[6,1]])) # 0
-print(solution([1,2,2,0,-1,-2,-2], [[0,1],[0,2],[0,3],[3,4],[4,5],[4,6]])) # 18
+# print(solution([1,2,2,0,-1,-2,-2], [[0,1],[0,2],[0,3],[3,4],[4,5],[4,6]])) # 18
 # print(solution([0,1,0], [[0,1], [1,2]])) # -1
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [[[-5,0,2,1,2], [[0,1],[3,4],[2,3],[0,3]]],
+             [[0,-5,2,1,2], [[0,1],[3,4],[2,3],[0,3]]],
+             [[-3,-2,2,1,2], [[0,1],[3,4],[2,3],[0,3]]],
+             [[0,-1,2,1,2,-2,-2], [[0,1],[3,4],[2,3],[0,3],[5,1],[6,1]]],
+             [[0,0,0,0,0,0,0], [[0,1],[3,4],[2,3],[0,3],[5,1],[6,1]]],
+             [[1,2,2,0,-1,-2,-2], [[0,1],[0,2],[0,3],[3,4],[4,5],[4,6]]],
+             [[0,1,0], [[0,1], [1,2]]]]
+    t = Timer(f"for t in {query}: solution(*t)", "from __main__ import solution")
+    print(t.timeit(number=10000))
