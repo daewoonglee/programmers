a90f0f5124430810d1f6665314f782e631a27e00
daewoong lee <dw.lee832@gmail.com>
2022-06-08 10:52:00 +0900

    선입 선출 스케줄링 문제풀이 04 - 풀이O (테스트 14/14, 효율성 6/6)

--- lev3/선입 선출 스케줄링.py
+++ lev3/선입 선출 스케줄링.py
@@ -2,41 +2,34 @@ import math
 def solution(n, cores):
     N = len(cores)
     if n <= N: return n
-    n -= N # 3
+    n -= N
 
-    left, right = math.ceil(min(cores) * n / N), math.ceil(max(cores) * n / N) # 1, 3
-    mid = (right+left) // 2 # 2
+    left, right = math.ceil(min(cores) * n / N), math.ceil(max(cores) * n / N)
     while left < right:
+        mid = (right+left) // 2
         fin_cores = 0
         for c in cores:
-            fin_cores += mid // c # 2 1 0
-        print(f"before mid: {mid}, left: {left}, right: {right}, fin: {fin_cores}")
-        if fin_cores > n: right = mid-1 # 1
-        elif fin_cores < n: left = mid+1 #
-        else: break
-        mid = (right+left) // 2 #
-        print(f"after mid: {mid}, left: {left}, right: {right}, fin: {fin_cores}\n")
-    print(f"left: {left}, right: {right}, mid: {mid}")
-
-    pre = sum([(mid-1)//c for c in cores])
+            fin_cores += mid // c
+        print(f"mid: {mid}, left: {left}, right: {right}, fin: {fin_cores}, n: {n}")
+        if fin_cores >= n: right = mid
+        else: left = mid+1
+    print(f"out left: {left}, right: {right}, mid: {mid}")
+    pre = sum([(right-1)//c for c in cores])
     cur = n-pre
     print(f"pre: {pre}, cur: {n - pre}")
     idx = 0
     for i, c in enumerate(cores):
-        if mid % c == 0: idx+=1
+        if right % c == 0: idx+=1
         if cur == idx: return i + 1
     return N
 
 
-"""print(solution(6, [1, 2, 3])) # 2
-    1   2   3
-0   O   O   O
-1   O
-2   O   O
-print(solution(3, [1, 3])) # 1
-    1   3
-0   O   O
-1   O
+""" 
+    1   2                       1   2   3
+0   O   O   [1   2]             O   O   O   [1  2   3]
+1   O       [3    ]             O           [4          ]
+2   O   O   [4   5]             O   O       [5  6       ]
+3   O       [6    ]             O       O   [7      8]
 
      1   2   3   1
 0    O   O   O   O   [1  1   1   1]  0   1   2   3
@@ -52,15 +45,11 @@ print(solution(3, [1, 3])) # 1
 10   O   O       O   [1  1   0   1]  16  17  18  19
 11   O           O   [1  0   0   1]  20  21  22  23
 12   O   O   O   O   [1  1   1   1]
-
-9  | 25 = 9 4 3 9
-6  | 17 = 6 3 2 6
-11 | 30 = 11 5 3 11
 """
 
 # print(solution(6, [1, 2, 3])) # 2
 # print(solution(22, [1, 2, 3, 1])) # 1
 # print(solution(34, [1, 2, 3, 1])) # 4
-# print(solution(330, [1, 2, 3, 1])) # 1
-print(solution(324, [1, 2])) # 2
-print(solution(3, [1, 3])) # 1
+print(solution(330, [1, 2, 3, 1])) # 1
+# print(solution(324, [1, 2])) # 1
+# print(solution(3, [1, 3])) # 1
