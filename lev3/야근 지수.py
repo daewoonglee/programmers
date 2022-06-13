5a65fe3be78b2a1f83806a98bfdf21c6b8bcb3ad
daewoong lee <dw.lee832@gmail.com>
2022-06-13 09:58:00 +0900

    야근 지수 문제풀이 02 - 풀이O (테스트 13/13, 효율 성 2/2)

--- lev3/야근 지수.py
+++ lev3/야근 지수.py
@@ -1,21 +1,17 @@
+import heapq
 def solution(n, works):
     if sum(works) <= n: return 0
-    # ver01
-    # works.sort(reverse=True)
+    works = [-w for w in works]
+    heapq.heapify(works)
     for _ in range(n):
-        # works[0] -= 1
-        # if works[0] < works[1]:
-        #     works.sort(reverse=True)
-
-        # ver02
-        works[works.index(max(works))] -= 1
-    return sum([w*w for w in works])
+        heapq.heappush(works, heapq.heappop(works)+1)
+    return sum([(w*w) for w in works])
 
 
 # print(solution(4,[4,3,3])) # 12
 # print(solution(1,[2,1,2])) # 6
 # print(solution(3,[1,1]))   # 0
 # print(solution(7,[10,9,3,3,3])) # 99
-print(solution(99,[2,15,22,55,55])) # 580
-# 2 12 12 12 12
-#99, [2, 15, 22, 55, 55], 580
+# print(solution(99,[2,15,22,55,55])) # 580
+print(solution(2,[8])) # 580
+
