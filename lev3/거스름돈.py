1634ed45c5ccc70139a8a956af3af8bd0f00cbd1
daewoong lee <dw.lee832@gmail.com>
2022-06-20 12:46:55 +0900

    거스름돈 문제풀이 03 - 효율성 접근 방법 참조, 풀이O (테스트 14/14, 효율성 6/6)

--- lev3/거스름돈.py
+++ lev3/거스름돈.py
@@ -1,50 +1,33 @@
 def solution(n, money):
-    def get_change(change, idx):
-        if idx >= len(money) or money[idx] > change: return
-        q = change // money[idx]
-        for sub_q in range(q+1):
-            diff = change-sub_q*money[idx]
-            if diff == 0: ans[0] += 1
-            else: get_change(diff, idx+1)
-
-    ans = [0]
     money.sort()
-    get_change(n, 0)
-
-    return ans[0] % 1000000007
+    ans = [1 if i % money[0] == 0 else 0 for i in range(n+1)]
+    N = len(ans)-1
+    for m in money[1:]:
+        for i in range(N):
+            if i+1 < m: continue
+            ans[i+1] += ans[i-m+1]
+    return ans[-1] % 1000000007
 
 
 """
-n=5, money=[1,2,3,4]
-1 1 1 1 1
-1 1 1 2
-1 1 3
-1 2 2
-1 4
-2 3
+            0           1       2           3       4           5
+1           1 (0,0)   1 (0,1)   1 (0,2)   1 (0,3)   1 (0,4)   1 (0,5)
+1,2         1 (1,0)   1 (1,1)   2 (1,2)   2 (1,3)   3 (1,4)   3 (1,5) 
+1,2,3       1 (2,0)   1 (2,1)   2 (2,2)   3 (2,3)   4 (2,4)   5 (2,5)
+1,2,3,4     1 (3,0)   1 (3,1)   2 (3,2)   3 (3,3)   5 (3,4)   6 (3,5)
 
-n=9, money=[1,2,3,4]
-1 1 1 1 1 1 1 1 1
-1 1 1 1 1 1 1 2
-1 1 1 1 1 1 3
-1 1 1 1 1 2 2
-1 1 1 1 1 4
-1 1 1 1 2 3
-1 1 1 2 2 2
-1 1 1 2 4
-1 1 1 3 3
-1 1 2 2 3
-1 1 4 3
-1 2 2 2 2
-1 2 2 4
-1 2 3 3
-1 4 4
-2 2 2 3
-2 4 3
-3 3 3
+            0   1   2   3   4   5
+1           1   1   1   1   1   1
+1,2         1   1   2   2   3   3 
+1,2,5       1   1   2   2   3   4
+
+            0   1   2   3   4   5
+2           1   0   1   0   1   0
+2,5         1   0   1   0   1   1
 """
 
-# print(solution(5, [1,2,5])) # 4
-# print(solution(5, [1,2,3,4])) # 6
+print(solution(5, [1,2,5])) # 4
+print(solution(5, [1,2,3,4])) # 6
+print(solution(5, [2,5])) # 1
 # print(solution(6, [1,2,3]))   # 7
-print(solution(9, [1,2,3,4])) # 18
+# print(solution(9, [1,2,3,4])) # 18
