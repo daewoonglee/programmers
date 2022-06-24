731a6e57a5c30e9352625d78bbce6f8a31e32074
daewoong lee <dw.lee832@gmail.com>
2022-06-24 12:56:53 +0900

    최고의 집합 문제풀이 01 - code refactoring

--- lev3/최고의 집합.py
+++ lev3/최고의 집합.py
@@ -1,33 +1,34 @@
 def solution(n, s):
+    # 1.577689774
     if n > s: return [-1]
-    ans = []
-    for i in range(n, 0, -1):
-        ans.append(s//i)
-        s -= ans[-1]
+    # ans = []
+    # for i in range(n, 0, -1):
+    #     ans.append(s//i)
+    #     s -= ans[-1]
+    # return ans
+
+    # code refactoring - 0.072525688, 몫에서 나머지 값의 범위만큼 맨 뒤에서부터 +1 해주면 됨
+    d = s % n
+    ans = [s//n]*n
+    for i in range(1, d+1):
+        ans[-i] += 1
     return ans
 
-# n=3, s=12, [4,4,4]
-# n=3, s=11, [3,4,4]
-"""
-n = 11
-1 1 9
-1 2 8
-1 3 7
-1 4 6
-1 5 5
-
-2 2 7 28
-2 3 6 36
-2 4 5 40
-
-3 3 5 45
-3 4 4 48
-"""
-
-# n=3, s=9, [3,3,3]
 
 # print(solution(2, 9))
-# print(solution(2, 1))
+print(solution(2, 1))
 # print(solution(3, 12))
 print(solution(3, 11))
 # print(solution(3, 9))
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [[2,9],
+             [2,1],
+             [3,12],
+             [3,11],
+             [3,9],
+             [1000,10000]]
+    t = Timer(f"for t in {query}: solution(*t)", "from __main__ import solution")
+    print(t.timeit(number=10000))
