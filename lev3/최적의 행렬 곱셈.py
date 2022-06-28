343b3484f8037edf28dbcc17352130aeec043735
daewoong lee <dw.lee832@gmail.com>
2022-06-28 16:34:30 +0900

    최적의 행렬 곱셈 문제풀이 02 - 풀이방법참조, 풀이O (테스트 14/14, 효율성 6/6)

--- lev3/최적의 행렬 곱셈.py
+++ lev3/최적의 행렬 곱셈.py
@@ -1,39 +1,35 @@
 def solution(matrix_sizes):
     N = len(matrix_sizes)
-    global_ans = 2**31-1
-    for i in range(N-1):
-        info = [0] * N
-        ans = 0
-        w, h = matrix_sizes[i]
-        info[i] = 1
-
-        while 1:
-            j = 0 # [5,3], [3,6] 계산 필요
-            while j < N:
-                if not info[j]:
-                    if h == matrix_sizes[j][0]:
-                        ans += (w * h * matrix_sizes[j][1])
-                        h = matrix_sizes[j][1]
-                    info[j] = 1
-                    break
-                j += 1
-            else: break
-        if sum(info) == N and global_ans > ans: global_ans = ans
-
-    return global_ans
-
+    DP = [[0]*N for _ in range(N)]
+    for i in range(N-2, -1, -1):
+        for j in range(i+1, N): # i:1, j:2, i:0, j:1, i:0, j:2
+            print(f"i: {i}, j: {j}")
+            calc = []
+            for m in range(i, j):
+                calc.append(DP[i][m] + DP[m+1][j] + (matrix_sizes[i][0] * matrix_sizes[m][1] * matrix_sizes[j][1]))
+            DP[i][j] = min(calc)
+    return DP[0][N-1]
 
 """
-중복된 행렬 값이 있을 수 있는가?, 지문에 중복이 없다는 말은 없음
 [5,3], [3,10], [10,6]
-[5,10], [3,6]
+[5, 3, 10, 6]
 
-[5,3], [3,10] -> 150
-[5,10], [10,6] -> 300
+((A,B),C) = (5*3*10) + (5*10*6)
+(A, (B, C)) = (5*3*6) + (3*10*6)
 
-[3,10], [10,6] -> 180
-[5,3], [3,6] -> 90
+        0(A)    1(B)    2(C)
+0(A)    0       150     270
+1(B)    0       0       180
+2(C)    0       0       0
+
+DP[s][e] = min(DP[s][m] + DP[m+1][e] + S[0] * M[1] * E[1], s <= m < e)
+DP[1][2] = min(DP[1][1] + DP[2][2] + matrix_sizes[1][0] * matrix_sizes[1][1] * matrix_sizes[2][1] = 180)
+DP[0][1] = min(DP[0][0] + DP[1][1] + matrix_sizes[0][0] * matrix_sizes[0][1] * matrix_sizes[1][1] = 150)
+DP[0][2] = min(
+    DP[0][0] + DP[1][2] + matrix_sizes[0][0] * matrix_sizes[0][1] * matrix_sizes[2][1] = 0 + 180 + (5 * 3 * 6) = 270,
+    DP[0][1] + DP[2][2] + matrix_sizes[0][0] * matrix_sizes[1][1] * matrix_sizes[2][1]  = 150 + 0 + (5 * 10 * 6) = 450
+)
 """
 
-print(solution([[5,3],[3,10],[10,6]])) # 270
-# print(solution([[5,3],[3,10],[6,3],[10,6]]))
+# print(solution([[5,3],[3,10],[10,6]])) # 270
+print(solution([[5,3],[3,10],[6,3],[10,6]]))
