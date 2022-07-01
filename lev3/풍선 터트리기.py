3925dd3a0b96c63d266ff60df31f59d8a0bc4480
daewoong lee <dw.lee832@gmail.com>
2022-07-01 12:24:05 +0900

    풍선 터뜨리기 문제풀이 02 - 풀이X (테스트 9/15, 시간초과)

--- lev3/풍선 터트리기.py
+++ lev3/풍선 터트리기.py
@@ -1,16 +1,21 @@
 def solution(a):
-    if len(a) < 3: return len(a)
+    N = len(a)
+    if N < 3: return N
+
+    min_l = [a[0]] * (N-2)
+    for i in range(2, N-1):
+        min_l[i-1] = a[i-1] if a[i-1] < min_l[i-2] else min_l[i-2]
+
+    min_r = [min(a[2:])] * (N-2)
+    for i in range(2, N-1):
+        min_r[i-1] = min(a[i+1:]) if a[i] == min_r[i-2] else min_r[i-2]
+
     ans = 2
-    l, r = a[0], min(a[2:])
     for i, n in enumerate(a[1:-1]):
-        print(f"n: {n}, left: {l}, right: {r}")
-        if l < n and r < n: continue
-        ans += 1
-        l = a[i+1] if a[i+1] < l else l
-        r = min(a[i+2:]) if a[i+1] == r else r
+        if min_l[i] > n or min_r[i] > n: ans += 1
     return ans
 
 
-print(solution([9,-1,-5])) # 3
-print(solution([-16,27,65,-2,58,-92,-71,-68,-61,-33])) # 6
-
+# print(solution([9,-1,-5])) # 3
+# print(solution([-16,27,65,-2,58,-92,-71,-68,-61,-33])) # 6
+print(solution([-16,27,-65,-33,-61,-64])) # 3
