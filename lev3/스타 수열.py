40e67455a9eca05d75c4f3c6b3560c9bf8410594
daewoong lee <dw.lee832@gmail.com>
2022-07-06 11:40:41 +0900

    스타 수열 문제풀이 03 - code refactoring

--- lev3/스타 수열.py
+++ lev3/스타 수열.py
@@ -2,64 +2,75 @@ def solution(a):
     N = len(a)-1
     if not N: return 0
 
-    idx = 0
-    cnt = dict()
-    # index_list = dict()
-    while idx < N:
-        if a[idx] not in cnt:
-            cnt[a[idx]] = 0
-            # index_list[a[idx]] = []
+    # version1 - 1.600814453
+    # idx = 0
+    # cnt = dict()
+    # while idx < N:
+    #     if a[idx] not in cnt:
+    #         cnt[a[idx]] = 0
+    #     cnt[a[idx]] += 1
+    #     if a[idx] == a[idx+1]:
+    #         i = idx+2
+    #         while i < N:
+    #             if a[idx] != a[i]: break
+    #             i += 1
+    #         cnt[a[idx]] += 1
+    #         idx = i
+    #     else: idx += 1
+    #
+    # if a[-2] != a[-1]:
+    #     if a[-1] not in cnt:
+    #         cnt[a[-1]] = 0
+    #     cnt[a[idx]] += 1
 
-        # index_list[a[idx]].append(idx)
-        cnt[a[idx]] += 1
-        if a[idx] == a[idx+1]:
-            i = idx+2
-            while i < N:
-                if a[idx] != a[i]: break
-                i += 1
-            # index_list[a[idx]].append(i-1)
-            cnt[a[idx]] += 1
-            idx = i
-        else: idx += 1
+    # version2 - 1.4618284209999999
+    # cnt = dict()
+    # cnt[a[0]] = 1
+    # for i in range(1, N):
+    #     if a[i-1] == a[i] == a[i+1]: continue
+    #     if a[i] not in cnt: cnt[a[i]] = 1
+    #     else: cnt[a[i]] += 1
+    #
+    # if a[-2] != a[-1]:
+    #     if a[-1] not in cnt:
+    #         cnt[a[-1]] = 0
+    #     cnt[a[-1]] += 1
 
-    if a[-2] != a[-1]:
-        if a[-1] not in cnt:
-            cnt[a[-1]] = 0
-            # index_list[a[-1]] = []
-        # index_list[a[-1]].append(N)
-        cnt[a[idx]] += 1
+    # k = sorted(cnt.items(), key=lambda x: x[1])[-1][0]
+    # ans = 0
+    # idx = 0
+    # pre = -1
+    # while idx < N:
+    #     if a[idx] != k:
+    #         if idx-1 > pre and a[idx-1] == k:
+    #             ans += 2
+    #         elif a[idx+1] == k:
+    #             ans += 2
+    #             idx += 1
+    #         pre = idx
+    #     idx += 1
+    # return ans
 
-    print(f"cnt: {cnt}")
-    # print(f"idx: {index_list}")
+    # code refactoring - 1.025373673
+    dic = dict()
+    for i, v in enumerate(a):
+        if v not in dic: dic[v] = list()
+        dic[v].append(i)
 
-    k = sorted(cnt.items(), key=lambda x: x[1])[-1][0]
+    N = len(a)-1
     ans = 0
-    # check_list = []
-    # for idx in index_list[k]:
-    #     if idx > 0 and a[idx] != a[idx-1] and idx-1 not in check_list:
-    #         check_list.append(idx-1)
-    #         ans += 2
-    #     elif idx < N-1 and a[idx] != a[idx+1] and idx+1 not in check_list:
-    #         check_list.append(idx+1)
-    #         ans += 2
-
-    idx = 0
-    pre = -1
-    while idx < N:
-        # print(f"before: {idx}, pre: {pre}, idx: {idx}")
-        if a[idx] != k:
-            if idx-1 > pre and a[idx-1] == k:
-                # pre = idx
-                ans += 2
-                # print(f"+1: {idx}, ans: {ans}")
-            elif a[idx+1] == k:
-                ans += 2
-                idx += 1
-                # pre = idx
-                # print(f"+2: {idx}, ans: {ans}")
-            pre = idx
-        idx += 1
-        # print(f"after: {idx}, pre: {pre}, idx: {idx}\n")
+    for k, v in dic.items():
+        if len(v) <= ans // 2: continue
+        now = a.copy()
+        cnt = 0
+        for j in v:
+            if j > 0 and now[j-1] != k:
+                now[j-1] = k
+                cnt += 2
+            elif j < N and now[j+1] != k:
+                now[j+1] = k
+                cnt += 2
+        ans = ans if ans > cnt else cnt
     return ans
 
 
@@ -72,7 +83,16 @@ def solution(a):
 # print(solution([5,2,3,4,6,7,8,9,2,4,7,1,7,10])) # 6
 # print(solution([5,2,2,4,2,3,4,1,4,6,4,7])) # 8
 # print(solution([1,2,3,4,5,6,7,8,9,10])) # 2
-# print(solution([5,2,2,4,2,3,4,1,4,6,4,7,2,2,2,2])) # 8
-# print(solution([0,2,0,3,1,1,1,1,1,1,1,1,1,0,4])) # 6
+print(solution([5,2,2,4,2,3,4,1,4,6,4,7,2,2,2,2])) # 8
+print(solution([0,2,0,3,1,1,1,1,1,1,1,1,1,0,4])) # 6
 print(solution([0,1,2,3,0])) # 4
-print(solution([0,1]*1000)) # 4
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [[0], [0,1], [0,0,0,0], [1,2,3,3,5,3], [2,3,3,5,4,3], [0,3,3,0,7,2,0,2,2,0], [5,2,3,4,6,7,8,9,2,4,7,1,7,10],
+             [5,2,2,4,2,3,4,1,4,6,4,7], [1,2,3,4,5,6,7,8,9,10], [5,2,2,4,2,3,4,1,4,6,4,7,2,2,2,2],
+             [0,2,0,3,1,1,1,1,1,1,1,1,1,0,4], [0,2,0,3,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0,4],
+             [0,1,2,3,0], [0,1]*1000, [i for i in range(1000)]]
+    t = Timer(f"for t in {query}: solution(t)", "from __main__ import solution")
+    print(t.timeit(number=1000))
