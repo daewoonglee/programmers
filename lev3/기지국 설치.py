5b1272769c720b989933b222d4a1b08487d7eb00
daewoong lee <dw.lee832@gmail.com>
2022-07-11 10:37:38 +0900

    기지국 설치 문제풀이 02 - 풀이O (테스트 21/21, 효율성 4/4)

--- lev3/기지국 설치.py
+++ lev3/기지국 설치.py
@@ -1,35 +1,27 @@
 import math
+
+
 def solution(n, stations, w):
     ans = 0
-    apart = [0] * n
     step = w*2+1
+    pre = 0
     for s in stations:
-        for i in range(s-w-1, s+w):
-            if 0 <= i < n: apart[i] = 1
-    print(f"log: {apart}")
+        ans += math.ceil((s-w-1-pre) / step)
+        # print(f"s: {s}, s-w: {s-w}, pre: {pre}, ans: {ans}")
+        pre = s+w
+    # if pre < n: ans += math.ceil((n-pre) / step)
 
-    pre, idx = -1, 0
-    while idx < n:
-        if apart[idx] == 0:
-            if pre == -1: pre = idx
-        else:
-            if pre != -1:
-                print(f"ans: {ans}, idx: {idx}, pre: {pre}, step: {step}, sum: {((idx-pre) // step + 1)}, //: {(idx-pre)/step}, ceil: {math.ceil((idx-pre)/step)}")
-                # ans += ((idx-pre) // step + 1)
-                ans += (math.ceil((idx-pre) / step))
-                for i in range(pre, idx):
-                    apart[i] = 1
-                    # print(f"pre: {pre}, idx: {idx}, i: {i}")
-                pre = -1
-        idx += 1
-
-    print(f"ans: {ans}, idx: {idx}, pre: {pre}, step: {step}, sum: {((idx-pre) // step + 1)}")
-    # if pre != -1: ans += ((idx-pre) // step + 1)
-    if pre != -1: ans += (math.ceil((idx-pre) / step))
+    print(f"pre: {pre}, ans: {ans}")
+    if pre < n: ans += math.ceil((n-pre) / step)
 
     return ans
 
 
-# print(solution(11, [4, 11], 1))
-print(solution(16, [9], 2))
-
+# print(solution(11, [4, 11], 1)) # 3
+# print(solution(16, [9], 2))     # 3
+# print(solution(11, [2,4,6,8,10], 1))    # 0
+# print(solution(14, [2,4,6,8,10], 1))    # 1
+# print(solution(15, [2,4,6,8,10], 1))    # 2
+print(solution(10, [2,4,6,8,10], 2))    # 0
+print(solution(12, [2,4,6,8,10], 2))    # 0
+print(solution(13, [2,4,6,8,10], 2))    # 0
