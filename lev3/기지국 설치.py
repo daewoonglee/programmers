45cf4190fda6e67f72d02d9eb1d0b730d7a8d9fe
daewoong lee <dw.lee832@gmail.com>
2022-07-12 09:49:49 +0900

    기지국 설치 문제풀이 02 - code refactoring

--- lev3/기지국 설치.py
+++ lev3/기지국 설치.py
@@ -2,26 +2,46 @@ import math
 
 
 def solution(n, stations, w):
-    ans = 0
+    # 0.141598809
+    # ans = 0
+    # step = w*2+1
+    # pre = 0
+    # for s in stations:
+    #     ans += math.ceil((s-w-1-pre) / step)
+    #     pre = s+w
+    # return ans + math.ceil((n-pre)/step) if pre < n else ans
+
+    # code refactoring - 0.09231730899999999
+    bef =- w
+    cnt = 0
     step = w*2+1
-    pre = 0
     for s in stations:
-        ans += math.ceil((s-w-1-pre) / step)
-        # print(f"s: {s}, s-w: {s-w}, pre: {pre}, ans: {ans}")
-        pre = s+w
-    # if pre < n: ans += math.ceil((n-pre) / step)
-
-    print(f"pre: {pre}, ans: {ans}")
-    if pre < n: ans += math.ceil((n-pre) / step)
-
-    return ans
+        cnt += (s-bef-1)//step
+        bef = s
+        print(f"cnt: {cnt}, bef: {bef}")
+    return cnt + (n+w-bef)//step
 
 
 # print(solution(11, [4, 11], 1)) # 3
-# print(solution(16, [9], 2))     # 3
+print(solution(16, [9], 2))     # 3
 # print(solution(11, [2,4,6,8,10], 1))    # 0
 # print(solution(14, [2,4,6,8,10], 1))    # 1
 # print(solution(15, [2,4,6,8,10], 1))    # 2
-print(solution(10, [2,4,6,8,10], 2))    # 0
-print(solution(12, [2,4,6,8,10], 2))    # 0
-print(solution(13, [2,4,6,8,10], 2))    # 0
+# print(solution(10, [2,4,6,8,10], 2))    # 0
+# print(solution(12, [2,4,6,8,10], 2))    # 0
+# print(solution(13, [2,4,6,8,10], 2))    # 1
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [[11, [4,11], 1],
+             [16, [9], 2],
+             [11, [2,4,6,8,10], 1],
+             [14, [2,4,6,8,10], 1],
+             [15, [2,4,6,8,10], 1],
+             [10, [2,4,6,8,10], 2],
+             [12, [2,4,6,8,10], 2],
+             [13, [2,4,6,8,10], 2],
+             [10000000, [100, 1000, 10000], 2]]
+    t = Timer(f"for t in {query}: solution(*t)", "from __main__ import solution")
+    print(t.timeit(number=10000))
