7a33e50b1a019a9ff9ba2bb3dbc0d5c4c0f9aa74
daewoong lee <dw.lee832@gmail.com>
2022-07-14 10:19:55 +0900

    스티커 모으기(2) 문제풀이 02 - code refactoring01

--- lev3/스티커 모으기(2).py
+++ lev3/스티커 모으기(2).py
@@ -2,7 +2,7 @@ def solution(sticker):
     N = len(sticker)
     if N <= 1: return max(sticker)
 
-    # 1.0120966219999998
+    # 0.913502072
     if N % 2 == 0:
         even, odd = 0, 0
         for i, s in enumerate(sticker):
@@ -10,18 +10,17 @@ def solution(sticker):
             else: odd += s
         return even if even > odd else odd
     else:
-        # 1.576705273
+        # 1.459002315
         dp1 = [0] * N
         dp1[0] = sticker[0]
         dp1[1] = sticker[0]
+
         dp2 = [0] * N
         dp2[1] = sticker[1]
-        for i in range(2, N-1):
-            dp1[i] = max(dp1[i-1], dp1[i-2]+sticker[i])
-        for i in range(2, N):
-            dp2[i] = max(dp2[i-1], dp2[i-2]+sticker[i])
-        s1, s2 = max(dp1), max(dp2)
-        return s1 if s1 > s2 else s2
+
+        for i in range(2, N-1): dp1[i] = max(dp1[i-1], dp1[i-2]+sticker[i])
+        for i in range(2, N): dp2[i] = max(dp2[i-1], dp2[i-2]+sticker[i])
+        return dp1[-2] if dp1[-2] > dp2[-1] else dp2[-1]
 
 
 print(solution([14, 6, 5, 11, 3, 9, 2, 10])) # 36
