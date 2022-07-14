31a558122d848d1a7144f355f33aecd61c5e9186
daewoong lee <dw.lee832@gmail.com>
2022-07-14 10:06:09 +0900

    스티커 모으기(2) 문제풀이 02 - 풀이O (접근방법 참조, 테스트 33/33, 효율성 4/4)

--- lev3/스티커 모으기(2).py
+++ lev3/스티커 모으기(2).py
@@ -1,23 +1,32 @@
 def solution(sticker):
     N = len(sticker)
-    if N <= 3: return max(sticker)
+    if N <= 1: return max(sticker)
 
-    if N % 2 == 0:
-        even, odd = 0, 0
-        for i, s in enumerate(sticker):
-            if i % 2 == 0: even += s
-            else: odd += s
-        return even if even > odd else odd
-    else:
-        ans = 0
-        for i in range(N):
-            temp = sticker[i] + sum([sticker[(i+j*2)%N] for j in range(1, N//2)])
-            ans = temp if ans < temp else ans
-        return ans
+    # if N % 2 == 0:
+    #     even, odd = 0, 0
+    #     for i, s in enumerate(sticker):
+    #         if i % 2 == 0: even += s
+    #         else: odd += s
+    #     return even if even > odd else odd
+    # else:
+    dp1 = [0 for _ in sticker]
+    dp1[0] = sticker[0]
+    dp1[1] = sticker[0]
+    dp2 = [0 for _ in sticker]
+    dp2[1] = sticker[1]
+    for i in range(2, N-1):
+        dp1[i] = max(dp1[i-1], dp1[i-2]+sticker[i])
+    for i in range(2, N):
+        dp2[i] = max(dp2[i-1], dp2[i-2]+sticker[i])
+    s1, s2 = max(dp1), max(dp2)
+    return s1 if s1 > s2 else s2
 
 
-# print(solution([14, 6, 5, 11, 3, 9, 2, 10])) # 36
+print(solution([14, 6, 5, 11, 3, 9, 2, 10])) # 36
 # print(solution([1, 3, 2, 5, 4])) # 8
 # print(solution([1])) # 1
 # print(solution([1, 1, 1, 1, 1])) # 2
-print(solution([14, 6, 5, 11, 3, 9, 2])) # 34
+
+# print(solution([14, 6, 5, 11, 3, 9, 2])) # 34
+# print(solution([14, 6, 5, 11, 3, 9, 2, 10, 11, 12, 13])) # 56
+print(solution([4, 6, 50, 11, 30, 9, 20, 10, 11, 12, 13])) # 56
