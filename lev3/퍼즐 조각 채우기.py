6feb1d29ca7e5118788bbfb2ac57c5aa740cfd8c
daewoong lee <dw.lee832@gmail.com>
2022-08-05 17:49:30 +0900

    퍼즐 조각 채우기 문제풀이 03 - 풀이X (구현 X)

--- lev3/퍼즐 조각 채우기.py
+++ lev3/퍼즐 조각 채우기.py
@@ -1,35 +1,46 @@
-def solution(game_board, table):
-    answer = -1
-    return answer
+def get_coords(board, n):
+    def dfs(x, y):
+        c = []
+        stack = [(x, y)]
+        while stack:
+            x, y = stack.pop()
+            if maps[x][y] == 0:
+                c.append((x, y))
+                if x+1 < N: stack.append((x+1, y))
+                if x-1 >= 0: stack.append((x-1, y))
+                if y+1 < N: stack.append((x, y+1))
+                if y-1 >= 0: stack.append((x, y-1))
+                maps[x][y] = 1
+        return c
 
+    N = len(board)
+    coords = []
+    maps = [[not n if r else n for r in row] for row in board]
+    # maps = [[1 if r else 0 for r in row] for row in board]
+    for i in range(N):
+        for j in range(N):
+            if maps[i][j] == 0:
+                coords.append(dfs(i, j))
+    return coords
+
+
+def solution(game_board, table):
+    g_coords = get_coords(game_board, 0)
+    for gc in g_coords:
+        print(gc)
+    print()
+    t_coords = get_coords(table, 1)
+    for tc in t_coords:
+        print(tc)
+    print()
 
 """
-game_board
+퍼즐 형상에 대한 1차원 좌표값 변환 함수 작성 O
 
-형상을 [0,0,1] 등의 2차원 배열로 가지게 하려면 구현이 너무 복잡
-차라리 0에 해당하는 좌표값들만 1차원 배열로 구성한 뒤, 이를 기반으로 table 형상과 비교하는게 나을듯
-
-blank = [
-            [[0,0,1],[1,0,1],[1,0,0]], -> [(0,2), (0,3), (1,3), (2,3), (2,4)] 형식으로 좌표값 가지기
-            [[0],[0]], -> [(0,5), (1,5)]
-            [[0,0],[0,1]], -> [(1,0), (1,1), (2,0)]
-            [[1,0,1],[0,0,0]], -> [(3,2), (4,1), (4,2), (4,3)]
-            [[0]], -> [(5,0)]
-            [[1,0],[0,0]] -> [(4,5), (5,4), (5,5)]
-        ]
-
-table  
-puzzle = [
-            [[1],[1]],
-            [[1,1,0],[0,1,0],[0,1,1]],
-            [[0,1,0],[1,1,0],[0,1,0]],
-            [[1,1]],
-            [[1,1],[0,1]]
-        ]
-        
-위와 같은 리스트를 구한 뒤 (0,0) 기준으로 퍼즐 회전을 구현하여 서로 같은 지 매칭
-
-리스트를 구하기 위해선 리스트를 탐색하다 0을 만났을 때, 해당 0을 기준으로 dfs/bfs 형식으로 퍼즐 형상을 구해야 함.
+구현 필요 요소
+1. 좌표값을 (0,0)으로 고정한 뒤 비교 좌표값을 상하좌우 회전하며 같은지 매칭 
+    1-1. 매칭이 된다면 그에 상응하는 퍼즐 크기를 정답 변수에 추가 & 매칭된 비교군의 퍼즐 좌표값 제거 (중복 비교 없애기 위해)
+    1-2. 매칭이 안된다면 비교군 중 아직 비교하지 않은 퍼즐에 대해 상하좌우 반복 비교
 """
 
 print(solution([[1,1,0,0,1,0],[0,0,1,0,1,0],[0,1,1,0,0,1],[1,1,0,1,1,1],[1,0,0,0,1,0],[0,1,1,1,0,0]],
