43268ac3f14311ac70658c369471156890b32822
daewoong lee <dw.lee832@gmail.com>
2022-08-09 14:58:13 +0900

    퍼즐 조각 채우기 문제풀이 05 - 풀이O (테스트 18/22)

--- lev3/퍼즐 조각 채우기.py
+++ lev3/퍼즐 조각 채우기.py
@@ -1,72 +1,77 @@
-def get_coords(board, n):
-    def trans_coord(coord):
-        coord.sort(key=lambda x: [x[0], x[1]])
-        min_x = coord[0][0]
-        min_y = min([c[1] for c in coord])
-        t_coord = []
-        for c in coord:
-            t_coord.append((c[0]-min_x, c[1]-min_y))
-        return t_coord
+def get_puzzles(board, n):
+    def trans_coord(puzzle_coords):
+        puzzle_coords.sort(key=lambda x: [x[0], x[1]])
+        min_x = puzzle_coords[0][0]
+        min_y = min([pc[1] for pc in puzzle_coords])
+        trans_puzzle_coords = []
+        for pc in puzzle_coords:
+            trans_puzzle_coords.append((pc[0]-min_x, pc[1]-min_y))
+        return trans_puzzle_coords
 
     def dfs(x, y):
-        c = []
+        puzzle = []
         stack = [(x, y)]
         while stack:
             x, y = stack.pop()
             if maps[x][y] == 0:
-                c.append((x, y))
+                puzzle.append((x, y))
                 if x+1 < N: stack.append((x+1, y))
                 if x-1 >= 0: stack.append((x-1, y))
                 if y+1 < N: stack.append((x, y+1))
                 if y-1 >= 0: stack.append((x, y-1))
                 maps[x][y] = 1
-        return c
+        return puzzle
 
     N = len(board)
-    coords = []
+    puzzles = []
     maps = [[not n if r else n for r in row] for row in board]
     for i in range(N):
         for j in range(N):
             if maps[i][j] == 0:
-                coords.append(trans_coord(dfs(i, j)))
-    return coords
+                puzzles.append(trans_coord(dfs(i, j)))
+    return puzzles
 
 
-def is_match(gc, tc, m, idx):
-    def rotate(coord):
-        r_coord = []
-        for c in coord:
-            x, y = c
-            r_coord.append()
-        return r_coord
+def is_match(gp, tp, m, idx):
+    def rotate(puzzle):
+        N = max(p[0] for p in puzzle)
+        rot_puzzle = []
+        for p in puzzle:
+            x, y = p
+            rot_puzzle.append((y, abs(x-N)))
+        rot_puzzle.sort(key=lambda x: [x[0], x[1]])
+        return rot_puzzle
 
-    if m[idx]: return False
-    for i in range(4):
-        for g, t in zip(gc, tc):
+    # print(f"m: {m}")
+    if m[idx] or len(gp) != len(tp): return False
+    for i in range(3):
+        for g, t in zip(gp, tp):
             if g != t: break
         else:
             m[idx] = 1
             return True
-        tc = rotate(tc)
+        tp = rotate(tp)
+        # print(f"rot: {tp}")
 
 
 def solution(game_board, table):
-    g_coords = get_coords(game_board, 0)
-    t_coords = get_coords(table, 1)
+    game_puzzles = get_puzzles(game_board, 0)
+    table_puzzles = get_puzzles(table, 1)
 
-    matching = [0 for _ in t_coords]
+    # print(f"table_puzzles: {table_puzzles}")
+    matching = [0 for _ in table_puzzles]
     ans = 0
-    for g_coord in g_coords:
-        for i, t_coord in enumerate(t_coords):
-            if is_match(g_coord, t_coord, matching, i):
-                ans += len(g_coord)
+    for game_puzzle in game_puzzles:
+        # print(f"gp: {game_puzzle}")
+        for i, table_puzzle in enumerate(table_puzzles):
+            # print(f"i: {i}, tp: {table_puzzle}")
+            if is_match(game_puzzle, table_puzzle, matching, i):
+                ans += len(game_puzzle)
+                # print(f"ans: {ans}")
                 break
+        # print()
     return ans
 
-"""
-구현 필요 요소
-1. 비교 좌표값을 상하좌우 회전하며 같은지 매칭
-"""
 
 print(solution([[1,1,0,0,1,0],[0,0,1,0,1,0],[0,1,1,0,0,1],[1,1,0,1,1,1],[1,0,0,0,1,0],[0,1,1,1,0,0]],
                [[1,0,0,1,1,0],[1,0,1,0,1,0],[0,1,1,0,1,1],[0,0,1,0,0,0],[1,1,0,1,1,0],[0,1,0,0,0,0]])) # 14
