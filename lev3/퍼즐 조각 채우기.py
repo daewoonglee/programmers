2864cd69e3e5eb5ef067b03cdc934ec136e363ea
daewoong lee <dw.lee832@gmail.com>
2022-08-11 15:17:08 +0900

    퍼즐 조각 채우기 문제풀이 06 - code refactoring

--- lev3/퍼즐 조각 채우기.py
+++ lev3/퍼즐 조각 채우기.py
@@ -1,11 +1,9 @@
+"""
 def get_puzzles(board, n):
     def trans_coord(puzzle_coords):
         puzzle_coords.sort(key=lambda x: [x[0], x[1]])
-        min_x = puzzle_coords[0][0]
-        min_y = min([pc[1] for pc in puzzle_coords])
-        trans_puzzle_coords = []
-        for pc in puzzle_coords:
-            trans_puzzle_coords.append((pc[0]-min_x, pc[1]-min_y))
+        min_x, min_y = puzzle_coords[0][0], min([pc[1] for pc in puzzle_coords])
+        trans_puzzle_coords = [(pc[0]-min_x, pc[1]-min_y) for pc in puzzle_coords]
         return trans_puzzle_coords
 
     def dfs(x, y):
@@ -23,27 +21,20 @@ def get_puzzles(board, n):
         return puzzle
 
     N = len(board)
-    puzzles = []
     maps = [[not n if r else n for r in row] for row in board]
-    for i in range(N):
-        for j in range(N):
-            if maps[i][j] == 0:
-                puzzles.append(trans_coord(dfs(i, j)))
+    puzzles = [trans_coord(dfs(i, j)) for i in range(N) for j in range(N) if maps[i][j] == 0]
     return puzzles
 
 
 def is_match(gp, tp, m, idx):
     def rotate(puzzle):
-        N = max(p[0] for p in puzzle)
-        rot_puzzle = []
-        for p in puzzle:
-            x, y = p
-            rot_puzzle.append((y, abs(x-N)))
+        H = max(p[0] for p in puzzle)
+        rot_puzzle = [(p[1], abs(p[0]-H)) for p in puzzle]
         rot_puzzle.sort(key=lambda x: [x[0], x[1]])
         return rot_puzzle
 
-    # print(f"m: {m}")
     if m[idx] or len(gp) != len(tp): return False
+
     for i in range(4):
         for g, t in zip(gp, tp):
             if g != t: break
@@ -51,38 +42,42 @@ def is_match(gp, tp, m, idx):
             m[idx] = 1
             return True
         tp = rotate(tp)
+    return False
 
 
+# 2.912573497
 def solution(game_board, table):
     game_puzzles = get_puzzles(game_board, 0)
     table_puzzles = get_puzzles(table, 1)
-
-    print(f"table_puzzles: {table_puzzles}")
     matching = [0 for _ in table_puzzles]
     ans = 0
     for game_puzzle in game_puzzles:
-        print(f"gp: {game_puzzle}")
         for i, table_puzzle in enumerate(table_puzzles):
-            # print(f"i: {i}, tp: {table_puzzle}")
             if is_match(game_puzzle, table_puzzle, matching, i):
                 ans += len(game_puzzle)
-                # print(f"ans: {ans}")
                 break
-        # print()
     return ans
+"""
 
 
-# print(solution([[1,1,0,0,1,0],[0,0,1,0,1,0],[0,1,1,0,0,1],[1,1,0,1,1,1],[1,0,0,0,1,0],[0,1,1,1,0,0]],
-#                [[1,0,0,1,1,0],[1,0,1,0,1,0],[0,1,1,0,1,1],[0,0,1,0,0,0],[1,1,0,1,1,0],[0,1,0,0,0,0]])) # 14
-#
+print(solution([[1,1,0,0,1,0],[0,0,1,0,1,0],[0,1,1,0,0,1],[1,1,0,1,1,1],[1,0,0,0,1,0],[0,1,1,1,0,0]],
+               [[1,0,0,1,1,0],[1,0,1,0,1,0],[0,1,1,0,1,1],[0,0,1,0,0,0],[1,1,0,1,1,0],[0,1,0,0,0,0]])) # 14
+
 # print(solution([[0,0,0],[0,0,0],[0,0,0]],
 #                [[1,1,1],[1,1,1],[1,1,1]])) # 9
 
-print(solution([[0,0,0,0],
-                [0,0,0,1],
-                [0,0,0,0],
-                [0,0,0,0]],
-               [[1,1,1,1],
-                [1,1,1,1],
-                [1,1,1,1],
-                [1,1,0,1]])) # 1
+# print(solution([[0,0,0,0],[0,0,0,1],[0,0,0,0],[0,0,0,0]],
+#                [[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,0,1]])) # 15
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [[[[1,1,0,0,1,0],[0,0,1,0,1,0],[0,1,1,0,0,1],[1,1,0,1,1,1],[1,0,0,0,1,0],[0,1,1,1,0,0]],
+              [[1,0,0,1,1,0],[1,0,1,0,1,0],[0,1,1,0,1,1],[0,0,1,0,0,0],[1,1,0,1,1,0],[0,1,0,0,0,0]]],
+             [[[0,0,0],[0,0,0],[0,0,0]],
+              [[1,1,1],[1,1,1],[1,1,1]]],
+             [[[0,0,0,0],[0,0,0,1],[0,0,0,0],[0,0,0,0]],
+              [[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,0,1]]]]
+    t = Timer(f"for t in {query}: solution(*t)", "from __main__ import solution")
+    print(t.timeit(number=10000))
+
