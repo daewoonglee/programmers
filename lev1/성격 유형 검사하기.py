4d2ccbd6688bf5667f4f6ba8ca5503b3aec28040
daewoong lee <dw.lee832@gmail.com>
2022-08-22 13:32:38 +0900

    성격 유형 검사하기 문제풀이 01 - 풀이O (테스트 20/20)

---
+++ lev1/성격 유형 검사하기.py
@@ -0,0 +1,13 @@
+def solution(survey, choices):
+    li = "RTCFJMAN"
+    scores = [3, 2, 1, 0, 1, 2, 3]
+    cnt = {k: 0 for k in li}
+    for s, c in zip(survey, choices):
+        if c < 4: cnt[s[0]] += scores[c-1]
+        elif c > 4: cnt[s[1]] += scores[c-1]
+    ans = [li[i*2] if cnt[li[i*2]] >= cnt[li[i*2+1]] else li[i*2+1] for i in range(4)]
+    return "".join(ans)
+
+
+print(solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5])) # TCMA
+print(solution(["TR", "RT", "TR"], [7, 1, 3])) # RCJA
