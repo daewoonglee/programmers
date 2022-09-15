a77a414f738160242c8a5079be96e7dc854d97e7
daewoong lee <dw.lee832@gmail.com>
2022-09-15 10:02:48 +0900

    멀리 뛰기 문제풀이 01 - code refactoring

--- lev2/멀리 뛰기.py
+++ lev2/멀리 뛰기.py
@@ -1,13 +1,28 @@
 def solution(n):
-    ans = [1, 1]
-    idx = 0
-    for i in range(n-1):
-        idx = i%2
-        ans[idx] += ans[idx-1]
-    return ans[idx] % 1234567
+    # 1.920356184
+    # ans = [1, 1]
+    # idx = 0
+    # for i in range(n-1):
+    #     idx = i%2
+    #     ans[idx] += ans[idx-1]
+    # return ans[idx] % 1234567
+
+    # code refactoring - 0.82522538
+    if n == 1: return 1
+    a, b = 1, 2
+    for i in range(2, n):
+        a, b = b, a+b
+    return b % 1234567
 
 
 print(solution(4))
 print(solution(5))
 print(solution(6))
 print(solution(7))
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [4, 5, 6, 7, 100, 1000]
+    t = Timer(f"for t in {query}: solution(t)", "from __main__ import solution")
+    print(t.timeit(number=10000))
