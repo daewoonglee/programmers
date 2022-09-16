140758df34f52b4adee402c31ae55676e4d5887b
daewoong lee <dw.lee832@gmail.com>
2022-09-16 14:45:37 +0900

    줄 서는 방법 문제풀이 01 - code refactoring

--- lev2/줄 서는 방법.py
+++ lev2/줄 서는 방법.py
@@ -1,23 +1,23 @@
 import math
-from itertools import permutations
 
 
 def solution(n, k):
     li = [i for i in range(1, n+1)]
+    # code refactoring - # 0.363528173
+    f = 1
+    for num in li: f *= num
     ans = []
     while n:
-        f = math.factorial(n)
+        # f = math.factorial(n) # 0.366991436
         g = f // n
-        q, d = (k-1) // g, k % g
-        print(f"before n: {n}, f: {f}, k: {k}, g: {g}, q: {q}, d: {d}")
+        q = (k-1) // g
         ans.append(li.pop(q))
+        f //= n
+        k %= g
         n -= 1
-        k = d
 
     return ans
 
-
-    # return [q] + list(list(permutations([i for i in range(1, n+1) if i != q], n-1))[d])
 
 print(solution(3, 1))
 print(solution(3, 2))
@@ -26,3 +26,10 @@ print(solution(3, 4))
 print(solution(3, 5))
 print(solution(4, 5))
 print(solution(4, 10))
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [[3,1],[3,2],[3,3],[3,4],[3,5],[4,5],[4,10],[20,243290200817664000],[18,640237370572800]]
+    t = Timer(f"for t in {query}: solution(*t)", "from __main__ import solution")
+    print(t.timeit(number=10000))
