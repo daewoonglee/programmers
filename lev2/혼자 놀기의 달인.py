06df23c4250768e15f12105b492b3e9e7f4c0620
idaeung <dw.lee832@gmail.com>
2022-10-11 22:04:03 +0900

    혼자 놀기의 달인 문제풀이 02 - 풀이O (테스트 10/10)

--- lev2/혼자 놀기의 달인.py
+++ lev2/혼자 놀기의 달인.py
@@ -1,23 +1,19 @@
 def solution(cards):
     ans = []
-    idx = 0
     pts = 0
-    N = len(cards)
-    while idx < N:
+    for i in range(len(cards)):
         cnt = 0
         while cards[pts]:
             cnt += 1
             temp = cards[pts]
             cards[pts] = 0
-            pts = temp-1
-        idx += 1
-        ans.append(cnt)
-        pts = idx-1
-    print(ans)
+            pts = temp - 1
+        if cnt: ans.append(cnt)
+        pts = i-1
+
     ans.sort()
-    if not ans: return 0
-    elif len(ans) > 1: return ans[-1] * ans[-2]
-    else: return ans
+    return 0 if len(ans) < 2 else ans[-1] * ans[-2]
 
 
-print(solution([8,6,3,7,2,5,1,4]))
+print(solution([8,6,3,7,2,5,1,4])) # 12
+print(solution([2,1])) # 0
