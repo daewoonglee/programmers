a9931aeea7b6f53e95279574c20dc5d542e337b9
idaeung <dw.lee832@gmail.com>
2022-10-14 20:35:15 +0900

    할인 행사 문제풀이 02 - 풀이O (테스트 12/12)

--- lev2/할인 행사.py
+++ lev2/할인 행사.py
@@ -1,6 +1,7 @@
 def solution(want, number, discount):
     def is_same():
-        print(f"dk: {d_keys}, d_dict: {d_dict}")
+        # print(f"dk: {d_keys}, d_dict: {d_dict}")
+        if len(w_keys) != len(d_keys): return 0
         for wk, dk in zip(w_keys, d_keys):
             if not(wk == dk and w_dict[wk] <= d_dict[dk]): return 0
         return 1
@@ -8,7 +9,7 @@ def solution(want, number, discount):
     ans = 0
     w_dict = {k: v for k, v in zip(want, number)}
     w_keys = sorted(w_dict)
-    print(f"wk: {w_keys}, w_dict: {w_dict}")
+    # print(f"wk: {w_keys}, w_dict: {w_dict}")
     d_dict = dict()
     for i in range(10):
         if discount[i] not in d_dict: d_dict[discount[i]] = 1
@@ -29,4 +30,4 @@ def solution(want, number, discount):
 
 # print(solution(["banana", "apple", "rice", "pork", "pot"], [3, 2, 2, 2, 1], ["chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"])) # 3
 # print(solution(["apple"], [10], ["banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana"])) # 0
-print(solution(["a"], [10], ["a"] * 12))
+print(solution(["a", "aa"], [8, 2], ["aa"] + ["a"]*10 + ["aa", "aa"] + ["a"]*10))
