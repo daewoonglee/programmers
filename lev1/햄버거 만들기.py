03a8006929451a59635944448ed3fe8f82e3a8de
daewoong lee <dw.lee832@gmail.com>
2022-11-04 07:59:13 +0900

    햄버거 만들기 문제풀이 03 - 풀이O (테스트 17/17)

--- lev1/햄버거 만들기.py
+++ lev1/햄버거 만들기.py
@@ -1,29 +1,24 @@
 def solution(ingredient):
     ans = 0
-    packing = [[i, igd] for i, igd in enumerate(ingredient[:3])]
-    idx = 2
-    N = len(ingredient)-1
-    while 0 <= idx < N:
-        print(f"idx: {idx}, pack; {packing}, igds: {ingredient}, N: {N}")
-        if packing[0][1] == 1 and packing[1][1] == 2 and packing[2][1] == 3:
-            for j, _ in packing[::-1]: ingredient.pop(j)
-            idx -= (5 if idx > 5 else idx)
-            N -= 3
-            packing = [[idx+i, igd] for i, igd in enumerate(ingredient[idx:idx+3])]
+    stack = []
+    for igd in ingredient:
+        stack.append(igd)
+        # print(stack)
+        if len(stack) >= 4 and stack[-1] == 1 and stack[-2] == 3 and stack[-3] == 2 and stack[-4] == 1:
+            [stack.pop() for _ in range(4)]
             ans += 1
-        else:
-            idx += 1
-            packing[0] = packing[1]
-            packing[1] = packing[2]
-            packing[2] = [idx, ingredient[idx]]
-    print(f"after idx: {idx}, pack; {packing}, igds: {ingredient}, N: {N}, ans: {ans}")
+    if len(stack) >= 4 and stack[-1] == 1 and stack[-2] == 3 and stack[-3] == 2 and stack[-4] == 1:
+        [stack.pop() for _ in range(4)]
+        ans += 1
     return ans
 
 
 # print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1])) # 2
-# print(solution([1, 2, 1, 2, 3, 1, 2, 3, 3])) # 3
-# print(solution([1, 1, 2, 1, 2, 3, 3, 2, 3, 3])) # 3
-print(solution([1, 1, 1, 1, 1, 2, 3, 2, 3, 2, 3, 2, 3, 1, 2, 3, 3, 1, 2, 3, 3, 3, 3, 3])) # 5
-print(solution([1, 2, 3] * 100)) # 5
-print(solution([1, 1, 2, 1, 2, 1, 2, 3, 3, 3, 2, 3])) # 4
-print(solution([1, 1, 1, 2, 3, 2, 3, 2, 3])) # 3
+# print(solution([1, 2, 1, 2, 3, 1, 2, 3, 3])) # 1
+# print(solution([1, 1, 2, 1, 2, 3, 3, 2, 3, 3])) # 0
+# print(solution([1, 1, 1, 1, 1, 2, 3, 2, 3, 2, 3, 2, 3, 1, 2, 3, 3, 1, 2, 3, 3, 3, 3, 3])) # 0
+# print(solution([1, 2, 3] * 100)) # 50
+# print(solution([1, 1, 2, 1, 2, 1, 2, 3, 3, 3, 2, 3])) # 0
+# print(solution([1, 1, 1, 2, 3, 2, 3, 2, 3])) # 0
+# print(solution([3, 2, 1, 3, 2, 1, 3, 2, 3, 2, 3, 1])) # 0
+print(solution([1,2,3,1,2,3,1,1,1,1,2,3,1,2,3,1])) # 3
