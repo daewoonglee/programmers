4342bd027e74c51f5859e8a3be19ea239316d614
daewoong lee <dw.lee832@gmail.com>
2022-11-04 08:06:10 +0900

    햄버거 만들기 문제풀이 03 - code refactoring

--- lev1/햄버거 만들기.py
+++ lev1/햄버거 만들기.py
@@ -3,22 +3,34 @@ def solution(ingredient):
     stack = []
     for igd in ingredient:
         stack.append(igd)
-        # print(stack)
-        if len(stack) >= 4 and stack[-1] == 1 and stack[-2] == 3 and stack[-3] == 2 and stack[-4] == 1:
+        # if len(stack) >= 4 and stack[-1] == 1 and stack[-2] == 3 and stack[-3] == 2 and stack[-4] == 1: # 1.018187671
+        if stack[-4:] == [1,2,3,1]: # 1.077183271
             [stack.pop() for _ in range(4)]
             ans += 1
-    if len(stack) >= 4 and stack[-1] == 1 and stack[-2] == 3 and stack[-3] == 2 and stack[-4] == 1:
-        [stack.pop() for _ in range(4)]
-        ans += 1
     return ans
 
 
-# print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1])) # 2
-# print(solution([1, 2, 1, 2, 3, 1, 2, 3, 3])) # 1
-# print(solution([1, 1, 2, 1, 2, 3, 3, 2, 3, 3])) # 0
-# print(solution([1, 1, 1, 1, 1, 2, 3, 2, 3, 2, 3, 2, 3, 1, 2, 3, 3, 1, 2, 3, 3, 3, 3, 3])) # 0
-# print(solution([1, 2, 3] * 100)) # 50
-# print(solution([1, 1, 2, 1, 2, 1, 2, 3, 3, 3, 2, 3])) # 0
-# print(solution([1, 1, 1, 2, 3, 2, 3, 2, 3])) # 0
-# print(solution([3, 2, 1, 3, 2, 1, 3, 2, 3, 2, 3, 1])) # 0
+print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1])) # 2
+print(solution([1, 2, 1, 2, 3, 1, 2, 3, 3])) # 1
+print(solution([1, 1, 2, 1, 2, 3, 3, 2, 3, 3])) # 0
+print(solution([1, 1, 1, 1, 1, 2, 3, 2, 3, 2, 3, 2, 3, 1, 2, 3, 3, 1, 2, 3, 3, 3, 3, 3])) # 0
+print(solution([1, 2, 3] * 100)) # 50
+print(solution([1, 1, 2, 1, 2, 1, 2, 3, 3, 3, 2, 3])) # 0
+print(solution([1, 1, 1, 2, 3, 2, 3, 2, 3])) # 0
+print(solution([3, 2, 1, 3, 2, 1, 3, 2, 3, 2, 3, 1])) # 0
 print(solution([1,2,3,1,2,3,1,1,1,1,2,3,1,2,3,1])) # 3
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [[2, 1, 1, 2, 3, 1, 2, 3, 1],
+             [1, 2, 1, 2, 3, 1, 2, 3, 3],
+             [1, 1, 2, 1, 2, 3, 3, 2, 3, 3],
+             [1, 1, 1, 1, 1, 2, 3, 2, 3, 2, 3, 2, 3, 1, 2, 3, 3, 1, 2, 3, 3, 3, 3, 3],
+             [1, 2, 3] * 100,
+             [1, 1, 2, 1, 2, 1, 2, 3, 3, 3, 2, 3],
+             [1, 1, 1, 2, 3, 2, 3, 2, 3],
+             [3, 2, 1, 3, 2, 1, 3, 2, 3, 2, 3, 1],
+             [1,2,3,1,2,3,1,1,1,1,2,3,1,2,3,1]]
+    t = Timer(f"for t in {query}: solution(t)", "from __main__ import solution")
+    print(t.timeit(number=10000))
