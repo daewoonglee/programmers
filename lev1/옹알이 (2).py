37cc484be0e28e6d33afcf4ddac070ebe2671409
daewoong lee <dw.lee832@gmail.com>
2022-11-11 07:18:13 +0900

    옹알이 (2) 문제풀이 02 - 풀이O (테스트 20/20)

--- lev1/옹알이 (2).py
+++ lev1/옹알이 (2).py
@@ -1,23 +1,25 @@
 def solution(babbling):
     pron = {
-        "a": ["aya", 3, 0],
-        "y": ["ye", 2, 1],
-        "w": ["woo", 3, 2],
-        "m": ["ma", 2, 3]
+        "a": ["aya", 3],
+        "y": ["ye", 2],
+        "w": ["woo", 3],
+        "m": ["ma", 2]
     }
     ans = 0
     for bbg in babbling:
         idx = 0
-        check = [0] * 4
+        check = ""
         while idx < len(bbg):
             ch = bbg[idx]
-            if ch in pron and not check[pron[ch][2]] and bbg[idx: idx+pron[ch][1]] == pron[ch][0]:
+            if ch in pron and check != pron[ch][0] and bbg[idx: idx+pron[ch][1]] == pron[ch][0]:
                 idx += pron[ch][1]
-                check[pron[ch][2]] = 1
+                check = pron[ch][0]
             else: break
         else: ans += 1
     return ans
 
 
 # print(solution(["aya", "yee", "u", "maa"]))#1
-print(solution(["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"]))#2
+# print(solution(["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"]))#2
+print(solution(["aayaye", "uyeuuye", "yeye", "yemawoo", "ayaayaa", "mawooyeayama"]))#2
+print(solution(["wooyemawooye"]))#1
