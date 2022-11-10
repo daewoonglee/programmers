5b96a9ba516b2854483456381a21a5bf12b4bf4b
daewoong lee <dw.lee832@gmail.com>
2022-11-11 07:11:00 +0900

    옹알이 (2) 문제풀이 01 - 풀이X (테스트 15/20)

---
+++ lev1/옹알이 (2).py
@@ -0,0 +1,23 @@
+def solution(babbling):
+    pron = {
+        "a": ["aya", 3, 0],
+        "y": ["ye", 2, 1],
+        "w": ["woo", 3, 2],
+        "m": ["ma", 2, 3]
+    }
+    ans = 0
+    for bbg in babbling:
+        idx = 0
+        check = [0] * 4
+        while idx < len(bbg):
+            ch = bbg[idx]
+            if ch in pron and not check[pron[ch][2]] and bbg[idx: idx+pron[ch][1]] == pron[ch][0]:
+                idx += pron[ch][1]
+                check[pron[ch][2]] = 1
+            else: break
+        else: ans += 1
+    return ans
+
+
+# print(solution(["aya", "yee", "u", "maa"]))#1
+print(solution(["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"]))#2
