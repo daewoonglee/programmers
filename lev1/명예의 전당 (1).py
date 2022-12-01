22a6ae14b779f45fa6c8c62bb391db00e37c5a5a
daewoong lee <dw.lee832@gmail.com>
2022-12-02 08:44:23 +0900

    명예의 전당 (1) 문제풀이 01 - code refactoring

--- lev1/명예의 전당 (1).py
+++ lev1/명예의 전당 (1).py
@@ -1,9 +1,8 @@
 def solution(k, score):
+    # 0.524096262
     N = k if k < len(score) else len(score)
     ans = [score[0]]
-    for s in score[1:N]:
-        if s < ans[-1]: ans.append(s)
-        else: ans.append(ans[-1])
+    for s in score[1:N]: ans.append(s if s < ans[-1] else ans[-1])
     rank = sorted(score[:N], reverse=True)
     # print(ans)
     for s in score[N:]:
@@ -19,3 +18,15 @@ print(solution(4, [0, 300, 40, 300, 20, 70, 150, 50, 500, 1000])) # [0, 0, 0, 0,
 print(solution(100, [0, 1000, 0, 0, 0, 0, 110, 0, 10, 1])) # [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 print(solution(3, [0, 1000, 0, 0, 0, 0, 110, 0, 10, 1])) # [0, 0, 0, 0, 0, 0, 0, 0, 10, 10]
 print(solution(100, [100, 1000, 10, 1, 0, 0, 110, 0, 10, 1])) # [100, 100, 10, 1, 0, 0, 0, 0, 0, 0]
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [[3, [10, 100, 20, 150, 1, 100, 200]],
+             [4, [0, 300, 40, 300, 20, 70, 150, 50, 500, 1000]],
+             [100, [0, 1000, 0, 0, 0, 0, 110, 0, 10, 1]],
+             [3, [0, 1000, 0, 0, 0, 0, 110, 0, 10, 1]],
+             [100, [100, 1000, 10, 1, 0, 0, 110, 0, 10, 1]],
+             [100, [100, 1000, 10, 1, 0, 0, 110, 0, 10, 1] * 100]]
+    t = Timer(f"for t in {query}: solution(*t)", "from __main__ import solution")
+    print(t.timeit(number=1000))
