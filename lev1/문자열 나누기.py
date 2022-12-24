5c7cb1bfeb1dc8f887e2f9de0fc067f32b5c7e7c
daewoong lee <dw.lee832@gmail.com>
2022-12-24 11:59:26 +0900

    문자열 나누기 문제풀이 01 - code refactoring

--- lev1/문자열 나누기.py
+++ lev1/문자열 나누기.py
@@ -1,26 +1,47 @@
 def solution(s):
-    ch = ""
-    same, diff = 0, 0
+    # # 0.017090471000000003
+    # ch = ""
+    # same, diff = 0, 0
+    # ans = 0
+    # flag = True
+    # for alpha in s:
+    #     if flag:
+    #         ch = alpha
+    #         same += 1
+    #         flag = False
+    #     else:
+    #         if alpha == ch: same += 1
+    #         else: diff += 1
+    #     if same == diff:
+    #         ans += 1
+    #         same, diff = 0, 0
+    #         flag = True
+    # if same or diff: ans += 1
+    # return ans
+
+    # code refactoring - 0.012088555999999997
     ans = 0
-    flag = True
+    same = 0
+    diff = 0
     for alpha in s:
-        if flag:
-            ch = alpha
-            same += 1
-            flag = False
-        else:
-            if alpha == ch:
-                same += 1
-            else:
-                diff += 1
         if same == diff:
             ans += 1
-            same, diff = 0, 0
-            flag = True
-    if same or diff: ans += 1
+            ch = alpha
+        if alpha == ch: same += 1
+        else: diff += 1
     return ans
 
 
 print(solution("banana")) # 3
 print(solution("abracadabra")) # 6
 print(solution("aaabbaccccabba")) # 3
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = ["banana",
+             "abracadabra",
+             "aaabbaccccabba",
+             "aaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbccccccccccccccccccccccccccccccccc"]
+    t = Timer(f"for t in {query}: solution(t)", "from __main__ import solution")
+    print(t.timeit(number=1000))
