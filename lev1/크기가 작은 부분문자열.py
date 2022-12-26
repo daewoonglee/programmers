d4c32ee6fea9685a9d54d3125230a9cdb629f80b
daewoong lee <dw.lee832@gmail.com>
2022-12-27 08:20:07 +0900

    크기가 작은 부분문자열 문제풀이 01 - code refactoring

--- lev1/크기가 작은 부분문자열.py
+++ lev1/크기가 작은 부분문자열.py
@@ -1,11 +1,30 @@
 def solution(t, p):
     N = len(p)
-    p = int(p)
-    return sum([1 for i in range(len(t)-N+1) if int(t[i:i+N]) <= p])
+    # # compare int version - 11.850301761999999
+    # p = int(p)
+    # return sum([1 for i in range(len(t)-N+1) if int(t[i:i+N]) <= p])
+
+    # compare str version - 5.240226359
+    return sum([1 for i in range(len(t) - N + 1) if t[i:i + N] <= p])
 
 
-# print(solution("3141592", "271")) # 2
-# print(solution("500220839878", "7")) # 8
-# print(solution("10203", "15")) # 3
+print(solution("3141592", "271")) # 2
+print(solution("500220839878", "7")) # 8
+print(solution("10203", "15")) # 3
 print(solution("3000000", "9999")) # 4
 print(solution("9999999", "3333")) # 0
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [["3141592", "271"],
+             ["500220839878", "7"],
+             ["10203", "15"],
+             ["3000000", "9999"],
+             ["9999999", "3333"],
+             ["9"*10000, "3"*18],
+             ["3"*10000, "9"*18],
+             ["3"*10000, "9"*1],
+             ["9"*10000, "3"*1]]
+    t = Timer(f"for t in {query}: solution(*t)", "from __main__ import solution")
+    print(t.timeit(number=1000))
