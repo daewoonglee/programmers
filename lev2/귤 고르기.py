fcce0d46256b37ec62c024d518686ac7b61ef31b
daewoong lee <dw.lee832@gmail.com>
2022-12-30 08:31:26 +0900

    귤 고르기 문제풀이 01 - code refactoring

--- lev2/귤 고르기.py
+++ lev2/귤 고르기.py
@@ -3,8 +3,16 @@ from collections import Counter
 
 def solution(k, tangerine):
     ans = 0
-    values = sorted(Counter(tangerine).values(), key=lambda x: x, reverse=True)
-    for v in values:
+
+    # # 0.265255571
+    # values = sorted(Counter(tangerine).values(), reverse=True)
+
+    # approach idea - 0.551333608
+    values = [0 for _ in range(len(tangerine))]
+    for i in range(len(tangerine)): values[tangerine[i]-1] += 1
+    values.sort(reverse=True)
+
+    for _, v in values:
         if k > 0:
             ans += 1
             k -= v
@@ -15,3 +23,14 @@ def solution(k, tangerine):
 print(solution(6, [1, 3, 2, 5, 4, 5, 2, 3])) # 3
 print(solution(4, [1, 3, 2, 5, 4, 5, 2, 3])) # 2
 print(solution(2, [1, 1, 1, 1, 2, 2, 2, 3])) # 1
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [[6, [1, 3, 2, 5, 4, 5, 2, 3]],
+             [4, [1, 3, 2, 5, 4, 5, 2, 3]],
+             [2, [1, 1, 1, 1, 2, 2, 2, 3]],
+             [1000, [i for i in range(1, 1000)]],
+             [1000, [i for i in range(1, 1000)]*2]]
+    t = Timer(f"for t in {query}: solution(*t)", "from __main__ import solution")
+    print(t.timeit(number=1000))
