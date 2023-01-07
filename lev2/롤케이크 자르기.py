0cecb78a75e866fdf40ab84f11a9482d2e112323
daewoong lee <dw.lee832@gmail.com>
2023-01-07 13:46:31 +0900

    롤케이크 자르기 문제풀이 03 - 풀이O (테스트 20/20)

--- lev2/롤케이크 자르기.py
+++ lev2/롤케이크 자르기.py
@@ -1,19 +1,21 @@
+from collections import Counter
+
+
 def solution(topping):
     ans = 0
+    cnt_dic = Counter(topping)
     piece1 = set()
-    piece2 = set(topping)
+    piece2 = len(cnt_dic)
     for i, t in enumerate(topping[:-1]):
         piece1.add(t)
-        print(f"t: {t}, topiing: {topping[i + 1:]}")
-        if t not in topping[i+1:]:
-            piece2.remove(t)
-            print(f"rmv p2: {piece2}, p1: {piece1}")
-        if len(piece1) == len(piece2):
-            ans += 1
+        cnt_dic[t] -= 1
+        if cnt_dic[t] == 0:
+            piece2 -= 1
+        if len(piece1) == piece2: ans += 1
     return ans
 
 
-# print(solution([1, 2, 1, 3, 1, 4, 1, 2])) # 2
-# print(solution([1, 2, 3, 1, 4])) # 0
-# print(solution([1, 2, 3, 4])) # 1
+print(solution([1, 2, 1, 3, 1, 4, 1, 2])) # 2
+print(solution([1, 2, 3, 1, 4])) # 0
+print(solution([1, 2, 3, 4])) # 1
 print(solution([1])) # 0
