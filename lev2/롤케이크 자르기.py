cb42f81f0186c55177064abaec240a4d8094fa7f
daewoong lee <dw.lee832@gmail.com>
2023-01-07 13:34:07 +0900

    롤케이크 자르기 문제풀이 02 - 풀이X (테스트 5/20)

--- lev2/롤케이크 자르기.py
+++ lev2/롤케이크 자르기.py
@@ -1,15 +1,19 @@
 def solution(topping):
     ans = 0
     piece1 = set()
-    piece2 = set(topping[1:])
+    piece2 = set(topping)
     for i, t in enumerate(topping[:-1]):
         piece1.add(t)
-        piece2 = set(topping[i+1:])
+        print(f"t: {t}, topiing: {topping[i + 1:]}")
+        if t not in topping[i+1:]:
+            piece2.remove(t)
+            print(f"rmv p2: {piece2}, p1: {piece1}")
         if len(piece1) == len(piece2):
             ans += 1
     return ans
 
 
-print(solution([1, 2, 1, 3, 1, 4, 1, 2])) # 2
-print(solution([1, 2, 3, 1, 4])) # 0
+# print(solution([1, 2, 1, 3, 1, 4, 1, 2])) # 2
+# print(solution([1, 2, 3, 1, 4])) # 0
+# print(solution([1, 2, 3, 4])) # 1
 print(solution([1])) # 0
