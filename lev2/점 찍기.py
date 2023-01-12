daf6d343c6f962b8a6c1a12e44947765e81407eb
daewoong lee <dw.lee832@gmail.com>
2023-01-12 18:21:44 +0900

    점 찍기 문제풀이 03 - code refactoring

--- lev2/점 찍기.py
+++ lev2/점 찍기.py
@@ -1,11 +1,12 @@
 def solution(k, d):
     ans = 0
     for x in range(0, d + 1, k):
-        y = ((d ** 2 - x ** 2) ** 0.5)
-        print(f"x: {x}, y: {y}, dist: {(x ** 2 + y ** 2) ** 0.5}")
+        # y = ((d ** 2 - x ** 2) ** 0.5) # 6.832716927
+        y = ((d*d - x*x) ** 0.5) # 3.111694371
+        # print(f"x: {x}, y: {y}, dist: {(x ** 2 + y ** 2) ** 0.5}")
         ans += y // k + 1
         """
-        {(x*k)**2 + (y*k)**2}**0.5 <= d 
+        {(x*k)**2 + (y*k)**2}**0.5 <= d
         = {X + Y}**0.5 <= d*d**0.5 # X=(x*k)**2, Y=(y*k)**2
         = X+Y <= d*d
         = Y <= d*d-X
@@ -15,6 +16,19 @@ def solution(k, d):
     return ans
 
 
+
 print(solution(2, 4)) # 6
 print(solution(1, 5)) # 26
 print(solution(5, 9)) # 4
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [
+        [2, 4],
+        [1, 5],
+        [5, 9],
+        [1, 1000000]
+    ]
+    t = Timer(f"for t in {query}: solution(*t)", "from __main__ import solution")
+    print(t.timeit(number=10))
