ad844bb1a40ebcc0a09009c2611ec61abc9b40c6
daewoong lee <dw.lee832@gmail.com>
2023-01-12 18:14:13 +0900

    점 찍기 문제풀이 03 - 풀이O (테스트 16/16)

--- lev2/점 찍기.py
+++ lev2/점 찍기.py
@@ -1,15 +1,20 @@
 def solution(k, d):
     ans = 0
-    for x in range(0, d+1, k):
-        for y in range(d//k+1, -1, -1):
-            print(f"x: {x}, y: {y}, y': {y*k}, dist: {(x**2+y**2)**0.5}")
-            if (x**2 + (y*k)**2)**0.5 <= d:
-                ans += y + 1
-                break
+    for x in range(0, d + 1, k):
+        y = ((d ** 2 - x ** 2) ** 0.5)
+        print(f"x: {x}, y: {y}, dist: {(x ** 2 + y ** 2) ** 0.5}")
+        ans += y // k + 1
+        """
+        {(x*k)**2 + (y*k)**2}**0.5 <= d 
+        = {X + Y}**0.5 <= d*d**0.5 # X=(x*k)**2, Y=(y*k)**2
+        = X+Y <= d*d
+        = Y <= d*d-X
+        = {(y*k)**2} <= d*d - (x*k)**2
+        = {(y*k)**2}**0.5 <= {d*d - (x*k)**2}**0.5
+        """
     return ans
 
 
 print(solution(2, 4)) # 6
 print(solution(1, 5)) # 26
 print(solution(5, 9)) # 4
-
