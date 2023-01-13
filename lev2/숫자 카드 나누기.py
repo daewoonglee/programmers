68fe0b44d8a1deaaa935f273d9657aaccb37e3a9
daewoong lee <dw.lee832@gmail.com>
2023-01-13 17:42:25 +0900

    숫자 카드 나누기 문제풀이 02 - 풀이O (테스트 36/36)

--- lev2/숫자 카드 나누기.py
+++ lev2/숫자 카드 나누기.py
@@ -1,20 +1,22 @@
 def solution(arrayA, arrayB):
     def get_intersection(arr):
+        def check_is_intersection(num):
+            for sa in sorted_arr[1:]:
+                if sa % num != 0: return -1
+            return num
+
+        sorted_arr = sorted(arr)
+        n = sorted_arr[0] # min num
         divisor = []
-        for a in arr:
-            local_divisor = set()
-            for i in range(2, int(a**0.5)+1):
-                if a % i == 0:
-                    local_divisor.add(i)
-                    local_divisor.add(a//i)
-            local_divisor.add(a)
-            divisor.append(local_divisor)
-            # print(f"a: {a}, div: {local_divisor}")
-        # print(f"a_div: {a_divisor}")
-        inter = divisor[0]
-        for a in divisor[1:]:
-            inter = inter.intersection(a)
-        return sorted(inter)[::-1]
+        for i in range(2, int(n**0.5)+1):
+            if n % i == 0:
+                if check_is_intersection(i) != -1:
+                    divisor.append(i)
+                if check_is_intersection(n//i) != -1:
+                    divisor.append(n//i)
+        if check_is_intersection(n) != -1:
+            divisor.append(n)
+        return sorted(divisor)[::-1]
 
     def get_integer(inter_list, arr):
         integer = 0
@@ -28,6 +30,7 @@ def solution(arrayA, arrayB):
 
     a_inter = get_intersection(arrayA)
     b_inter = get_intersection(arrayB)
+
     a_integer = get_integer(a_inter, arrayB)
     b_integer = get_integer(b_inter, arrayA)
     return max(a_integer, b_integer)
