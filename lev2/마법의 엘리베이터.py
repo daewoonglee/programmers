18e2f6f2ca38ac1d5732778f95b3a602ac8b6de2
daewoong lee <dw.lee832@gmail.com>
2023-01-15 15:27:46 +0900

    마법의 엘리베이터 문제풀이 01 - code refactoring

--- lev2/마법의 엘리베이터.py
+++ lev2/마법의 엘리베이터.py
@@ -1,17 +1,31 @@
 def solution(storey):
+    # # 0.239197214 - convert str
+    # ans = 0
+    # rnd = 0
+    # storey = str(storey)[::-1]
+    # for i, n in enumerate(storey):
+    #     q = (int(n) + rnd)
+    #     if q >= 5: # up
+    #         ans += 10-q
+    #         # 5 자릿수 앞에 0~4이거나 5앞에 수가 없으면 0(down) 그렇지 않으면 1(up)
+    #         rnd = 0 if q == 5 and (i+1 == len(storey) or int(storey[i+1]) < 5) else 1
+    #     else: # down
+    #         ans += q
+    #         rnd = 0
+    #     # print(f"q: {q}, rnd: {rnd}, ans: {ans}")
+    # return ans + rnd
+
+    # code refactoring - # 0.107456529
     ans = 0
-    rnd = 0
-    storey = str(storey)[::-1]
-    for i, n in enumerate(storey):
-        q = (int(n) + rnd)
-        if q >= 5: # up
-            ans += 10-q
-            rnd = 0 if q == 5 and (i+1 == len(storey) or int(storey[i+1]) < 5) else 1
+    while storey > 0:
+        storey, n = divmod(storey, 10)
+        if n >= 5: # up
+            ans += 10-n
+            if not (n == 5 and storey % 10 < 5):
+                storey += 1
         else: # down
-            ans += q
-            rnd = 0
-        # print(f"q: {q}, rnd: {rnd}, ans: {ans}")
-    return ans + rnd
+            ans += n
+    return ans
 
 
 print(solution(16)) # 6
@@ -43,3 +57,10 @@ print(solution(7)) # 4 (up)
 print(solution(8)) # 3 (up)
 print(solution(9)) # 2 (up)
 print(solution(10)) # 1 (up)
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [16, 2554, 2054, 44, 45, 46, 54, 55, 56, 455, 555, 64, 65, 66, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100000000]
+    t = Timer(f"for t in {query}: solution(t)", "from __main__ import solution")
+    print(t.timeit(number=10000))
