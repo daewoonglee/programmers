d322826435fcb2a3d182acb3816b596fb747ca7e
daewoong lee <dw.lee832@gmail.com>
2023-01-16 10:49:07 +0900

    디펜스 게임 문제풀이 02 - 풀이X, 시간초과 (테스트 30/32)

--- lev2/디펜스 게임.py
+++ lev2/디펜스 게임.py
@@ -1,21 +1,26 @@
-def defence(n, k, enemy, rnd):
-    if n < 0:
-        # print(f"BREAK !!! n < 0, n: {n}, k: {k}, enemy: {enemy}, rnd: {rnd}\n")
-        return rnd-1
-    elif rnd >= len(enemy):
-        return rnd
-
-    # print(f"n: {n}, k: {k}, enemy: {enemy}, rnd: {rnd}")
-    return max(defence(n-enemy[rnd], k, enemy, rnd + 1), defence(n, k-1, enemy, rnd+1) if k > 0 else rnd)
-
-
 def solution(n, k, enemy):
-    game_round = 0
-    return max(defence(n-enemy[game_round], k, enemy, game_round+1), defence(n, k-1, enemy, game_round+1))
+    invincibility = sorted(enemy[:k])
+    while n > 0 and k < len(enemy):
+        if enemy[k] > invincibility[0]:
+            n -= invincibility[0]
+            invincibility[0] = enemy[k]
+            invincibility.sort()
+        else:
+            n -= enemy[k]
+        k += 1
+
+    if k > len(enemy):
+        return len(enemy)
+    if n >= 0:
+        return k
+    else:
+        return k-1
 
 
 print(solution(7, 3, [4, 2, 4, 5, 3, 3, 1])) # 5
+print(solution(7, 3, [4, 2, 1, 3, 1, 4, 5, 3])) # 7
 print(solution(7, 3, [4, 4, 4, 5, 2, 1, 1])) # 6
 print(solution(7, 3, [2, 4, 3, 3, 5, 4, 3])) # 5
 print(solution(7, 3, [4, 2, 3, 2, 5, 3, 6])) # 6
 print(solution(2, 4, [3, 3, 3, 3])) # 4
+print(solution(13, 5, [3, 3, 3, 3])) # 4
