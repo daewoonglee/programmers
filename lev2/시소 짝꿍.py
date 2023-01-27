d78ee71587356a04ec726e0b4f378263f9e52277
daewoong lee <dw.lee832@gmail.com>
2023-01-27 11:51:51 +0900

    시소 짝궁 문제풀이 04 - code refactoring

--- lev2/시소 짝꿍.py
+++ lev2/시소 짝꿍.py
@@ -4,12 +4,19 @@ from collections import Counter
 def solution(weights):
     w_dict = Counter(weights)
     dist = [2/3, 2, 3/4]
-    ans = 0
-    for w, v in w_dict.items():
-        if v > 1:
-            ans += (v - 1) * v // 2
-        ans += sum([w_dict[w*d]*v for d in dist if w*d in w_dict])
-    return ans
+    # ans = 0
+    # for w, v in w_dict.items():
+    #     # 0.377581197
+    #     if v > 1:
+    #         ans += (v - 1) * v // 2
+    #     ans += sum([w_dict[w*d]*v for d in dist if w*d in w_dict])
+    #
+    #     # # without if - 0.357757928
+    #     # ans += sum([w_dict[w*d]*v for d in dist if w*d in w_dict]) + (v - 1) * v // 2
+    # return ans
+
+    # # code refactoring, comprehension - 0.36814881499999996
+    return sum([sum([w_dict[w*d]*v for d in dist if w*d in w_dict]) + (v - 1) * v // 2 for w, v in w_dict.items()])
 
 
 # print(solution([100,180,360,100,270])) # 4
@@ -22,3 +29,16 @@ def solution(weights):
 # print(solution([100,100,100,100,100,100])) # 15
 # print(solution([100,110,300,200])) # 2
 print(solution([100,200,100,200,100,200])) # 15
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [
+        [100,180,360,100,270],
+        [100,110],
+        [100,100,100,100,100,100],
+        [100,200,100,200,100,200],
+        [100,200,100,200,100,200]*100,
+    ]
+    t = Timer(f"for t in {query}: solution(t)", "from __main__ import solution")
+    print(t.timeit(number=10000))
