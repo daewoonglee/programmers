c07233d3db1e3e6623e52ecd9247cebe3b61b75d
daewoong lee <dw.lee832@gmail.com>
2023-01-30 14:39:48 +0900

    숫자 변환하기 문제풀이 02 - 풀이O (테스트 16/16)

--- lev2/숫자 변환하기.py
+++ lev2/숫자 변환하기.py
@@ -1,22 +1,19 @@
 def solution(x, y, n):
-    def conv_digit(x1, depth):
-        if x1 == y:
-            return depth
-        elif x1 > y:
+    def conv_digit(nums, depth): # dfs -> bfs 수정
+        res = set()
+        for num in nums:
+            if num == y: return depth
+            res.add(num+n)
+            res.add(num*2)
+            res.add(num*3)
+
+        if all([1 if r > y else 0 for r in res]):
             return -1
+        return conv_digit(res, depth+1)
 
-        for num in sorted([conv_digit(x1+n, depth+1), conv_digit(x1*2, depth+1), conv_digit(x1*3, depth+1)]):
-            if num != -1:
-                return num
-        return -1
+    if x == y: return 0
+    return conv_digit([x+n, x*2, x*3], 1)
 
-    return conv_digit(x, 0)
-
-"""
-+n
-*2
-*3
-"""
 
 # print(solution(10, 40, 5)) # 2
 # print(solution(9, 40, 1)) # 3
