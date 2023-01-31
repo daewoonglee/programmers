67540683987498c5292bd4d309f6bfc28f45dd06
daewoong lee <dw.lee832@gmail.com>
2023-01-31 16:53:03 +0900

    뒤에 있는 큰 수 찾기 문제풀이 02 - 풀이O (테스트 23/23)

--- lev2/뒤에 있는 큰 수 찾기.py
+++ lev2/뒤에 있는 큰 수 찾기.py
@@ -1,16 +1,19 @@
 def solution(numbers):
     N = len(numbers)
-    ans = []
-    for i, n in enumerate(numbers):
-        for j in range(i+1, N):
-            if n < numbers[j]:
-                ans.append(numbers[j])
-                break
-        else: ans.append(-1)
+    ans = [-1] * N
+    idxs = [[0, numbers[0]]]
+    for i in range(1, N):
+        while idxs and idxs[-1][1] < numbers[i]:
+            ans[idxs.pop()[0]] = numbers[i]
+        idxs.append([i, numbers[i]])
     return ans
 
 
-print(solution([2, 3, 3, 5])) # [3, 5, 5, -1]
-print(solution([9, 1, 5, 3, 6, 2])) # [-1, 5, 6, 6, -1, -1]
-print(solution([1, 2, 3, 4])) # [2,3,4,-1]
-print(solution([4, 3, 2, 1])) # [-1,-1,-1,-1]
+# print(solution([2, 3, 3, 5])) # [3, 5, 5, -1]
+# print(solution([9, 1, 5, 3, 6, 2])) # [-1, 5, 6, 6, -1, -1]
+# print(solution([9, 1, 5, 4, 3, 2, 1, 6, 2])) # [-1, 5, 6, 6, 6, 6, 6, -1, -1]
+print(solution([1, 2, 3, 1, 2, 3, 1, 2, 3, 10])) # [2,3,10,2,3,10,2,3,10,-1]
+print(solution([1, 2, 3, 1, 1, 2, 4, 2, 3, 10])) # [2,3,4,2,2,4,10,3,10,-1]
+# print(solution([1, 1, 1, 1, 1, 1, 1, 1, 1])) # [-1,-1,-1,-1,-1,-1,-1,-1,-1]
+# print(solution([1, 2, 3, 4])) # [2,3,4,-1]
+# print(solution([4, 3, 2, 1])) # [-1,-1,-1,-1]
