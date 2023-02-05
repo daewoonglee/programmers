a2af0dfa1d314189ab00aa59a862b7a982d805f4
daewoong lee <dw.lee832@gmail.com>
2023-02-06 08:39:12 +0900

    유사 칸토어 비트열 문제풀이 02 - 풀이X, 시간초과 (테스트 10/15)

--- lev2/유사 칸토어 비트열.py
+++ lev2/유사 칸토어 비트열.py
@@ -1,9 +1,14 @@
 def solution(n, l, r):
-    cantor = {"1": "11011", "0": "00000"}
-    bit = "1"
-    for _ in range(n):
-        bit = "".join([cantor[b] for b in bit])
+    bit = "11011"
+    for _ in range(n-1):
+        bit = bit*2 + "0"*len(bit) + bit*2
+    print(bit, len(bit))
     return bit[l-1: r].count("1")
 
 
+print(solution(1, 4, 5)) # 8
 print(solution(2, 4, 17)) # 8
+print(solution(3, 4, 17)) # 8
+print(solution(4, 4, 17)) # 8
+print(solution(5, 4, 17)) # 8
+# print(solution(20, 4, 17)) # 8
