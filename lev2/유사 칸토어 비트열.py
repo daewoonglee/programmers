4c572abc4e49c768dbf5309e9dadd553ff9a8a24
daewoong lee <dw.lee832@gmail.com>
2023-02-09 06:56:02 +0900

    유사 칸토어 비트열 문제풀이 05 - 풀이O (테스트 15/15)

--- lev2/유사 칸토어 비트열.py
+++ lev2/유사 칸토어 비트열.py
@@ -5,9 +5,8 @@ def solution(n, l, r):
     # return bit[l - 1: r].count("1")
 
     def cnt_cantor(N, d):
-        idx = [4, 1, 2, 2, 3]
+        plus = [0, 1, 2, 2, 3]
         cnt = 0
-        print(f"start: {d}")
         while N > 1:
             bit = 4**N // 4
             length = 5**(N-1)
@@ -17,16 +16,11 @@ def solution(n, l, r):
             print(f"whille loop n: {N}, bit: {bit}, len: {length}, q: {q}, d: {d}, cnt: {cnt}")
             N -= 1
 
-        """
-        L: 1 2 3 4 0 -> 1 2 2 3 4
-        R: 1 2 3 4 0 -> 1 2 2 3 4
-        """
-        if q == 0 and d == 0:
-            return cnt
-        elif q != 2:
-            return cnt + idx[d]
+            if q == 2: break
         else:
-            return cnt
+            cnt += plus[d]
+
+        return cnt
 
     if n == 1:
         return "11011"[l-1:r].count("1")
@@ -46,9 +40,16 @@ L <= cantor <= R 범위 값을 구하려고하면 예외 상황이 너무 많이
             > 다른 그룹 내 서브에서 같은 그룹인 경우 등 (실제 같은 그룹인게 아닌 가르키는 그룹 인덱스가 같음)
             
 0<=cantor<=R - 0<=cantor<=L 로 하면 위 예외상황을 모두 무시할 수 있음 
+단, 00000 00000 00000 00000 00000 구간에 들어간 경우 판단 필요, 어떻게 처리?
 ------------------------------------------------------------------------
 1 2 3 4 5  6 7 8 9 10   11 12 13 14 15   16 17 18 19 20   21 22 23 24 25 (1-base)
 1 1 0 1 1  1 1 0 1  1    0  0  0  0  0    1  1  0  1  1    1  1  0  1  1
+
+1 2 3 4 5  6 7 8 9 10   11 12 13 14 15   16 17 18 19 20   21 22 23 24 25 (1-base)
+1 1 0 1 1  1 1 0 1  1    0  0  0  0  0    1  1  0  1  1    1  1  0  1  1
+
+1 2 3 4 5  6 7 8 9 60   61 62 63 64 65   16 17 18 19 20   21 22 23 24 25 (1-base)
+0 0 0 0 0  0 0 0 0  0    0  0  0  0  0    0  0  0  0  0    0  0  0  0  0
 
 n   bit                 total   len
 1   1/1/0/1/1           4       5
@@ -61,27 +62,30 @@ n   bit                 total   len
 
 # print(solution(1, 2, 5)) # 3
 
-print(solution(2, 1, 1)) # 1
+# print(solution(2, 1, 1)) # 1
 # print(solution(2, 3, 3)) # 0
 # print(solution(2, 1, 5)) # 4
 # print(solution(2, 4, 8)) # 4
-print(solution(2, 4, 20)) # 10
-print(solution(2, 4, 25)) # 14
+# print(solution(2, 4, 20)) # 10
+# print(solution(2, 4, 25)) # 14
 # print(solution(2, 4, 19)) # 9
 # print(solution(2, 3, 14)) # 6
 # print(solution(2, 17, 24)) # 6
 # print(solution(2, 10, 24)) # 8
 
+# print(solution(3, 1, 16)) # 9
 # print(solution(3, 1, 25)) # 16
 # print(solution(3, 1, 125)) # 64
 # print(solution(3, 4, 99)) # 45
 # print(solution(3, 4, 100)) # 46
 # print(solution(3, 20, 25)) # 5
 
+# print(solution(4, 1, 64)) # 32
 # print(solution(4, 1, 125)) # 64
 # print(solution(4, 4, 600)) # 238
 # print(solution(4, 500, 600)) # 49
 
-# print(solution(5, 1, 256)) # 128
+print(solution(5, 1, 256)) # 128
+# print(solution(5, 1, 625)) # 256
 
 # print(solution(20, 4, 19)) # 9
