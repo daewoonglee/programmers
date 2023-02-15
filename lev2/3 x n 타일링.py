b3bcc55c81961a62e8df8c29c8cdb15c01c69f46
daewoong lee <dw.lee832@gmail.com>
2023-02-15 14:46:06 +0900

    3 x n 타일링 문제풀이 02 - 풀이O (테스트 14/14, 효율성 6/6)

--- lev2/3 x n 타일링.py
+++ lev2/3 x n 타일링.py
@@ -1,22 +1,27 @@
 def solution(n):
     if n % 2 == 1: return 0
-    alpha = 0
-    for _ in range(n//2-1):
-        alpha = alpha * 3 * 2 + 2
-    print(f"alpha: {alpha}")
-    return (3 ** (n//2) + alpha) % 1000000007
+    n //= 2
+    if n <= 1: return [0, 3][n]
+
+    tile = [3]
+    for i in range(1, n):
+        tile.append(sum([t*3 if j == 0 else t * 2 for j, t in enumerate(tile[::-1])])+2)
+    return tile[n-1] % 1000000007
 
 """
-n=홀수이면 0, 나올 수 있는 방법이 없음 (가로 길이 만족 할 수 없어서)
 n   ans
-2   3 + 0
-4   9 + (0*3)*2 + 2 // 3 * 3 + (2)
-6   27 + (2*3)*2 + 2  // 3 * 3 * 3 + (2) * 3 * 2 + (2)
-8   81 + 70 + 2 // (2*3*3)*2*2 + 2
-10  243 + (72*3)*2 + 2
+0   0 =  0
+2   3 =  3
+4   11 = 3*3 + 2                = f(2)*3 + 2
+6   41 = 11*3 + 3*2 + 2         = f(4)*3 + f(2)*2 + 2
+8  153 = 41*3 + 11*2 + 3*2 + 2  = f(6)*3 + f(4)*2 + f(2)*2 + 2
+
+f(N) = f(N-2)*3 + f(N-4)*2 + f(N-6)*2 + ... + f(2)*2 + 2
 """
 
-print(solution(2)) # 3
-print(solution(4)) # 11
-print(solution(6)) # 41
-print(solution(8)) # 167 -> 153
+# print(solution(0)) # 0
+# print(solution(1)) # 0
+# print(solution(2)) # 3
+# print(solution(4)) # 11
+# print(solution(6)) # 41
+print(solution(8)) # 153
