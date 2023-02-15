7b149823c0683f89134512deb1e91b02c0ab6e5c
daewoong lee <dw.lee832@gmail.com>
2023-02-15 14:58:55 +0900

    3 x n 타일링 문제풀이 02 - code refactoring

--- lev2/3 x n 타일링.py
+++ lev2/3 x n 타일링.py
@@ -1,23 +1,29 @@
 def solution(n):
+    # if n % 2 == 1: return 0
+    # n //= 2
+    # if n <= 1: return [0, 3][n]
+    #
+    # tile = [3]
+    # for i in range(1, n):
+    #     # 10.483321146
+    #     # tile.append(sum([t*3 if j == 0 else t * 2 for j, t in enumerate(tile[::-1])])+2)
+    #
+    #     # code refactoring 01 - 2.616065683
+    #     # tile.append(tile[i-1]*3 + sum(tile[:i-1])*2 +2)
+    # # return tile[n - 1] % 1000000007
+    #
+    #     # code refactoring 02 - 0.30438955700000003
+    #     tile.append((tile[i-1]*3 + sum(tile[:i-1])*2 + 2) % 1000000007)
+    # return tile[n-1]
+
+    # code refactoring 03 - 0.005943779999999999
+    # f(N-2)*4 - f(N-4)
     if n % 2 == 1: return 0
-    n //= 2
-    if n <= 1: return [0, 3][n]
+    pre = cur = 1
+    for _ in range(n // 2):
+        pre, cur = cur, (4 * cur - pre) % 1000000007
+    return cur
 
-    tile = [3]
-    for i in range(1, n):
-        tile.append(sum([t*3 if j == 0 else t * 2 for j, t in enumerate(tile[::-1])])+2)
-    return tile[n-1] % 1000000007
-
-"""
-n   ans
-0   0 =  0
-2   3 =  3
-4   11 = 3*3 + 2                = f(2)*3 + 2
-6   41 = 11*3 + 3*2 + 2         = f(4)*3 + f(2)*2 + 2
-8  153 = 41*3 + 11*2 + 3*2 + 2  = f(6)*3 + f(4)*2 + f(2)*2 + 2
-
-f(N) = f(N-2)*3 + f(N-4)*2 + f(N-6)*2 + ... + f(2)*2 + 2
-"""
 
 # print(solution(0)) # 0
 # print(solution(1)) # 0
@@ -25,3 +31,12 @@ f(N) = f(N-2)*3 + f(N-4)*2 + f(N-6)*2 + ... + f(2)*2 + 2
 # print(solution(4)) # 11
 # print(solution(6)) # 41
 print(solution(8)) # 153
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [
+        0, 1, 2, 4, 6, 8, 1000, 2000, 5000
+    ]
+    t = Timer(f"for t in {query}: solution(t)", "from __main__ import solution")
+    print(t.timeit(number=10))
