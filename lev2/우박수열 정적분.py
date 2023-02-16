d6223b097d8edbd2dd8a7c12aa031315f9965ba9
daewoong lee <dw.lee832@gmail.com>
2023-02-16 15:59:48 +0900

    우박수열 정적분 문제풀이 01 - code refactoring

--- lev2/우박수열 정적분.py
+++ lev2/우박수열 정적분.py
@@ -7,33 +7,41 @@ def solution(k, ranges):
         return li
 
     collatz_list = collatz(k)
-    N = len(collatz_list)
-    print(collatz_list)
+
+    # # 0.553656661
+    # N = len(collatz_list)
+    # area = [(cl + collatz_list[i+1])/2 for i, cl in enumerate(collatz_list[:-1])]
+    #
+    # ans = []
+    # for i in ranges:
+    #     if i[0] == N+i[1]-1 < 0:
+    #         ans.append(0.)
+    #     elif i[0] > N+i[1]-1:
+    #         ans.append(-1.)
+    #     else:
+    #         if i[1] == 0:
+    #             ans.append(sum(area[i[0]:]))
+    #         else:
+    #             ans.append(sum(area[i[0]:i[1]]))
+    # return ans
+
+    # code refactoring - 0.453809352
     area = [(cl + collatz_list[i+1])/2 for i, cl in enumerate(collatz_list[:-1])]
-    print(area)
-
-    ans = []
-    for i in ranges:
-        if i[0] == N+i[1]-1 < 0:
-            ans.append(0.)
-        elif i[0] > N+i[1]-1:
-            ans.append(-1.)
-        else:
-            if i[1] == 0:
-                ans.append(sum(area[i[0]:]))
-            else:
-                ans.append(sum(area[i[0]:i[1]]))
-    return ans
-
-
-
-"""
-사다리꼴 = (밑변+윗변)*높이/2
-밑변 = f(x1)
-윗변 = f(x2)
-높이 = 1
-(collatz_list[i] + collatz_list[i+1]) / 2
-"""
+    N = len(area)
+    return [-1 if s > e+N else sum(area[s:e+N]) for s, e in ranges] # [2:2] -> [] -> 0
 
 
 print(solution(5, [[0,0],[0,-1],[2,-3],[3,-3]])) # [33.0,31.5,0.0,-1.0]
+print(solution(27, [[0,0],[0,-1],[2,-3],[3,-3],[100, 126]])) # [101426.0, 101424.5, 101299.5, 101217.0, 372.0]
+print(solution(127, [[0,0],[0,-1],[2,-3],[3,-3],[0,46],[33,-1]])) # [28024.0, 28022.5, 27472.5, 27090.0, 28024.0, 229.0]
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [
+        [5, [[0,0],[0,-1],[2,-3],[3,-3]]],
+        [27, [[0,0],[0,-1],[2,-3],[3,-3],[100, 126]]],
+        [127, [[0,0],[0,-1],[2,-3],[3,-3],[0,46],[33,-1]]]
+    ]
+    t = Timer(f"for t in {query}: solution(*t)", "from __main__ import solution")
+    print(t.timeit(number=10000))
