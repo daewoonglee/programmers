c91b1c00bc3401bc47ce11b919fb45e816e92317
daewoong lee <dw.lee832@gmail.com>
2023-02-18 10:55:21 +0900

    카드 뭉치 문제풀이 01 - code refactoring

--- lev1/카드 뭉치.py
+++ lev1/카드 뭉치.py
@@ -3,25 +3,26 @@ def solution(cards1, cards2, goal):
     N2 = len(cards2)
     i1 = i2 = 0
 
-    # # 0.04525750099999999
-    # for g in goal:
-    #     if i1 < N1 and cards1[i1] == g:
-    #         i1 += 1
-    #     elif i2 < N2 and cards2[i2] == g:
-    #         i2 += 1
-    #     else:
-    #         return "No"
-    # return "Yes"
-
-    # code refactoring - 0.04101547800000001
+    # 0.04525750099999999
     for g in goal:
-        if cards1[i1] == g and i1 + 1 < N1:
+        if i1 < N1 and cards1[i1] == g:
             i1 += 1
-        elif cards2[i2] == g and i2 + 1 < N2:
+        elif i2 < N2 and cards2[i2] == g:
             i2 += 1
         else:
             return "No"
     return "Yes"
+
+    # i1 + 1 < N1 or i2 + 1 < N2는 틀린 조건문, 문제 만족하지 못함
+    # i1 + 1 <= N1 and i2 + 1 <= N2가 맞으며 이렇게 되면 속도가 오히려 느림
+    # for g in goal:
+    #     if cards1[i1] == g and i1 + 1 < N1:
+    #         i1 += 1
+    #     elif cards2[i2] == g and i2 + 1 < N2:
+    #         i2 += 1
+    #     else:
+    #         return "No"
+    # return "Yes"
 
 
 # print(solution(["i", "drink", "water"], ["want", "to"], ["i", "want", "to", "drink", "water"])) # Yes
