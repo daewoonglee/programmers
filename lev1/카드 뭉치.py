60e7e86dcb5770e15a650c4ab91173f8070b0d30
daewoong lee <dw.lee832@gmail.com>
2023-02-18 10:52:17 +0900

    카드 뭉치 문제풀이 01 - code refactoring

--- lev1/카드 뭉치.py
+++ lev1/카드 뭉치.py
@@ -2,20 +2,42 @@ def solution(cards1, cards2, goal):
     N1 = len(cards1)
     N2 = len(cards2)
     i1 = i2 = 0
+
+    # # 0.04525750099999999
+    # for g in goal:
+    #     if i1 < N1 and cards1[i1] == g:
+    #         i1 += 1
+    #     elif i2 < N2 and cards2[i2] == g:
+    #         i2 += 1
+    #     else:
+    #         return "No"
+    # return "Yes"
+
+    # code refactoring - 0.04101547800000001
     for g in goal:
-        if i1 < N1 and cards1[i1] == g:
+        if cards1[i1] == g and i1 + 1 < N1:
             i1 += 1
-        elif i2 < N2 and cards2[i2] == g:
+        elif cards2[i2] == g and i2 + 1 < N2:
             i2 += 1
         else:
             return "No"
     return "Yes"
 
 
-"""
-goal 배열 탐색하며 card1 or card2 단어와 배열이 동일한 지 비교
-모두 소문자로만 구성되어 있다는 조건이 있기 때문에 대소문자 고려X
-"""
+# print(solution(["i", "drink", "water"], ["want", "to"], ["i", "want", "to", "drink", "water"])) # Yes
+# print(solution(["i", "water", "drink"], ["want", "to"], ["i", "want", "to", "drink", "water"])) # No
+# print(solution(["i1"], ["i2"], ["i1", "i2", "i3"])) # No
+print(solution(["i1", "i2"], ["i3", "i4"], ["i1", "i2"])) # No
 
-print(solution(["i", "drink", "water"], ["want", "to"], ["i", "want", "to", "drink", "water"])) # Yes
-print(solution(["i", "water", "drink"], ["want", "to"], ["i", "want", "to", "drink", "water"])) # No
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [
+        [["i", "drink", "water"], ["want", "to"], ["i", "want", "to", "drink", "water"]],
+        [["i", "water", "drink"], ["want", "to"], ["i", "want", "to", "drink", "water"]],
+        [["i1"], ["i2"], ["i1", "i2", "i3"]],
+        [["i1", "i2"], ["i3", "i4"], ["i1", "i2"]],
+        [[f"i{i}" for i in range(1, 11)], [f"j{i}" for i in range(1, 11)], [[f"i{i}" for i in range(1, 11)], [f"j{i}" for i in range(1, 11)]]],
+    ]
+    t = Timer(f"for t in {query}: solution(*t)", "from __main__ import solution")
+    print(t.timeit(number=10000))
