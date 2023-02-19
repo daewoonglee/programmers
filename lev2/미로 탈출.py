d6c1890fccbd0f85dc8d6851c51ad37dac5274a0
daewoong lee <dw.lee832@gmail.com>
2023-02-20 07:21:56 +0900

    미로 탈출 문제풀이 01 - code refactoring

--- lev2/미로 탈출.py
+++ lev2/미로 탈출.py
@@ -2,6 +2,7 @@ from collections import deque
 
 
 def solution(maps):
+    # code refactoring - 1.577800685
     def find_start_loc():
         for i, row in enumerate(maps):
             for j, col in enumerate(row):
@@ -15,24 +16,19 @@ def solution(maps):
             i, j, cnt = q.popleft()
             if maps[i][j] == target:
                 return i, j, cnt
-            elif visited[i][j] or maps[i][j] == "X":
-                continue
 
             visited[i][j] = 1
-
-            if i+1 < ROWS:
-                q.append([i+1, j, cnt+1])
-            if i-1 >= 0:
-                q.append([i-1, j, cnt+1])
-            if j+1 < COLS:
-                q.append([i, j+1, cnt+1])
-            if j-1 >= 0:
-                q.append([i, j-1, cnt+1])
+            for dx, dy in direction:
+                nx = i + dx
+                ny = j + dy
+                if 0 <= nx < ROWS and 0 <= ny < COLS and not visited[nx][ny] and maps[nx][ny] != "X":
+                    q.append([nx, ny, cnt+1])
 
         return -1, -1, -1
 
     ROWS = len(maps)
     COLS = len(maps[0])
+    direction = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
     x, y = find_start_loc()
     lever_queue = deque([[x, y, 0]])
@@ -46,34 +42,20 @@ def solution(maps):
     return Lcnt + Ecnt
 
 
-"""
-Find S -> Find L -> Find E
-
-S 위치에서 L을 bfs로 탐색, L을 찾으면 그게 L의 최단 경로, 이외 찾던 경로 모두 리셋
--> 찾지 못하면 -1 반환
-L 위치에서 E를 bfs로 탐색, E을 찾으면 그게 E의 최단 경로, 이외 찾던 경로 모두 리셋
--> 찾지 못하면 -1 반환
-
-S O O O L
-X X X X O
-O O O O O
-O X X X X
-O O O O E 16
-
-S O O O L
-O O O O O
-O O O O O
-O O O O O
-O O O O E 8
-
-S O O O L
-X X X X O
-O O O O O
-O X X X X
-O O O X E -1
-"""
-
-# print(solution(["SOOOL","XXXXO","OOOOO","OXXXX","OOOOE"])) # 16
-# print(solution(["SOOOL","OOOOO","OOOOO","OOOOO","OOOOE"])) # 8
+print(solution(["SOOOL","XXXXO","OOOOO","OXXXX","OOOOE"])) # 16
+print(solution(["SOOOL","OOOOO","OOOOO","OOOOO","OOOOE"])) # 8
 print(solution(["SOOOL","XXXXO","OOOOO","OXXXX","OOOXE"])) # -1
-# print(solution(["LOOXS","OOOOX","OOOOO","OOOOO","EOOOO"])) # -1
+print(solution(["LOOXS","OOOOX","OOOOO","OOOOO","EOOOO"])) # -1
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [
+        [["SOOOL","XXXXO","OOOOO","OXXXX","OOOOE"]],
+        [["SOOOL","OOOOO","OOOOO","OOOOO","OOOOE"]],
+        [["SOOOL","XXXXO","OOOOO","OXXXX","OOOXE"]],
+        [["LOOXS","OOOOX","OOOOO","OOOOO","EOOOO"]],
+        [["SOOOL","XXXXO","OOOOO","OXXXX","OOOOO","XXXXO","OOOOO","OXXXX","OOOOO","XXXXO","OOOOE"]],
+    ]
+    t = Timer(f"for t in {query}: solution(*t)", "from __main__ import solution")
+    print(t.timeit(number=10000))
