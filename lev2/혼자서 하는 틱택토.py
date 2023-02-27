e251c963f43874157c90c2c7de017964b3aa9eae
daewoong lee <dw.lee832@gmail.com>
2023-02-27 14:59:52 +0900

    혼자서 하는 틱택토 문제풀이 02 - code refactoring

--- lev2/혼자서 하는 틱택토.py
+++ lev2/혼자서 하는 틱택토.py
@@ -1,6 +1,19 @@
 def solution(board):
-    def cnt_OX(target):
-        return sum([row.count(target) for row in board])
+    # def cnt_OX(target):
+        # 0.414120274
+        # return sum([row.count(target) for row in board])
+
+    def cnt_OX():
+        # code refactoring - 0.323417551
+        cnt_O = 0
+        cnt_X = 0
+        for row in board:
+            for r in row:
+                if r == 'O':
+                    cnt_O += 1
+                elif r == 'X':
+                    cnt_X += 1
+        return cnt_O, cnt_X
 
     def check_OX(target):
         width = [(0,0),(1,0),(2,0)]
@@ -24,8 +37,9 @@ def solution(board):
                 else:
                     return 1
 
-    cnt_O = cnt_OX("O")
-    cnt_X = cnt_OX("X")
+    # cnt_O, cnt_X = cnt_OX("O"), cnt_OX("X")
+    cnt_O, cnt_X = cnt_OX()
+    
     if cnt_O == cnt_X+1:
         return 0 if check_OX("X") else 1
     elif cnt_O == cnt_X:
@@ -33,17 +47,6 @@ def solution(board):
     else:
         return 0
 
-
-"""
-if O==X+1  1
-elif O==X
-    if OOO -> 0 O가 선공이라 이후 X가 한번 더 진행했다는 의미 
-    elif XXX -> 1 
-else -1
-"""
-"O.X"   "OOO"   "..."   "O.O"   "O.O"   "OOX"   "XOX"
-".O."   "..."   ".X."   "XOX"   "XOX"   "XXO"   "OOX"
-"..X"   "XXX"   "..."   "XXO"   "X.O"   "..."   "OOX"
 
 # print(solution(["O.X", ".O.", "..X"])) # 1
 # print(solution(["OOO", "...", "XXX"])) # 0
@@ -54,3 +57,20 @@ else -1
 # print(solution(["O.O", "XOX", "X.O"])) # 1
 # print(solution(["OOX", "XXO", "..."])) # 1
 print(solution(["XOX", "OOX", "OOX"])) # 0
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [
+        ["O.X", ".O.", "..X"],
+        ["OOO", "...", "XXX"],
+        ["...", ".X.", "..."],
+        ["X.X", ".O.", "OXO"],
+        ["...", "...", "..."],
+        ["O.O", "XOX", "XXO"],
+        ["O.O", "XOX", "X.O"],
+        ["OOX", "XXO", "..."],
+        ["XOX", "OOX", "OOX"]
+    ]
+    t = Timer(f"for t in {query}: solution(t)", "from __main__ import solution")
+    print(t.timeit(number=10000))
