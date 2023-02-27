bc9d6ea5d3ba55320153c1344628a81e422256d8
daewoong lee <dw.lee832@gmail.com>
2023-02-27 14:40:48 +0900

    혼자서 하는 틱택토 문제풀이 02 - 풀이O (테스트 54/54)

--- lev2/혼자서 하는 틱택토.py
+++ lev2/혼자서 하는 틱택토.py
@@ -2,7 +2,7 @@ def solution(board):
     def cnt_OX(target):
         return sum([row.count(target) for row in board])
 
-    def check_O():
+    def check_OX(target):
         width = [(0,0),(1,0),(2,0)]
         height = [(0,0),(0,1),(0,2)]
         diagonal = [(0,0),(0,2)]
@@ -11,7 +11,7 @@ def solution(board):
             for x, y in pts:
                 nx, ny = x, y
                 for _ in range(3):
-                    if board[nx][ny] != "O":
+                    if board[nx][ny] != target:
                         break
 
                     if i == 0:
@@ -24,38 +24,12 @@ def solution(board):
                 else:
                     return 1
 
-        # for x, y in width:
-        #     for i in range(2):
-        #         if board[x][y+i+1] != "O":
-        #             break
-        #     else:
-        #         return 1
-        #
-        # for x, y in height:
-        #     for i in range(2):
-        #         if board[x+i+1][y] != "O":
-        #             break
-        #     else:
-        #         return 1
-        #
-        # for x, y in diagonal:
-        #     ny = 1 if y == 0 else -1
-        #     for _ in range(2):
-        #         x += 1
-        #         y += ny
-        #         if board[x][y] != "O":
-        #             break
-        #     else:
-        #         return 1
-
     cnt_O = cnt_OX("O")
     cnt_X = cnt_OX("X")
-    print(f"O: {cnt_O}, X: {cnt_X}")
-
     if cnt_O == cnt_X+1:
-        return 1
+        return 0 if check_OX("X") else 1
     elif cnt_O == cnt_X:
-        return 0 if check_O() else 1
+        return 0 if check_OX("O") else 1
     else:
         return 0
 
@@ -67,9 +41,9 @@ elif O==X
     elif XXX -> 1 
 else -1
 """
-"O.X"   "OOO"   "..."   "O.O"   "O.O"   "OOX"   "X.X"
-".O."   "..."   ".X."   "XOX"   "XOX"   "XXO"   ".O."
-"..X"   "XXX"   "..."   "XXO"   "X.O"   "..."   "OXO"
+"O.X"   "OOO"   "..."   "O.O"   "O.O"   "OOX"   "XOX"
+".O."   "..."   ".X."   "XOX"   "XOX"   "XXO"   "OOX"
+"..X"   "XXX"   "..."   "XXO"   "X.O"   "..."   "OOX"
 
 # print(solution(["O.X", ".O.", "..X"])) # 1
 # print(solution(["OOO", "...", "XXX"])) # 0
@@ -77,5 +51,6 @@ else -1
 # print(solution(["X.X", ".O.", "OXO"])) # 1
 # print(solution(["...", "...", "..."])) # 1
 # print(solution(["O.O", "XOX", "XXO"])) # 0
-print(solution(["O.O", "XOX", "X.O"])) # 1
-print(solution(["OOX", "XXO", "..."])) # 1
+# print(solution(["O.O", "XOX", "X.O"])) # 1
+# print(solution(["OOX", "XXO", "..."])) # 1
+print(solution(["XOX", "OOX", "OOX"])) # 0
