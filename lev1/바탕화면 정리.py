141c3d32c3a4239106d104ef5013cbad5a0ab13d
daewoong lee <dw.lee832@gmail.com>
2023-03-06 06:48:34 +0900

    바탕화면 정리 문제풀이 01 - code refactoring

--- lev1/바탕화면 정리.py
+++ lev1/바탕화면 정리.py
@@ -1,23 +1,42 @@
 def solution(wallpaper):
-    ans = [51, 51, -1, -1]
+    # # 0.629445131
+    # ans = [51, 51, -1, -1]
+    # for i, rows in enumerate(wallpaper):
+    #     for j, r in enumerate(rows):
+    #         if r == "#":
+    #             if ans[0] > i: ans[0] = i
+    #             if ans[1] > j: ans[1] = j
+    #             if ans[2] < i+1: ans[2] = i+1
+    #             if ans[3] < j+1: ans[3] = j+1
+    # return ans
+
+    # code refactoring - 0.5012737780000001
+    x, y = [], []
     for i, rows in enumerate(wallpaper):
         for j, r in enumerate(rows):
             if r == "#":
-                if ans[0] > i: ans[0] = i
-                if ans[1] > j: ans[1] = j
-                if ans[2] < i+1: ans[2] = i+1
-                if ans[3] < j+1: ans[3] = j+1
-    return ans
-
-
-"""
-반드시 하나 이상의 파일이 존재
-#을 서치하면 min x,y, max x,y를 찾으면 그게 최단 거리
-맨위# minx, 맨아# maxx, 맨왼# miny, 맨오# maxy, 
-"""
+                x.append(i)
+                y.append(j)
+    return [min(x), min(y), max(x) + 1, max(y) + 1]
 
 
 # print(solution([".#...", "..#..", "...#."])) # [0, 1, 3, 4]
 # print(solution(["..........", ".....#....", "......##..", "...##.....", "....#....."])) # [1, 3, 5, 8]
 # print(solution([".##...##.", "#..#.#..#", "#...#...#", ".#.....#.", "..#...#..", "...#.#...", "....#...."])) # [0, 0, 7, 9]
 print(solution(["..", "#."])) # [1, 0, 2, 1]
+print(solution("#")) # [0, 0, 1, 1]
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [
+        [".#...", "..#..", "...#."],
+        ["..........", ".....#....", "......##..", "...##.....", "....#....."],
+        [".##...##.", "#..#.#..#", "#...#...#", ".#.....#.", "..#...#..", "...#.#...", "....#...."],
+        ["..", "#."],
+        ["#"],
+        ["##################################################"*50],
+        ["...#..................#.........." * 10]
+    ]
+    t = Timer(f"for t in {query}: solution(t)", "from __main__ import solution")
+    print(t.timeit(number=1000))
