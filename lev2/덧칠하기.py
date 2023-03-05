9873da646a06e69636064f054f0acdeadf625bac
daewoong lee <dw.lee832@gmail.com>
2023-03-06 07:14:43 +0900

    덧칠하기 문제풀이 01 - code refactoring

--- lev2/덧칠하기.py
+++ lev2/덧칠하기.py
@@ -1,19 +1,39 @@
 def solution(n, m, section):
-    ans = 0
-    step = 0
+    # # 1.810782208
+    # ans = 0
+    # step = 0
+    # for s in section:
+    #     if step < s:
+    #         step = m + s - 1
+    #         ans += 1
+    # return ans
+
+    # code refactoring - 1.5863293029999999
+    ans = 1
+    step = section[0]
     for s in section:
-        if step < s:
-            step = m + s - 1
+        if s - step >= m:
+            step = s
             ans += 1
     return ans
 
-
-"""
-주어진 섹터에서 롤러 길이로 최대한 많이 칠할 수 있는 벽의 위치를 탐색하여 칠함(ans+1)
-이후 남은 섹터에서 롤러 길이로 최대한 많이 칠할 수 있는 위치를 탐색하여 칠함 (반복)
-"""
 
 print(solution(8, 4, [2, 3, 6])) # 2
 print(solution(5, 4, [1, 3])) # 1
 print(solution(4, 1, [1, 2, 3, 4])) # 4
 print(solution(20, 4, [1, 4, 5, 6, 7, 12, 14, 15])) # 3
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [
+        [8, 4, [2, 3, 6]],
+        [5, 4, [1, 3]],
+        [4, 1, [1, 2, 3, 4]],
+        [20, 4, [1, 4, 5, 6, 7, 12, 14, 15]],
+        [100000, 1, [i for i in range(1, 100001)]],
+        [100000, 3, [i for i in range(1, 100001, 5)]],
+        [100000, 10, [i for i in range(1, 100001)]]
+    ]
+    t = Timer(f"for t in {query}: solution(*t)", "from __main__ import solution")
+    print(t.timeit(number=100))
