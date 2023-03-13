3e2bcea82bb313a215d477c37d4f57db31acc46a
daewoong lee <dw.lee832@gmail.com>
2023-03-13 18:12:22 +0900

    징검다리 건너기 문제풀이 02 - 풀이X (테스트 25/25, 효율성 13/14)

--- lev3/징검다리 건너기.py
+++ lev3/징검다리 건너기.py
@@ -1,20 +1,23 @@
+from collections import deque
+
+
 def solution(stones, k):
-    ans = max(stones)
-    for i in range(len(stones)-k+1):
-        m = max(stones[i:i+k])
-        if ans > m:
-            ans = m
+    temp = deque(stones[:k])
+    max_value = max(temp)
+    ans = max_value
+    for i in range(k, len(stones)):
+        val = temp.popleft()
+        temp.append(stones[i])
+        if val == max_value:
+            max_value = max(temp)
+            if ans > max_value:
+                ans = max_value
     return ans
 
 
 """
-1 <= stones len <= 200,000
-1 <= stones[i] <= 200,000,000
-1 <= k <= stones len
-
-매번 루프 탐색으론 효율성이 안나올 가능성 높음
-루프를 한번 탐색하며 k 값으로 건널 수 있는 중간 위치 값을 탐색, 가장 max 값을 반환하면 O(n)으로 돌 수 있음
-    중간 위치 탐색은 stones에서 k 깂 슬라이스 구간 탐색
+루프를 한번 탐색하며 k 값 슬라이스하며 구간 탐색
+탐색된 구간 내 maximum 값 < 이전 maximum 값이면 ans 재할당 
 """
 
 # print(solution([5, 4, 3, 2], 4)) # 5
@@ -28,8 +31,8 @@ def solution(stones, k):
 # print(solution([4, 4, 4, 4, 4, 4, 4, 2, 1, 4], 2)) # 2
 # print(solution([4, 3, 2, 1, 2, 3, 4, 1, 4, 1], 3)) # 2
 # print(solution([3, 1, 3, 4, 5, 3, 1, 3, 2, 5], 3)) # 3
-# print(solution([10],1)) # 10
+# print(solution([10], 1)) # 10
 # print(solution([5, 5, 5, 2, 2, 9], 3)) # 5
+print(solution([5, 5, 6, 7, 7, 7], 3)) # 6
+print(solution([5, 5, 5, 2, 2, 9, 3, 3, 3, 4], 3)) # 3
 
-print(solution([5, 5, 6, 7, 7, 7], 3)) # 7
-print(solution1([5, 5, 6, 7, 7, 7], 3)) # 6
