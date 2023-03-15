11548efc3d58154cc38e656c96666de0f8eaae7f
daewoong lee <dw.lee832@gmail.com>
2023-03-15 22:00:41 +0900

    징검다리 건너기 문제풀이 04 - code refactoring

--- lev3/징검다리 건너기.py
+++ lev3/징검다리 건너기.py
@@ -1,4 +1,5 @@
 def solution(stones, k):
+    # 0.357573229
     left = 1
     right = max(stones)
     ans = 1
@@ -8,16 +9,14 @@ def solution(stones, k):
         for stone in stones:
             if stone <= mid:
                 cnt += 1
+                if cnt == k:
+                    ans = mid
+                    right = mid - 1
+                    break
             else:
                 cnt = 0
-            if cnt == k:
-                break
-
-        if cnt < k:
-            left = mid+1
         else:
-            ans = mid
-            right = mid-1
+            left = mid+1
     return ans
 
 
@@ -39,3 +38,29 @@ print(solution([5, 5, 5, 2, 2, 9, 3, 3, 3, 4], 3)) # 3
 print(solution([3, 3, 3, 2, 2, 9, 5, 5, 5, 4], 3)) # 3
 # print(solution([2, 3, 6, 7, 8, 9, 10, 11, 12, 13], 3)) # 6
 
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [
+        [[5, 4, 3, 2], 4],
+        [[2, 4, 5, 3, 2, 1, 4, 2, 5, 1], 1],
+        [[3, 4, 4, 4, 4, 4, 4, 4, 4, 4], 1],
+        [[2, 4, 5, 3, 2, 1, 4, 2, 5, 1], 1],
+        [[2, 4, 5, 3, 2, 1, 4, 2, 5, 1], 3],
+        [[4, 4, 4, 4, 4, 4, 4, 4, 4, 4], 3],
+        [[4, 4, 4, 4, 4, 4, 4, 4, 4, 4], 10],
+        [[4, 4, 4, 4, 4, 4, 4, 1, 4, 4], 2],
+        [[4, 4, 4, 4, 4, 4, 4, 2, 1, 4], 2],
+        [[4, 3, 2, 1, 2, 3, 4, 1, 4, 1], 3],
+        [[3, 1, 3, 4, 5, 3, 1, 3, 2, 5], 3],
+        [[10], 1],
+        [[5, 5, 5, 2, 2, 9], 3],
+        [[5, 5, 6, 7, 7, 7], 3],
+        [[5, 5, 5, 2, 2, 9, 3, 3, 3, 4], 3],
+        [[3, 3, 3, 2, 2, 9, 5, 5, 5, 4], 3],
+        [[2, 3, 6, 7, 8, 9, 10, 11, 12, 13], 3],
+        [[i for i in range(1, 1000)], 1],
+        [[i for i in range(1000, 1, -1)], 1]
+    ]
+    t = Timer(f"for t in {query}: solution(*t)", "from __main__ import solution")
+    print(t.timeit(number=1000))
