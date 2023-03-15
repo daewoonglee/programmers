4a79ea1b1a9f90cde241965f77df910ab3e067de
daewoong lee <dw.lee832@gmail.com>
2023-03-15 21:29:27 +0900

    징검다리 건너기 문제풀이 03 - 풀이X, max(temp) 구하기 수정 (테스트 25/25, 효율성 13/14)

--- lev3/징검다리 건너기.py
+++ lev3/징검다리 건너기.py
@@ -1,24 +1,17 @@
-from collections import deque
-
-
 def solution(stones, k):
-    temp = deque(stones[:k])
-    max_value = max(temp)
-    ans = max_value
-    for i in range(k, len(stones)):
-        val = temp.popleft()
-        temp.append(stones[i])
-        if val == max_value:
-            max_value = max(temp)
-            if ans > max_value:
-                ans = max_value
+    temp = list()
+    ans = float("inf")
+    for i, stone in enumerate(stones):
+        while temp and temp[-1][-1] < stone:
+            temp.pop()
+        temp.append([i, stone])
+        if i >= k-1:
+            if temp[0][0] <= i-k:
+                temp.pop(0)
+            if ans > temp[0][-1]:
+                ans = temp[0][-1]
     return ans
 
-
-"""
-루프를 한번 탐색하며 k 값 슬라이스하며 구간 탐색
-탐색된 구간 내 maximum 값 < 이전 maximum 값이면 ans 재할당 
-"""
 
 # print(solution([5, 4, 3, 2], 4)) # 5
 # print(solution([2, 4, 5, 3, 2, 1, 4, 2, 5, 1], 1)) # 1
@@ -33,6 +26,7 @@ def solution(stones, k):
 # print(solution([3, 1, 3, 4, 5, 3, 1, 3, 2, 5], 3)) # 3
 # print(solution([10], 1)) # 10
 # print(solution([5, 5, 5, 2, 2, 9], 3)) # 5
-print(solution([5, 5, 6, 7, 7, 7], 3)) # 6
+# print(solution([5, 5, 6, 7, 7, 7], 3)) # 6
 print(solution([5, 5, 5, 2, 2, 9, 3, 3, 3, 4], 3)) # 3
+print(solution([2, 3, 6, 7, 8, 9, 10, 11, 12, 13], 3)) # 6
 
