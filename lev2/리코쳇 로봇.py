9b50d752e7fde6103b9f72b67c5886f6cfda2dd3
daewoong lee <dw.lee832@gmail.com>
2023-03-20 09:52:01 +0900

    리코쳇 로봇 문제풀이 01 - code refactoring

--- lev2/리코쳇 로봇.py
+++ lev2/리코쳇 로봇.py
@@ -30,16 +30,21 @@ def solution(board):
     def bfs(q, visited):
         while q:
             x, y, dep = q.popleft()
-            print(f" xy: {x}, {y}, depth: {dep}")
+            # print(f" xy: {x}, {y}, depth: {dep}")
             for m in ["U", "D", "L", "R"]:
                 nx, ny = move_robot(x, y, m)
                 if board[nx][ny] == "G":
                     return dep+1
-                elif visited[nx][ny]:
-                    continue
-                else:
+                # 1.5365211840000002
+                # elif visited[nx][ny]:
+                #     continue
+                # else:
+                #     visited[nx][ny] = 1
+
+                # code refactoring - 1.469149224
+                if not visited[nx][ny]:
                     visited[nx][ny] = 1
-                q.append([nx, ny, dep+1])
+                    q.append([nx, ny, dep+1])
         return -1
 
     ROW, COL = len(board), len(board[0])
@@ -49,22 +54,22 @@ def solution(board):
     return bfs(deque([[*start, 0]]), visited_list)
 
 
-"""
-R에서부터 BFS로 탐색
-R에서부터 이동가능한 상하좌우 탐색하여 queue에 넣음
-queue에서 뽑은 노드에서 이동가능한 경우 탐색하여 queue에 넣음
-G를 찾을때까지 or queue가 빌 때까지 반복
-
-...D..R     ..D.R..     .G.
-.D.G...     .......     ...
-....D.D     .......     ..R
-D....D.     D..DD.D
-..D....     ......G
-"""
-
-
 print(solution(["...D..R", ".D.G...", "....D.D", "D....D.", "..D...."])) # 7
 print(solution([".D.R", "....", ".G..", "...D"])) # -1
 print(solution(["..D.R..", ".......", ".......", "D..DD.D", "......G"])) # 6
 print(solution([".G.", "...", "..R"])) # -1
 print(solution(["DG.", "...", "..R"])) # 2
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [
+        ["...D..R", ".D.G...", "....D.D", "D....D.", "..D...."],
+        [".D.R", "....", ".G..", "...D"],
+        ["..D.R..", ".......", ".......", "D..DD.D", "......G"],
+        [".G.", "...", "..R"],
+        ["DG.", "...", "..R"],
+        ["........R", ".........", ".........", ".........", ".........", "D........", "GDDDDDDDD"]
+    ]
+    t = Timer(f"for t in {query}: solution(t)", "from __main__ import solution")
+    print(t.timeit(number=10000))
