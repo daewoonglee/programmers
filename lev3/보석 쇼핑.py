c96be2cbc0b0048d7d3d44116a9c2954d1da042d
daewoong lee <dw.lee832@gmail.com>
2023-03-22 10:05:09 +0900

    보석 쇼핑 문제풀이 01 - code refactoring

--- lev3/보석 쇼핑.py
+++ lev3/보석 쇼핑.py
@@ -1,21 +1,31 @@
 def solution(gems):
+    """
     def shopping_gems():
-        shopping_N = len(gem_list)
         shopping_gem_dict = dict()
-        li = []
+        shopping_idx = 0
         for i, gem in enumerate(gem_list[::-1]):
             if gem not in shopping_gem_dict:
                 shopping_gem_dict[gem] = 1
             if len(shopping_gem_dict) == total_gem_list:
-                li.append(shopping_N-i+skip_N)
-                li.append(shopping_N+skip_N)
+                shopping_idx = i
                 break
-        return li
+        return [len(gem_list)-shopping_idx+step, len(gem_list)+step]
 
+    def update_shopping_list():
+        for g in gem_list[:shopping_list[0] - step]:
+            if g in gem_dict:
+                if gem_dict[g] > 1:
+                    gem_dict[g] -= 1
+                else:
+                    del gem_dict[g]
+        del gem_list[:shopping_list[0] - step]
+
+    # 4.967148605999999
     ans = [0, 100000]
     N = len(gems)
-    skip_N = 0
     total_gem_list = len(set(gems))
+
+    step = 0
     idx = 0
     gem_dict = dict()
     gem_list = list()
@@ -29,31 +39,66 @@ def solution(gems):
 
         if len(gem_dict) == total_gem_list:
             shopping_list = shopping_gems()
-            if (shopping_list[-1] - shopping_list[0]) < (ans[-1] - ans[0]):
+            shopping_diff = shopping_list[-1] - shopping_list[0]
+            ans_diff = ans[-1] - ans[0]
+            if shopping_diff < ans_diff or (shopping_diff == ans_diff and shopping_list[0] < ans[0]):
                 ans = shopping_list
-            elif (shopping_list[-1] - shopping_list[0]) == (ans[-1] - ans[0]) and shopping_list[0] < ans[0]:
-                ans = shopping_list
-            idx = shopping_list[0]
-            skip_N = shopping_list[0]
-            gem_dict = dict()
-            gem_list = list()
+
+            update_shopping_list()
+
+            idx = shopping_list[-1]
+            step = shopping_list[0]
         else:
             idx += 1
 
     return ans
+    """
+    # code refactoring - 4.472075382
+    N = len(gems)
+    gem_size = len(set(gems))
+    dic = {gems[0]: 1}
+    ans = [0, N]
+    start, end = 0, 0
+    while start < N and end < N:
+        if len(dic) == gem_size:
+            if end - start < ans[1] - ans[0]:
+                ans = [start, end]
+            if dic[gems[start]] == 1:
+                del dic[gems[start]]
+            else:
+                dic[gems[start]] -= 1
+            start += 1
 
+        else:
+            end += 1
+            if end == N:
+                break
+            if gems[end] in dic.keys():
+                dic[gems[end]] += 1
+            else:
+                dic[gems[end]] = 1
 
-"""
-DP, O(nk)으로 풀어야 할 듯
-1. 루프 진행하면서 전체 보석의 종류가 들어오는 구간 탐색
-    - 전체 보석이 쌓인 경우의 리스트를 뒤에서부터 탐색
-    - 맨 뒤에서부터 보석 종류 탐색하며 k개의 보석 종류가 다 탐색된 시점이 정답
-    - 가장 적은 경우를 가지는 탐색 시점을 저장
-2. 팀섹된 시점+1부터 1번 반복 
-"""
+    return [ans[0] + 1, ans[1] + 1]
+
 
 # print(solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"])) #[3, 7]
 # print(solution(["AA", "AB", "AC", "AA", "AC"])) # [1, 3]
 # print(solution(["XYZ", "XYZ", "XYZ"])) # [1, 1]
 # print(solution(["ZZZ", "YYY", "NNNN", "YYY", "BBB"])) # [1, 5]
 print(solution(["A", "A", "A", "B", "A", "A", "C", "B", "A", "A"])) # [6, 8]
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [
+        ["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"],
+        ["AA", "AB", "AC", "AA", "AC"],
+        ["XYZ", "XYZ", "XYZ"],
+        ["ZZZ", "YYY", "NNNN", "YYY", "BBB"],
+        ["A", "A", "A", "B", "A", "A", "C", "B", "A", "A"],
+        ["A"*1000],
+        [str(i) for i in range(1000)],
+        ["1","2","3","4","5","5","6","6","7","8","9","10","9","1","2","3","4","1","2","3","8","9"]
+    ]
+    t = Timer(f"for t in {query}: solution(t)", "from __main__ import solution")
+    print(t.timeit(number=10000))
