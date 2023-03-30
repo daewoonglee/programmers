16f18fe5b2aee76a5fe009014a1f0957791aaa4b
daewoong lee <dw.lee832@gmail.com>
2023-03-30 15:15:45 +0900

    합승 택시 요금 문제풀이 01 - code refactoring02 (using floyd warshall algorithm)

--- lev3/합승 택시 요금.py
+++ lev3/합승 택시 요금.py
@@ -18,20 +18,39 @@ def solution(n, s, a, b, fares):
                 if maps[next_node] > maps[cur_node] + next_fare:
                     maps[next_node] = maps[cur_node] + next_fare
                     heapq.heappush(h, (maps[next_node], next_node))
-        # print(f"idx: {idx}, maps: {maps}")
+
         return maps
 
-    fare_dict = {k:[] for k in range(n)}
-    for data in fares:
-        start, end, fare = data
-        fare_dict[start-1].append([end-1, fare]) # 1~n시작을 0~n-1로 변환
-        fare_dict[end-1].append([start-1, fare])
+    def floyd_warshall():
+        for z in range(n):
+            for i in range(n):
+                # if i == z: continue # 로직상 if문 없어도 제대로 돔, if문 비교 횟수 감소가 더 나음
+                for j in range(i+1):
+                    fare_maps[i][j] = fare_maps[j][i]
+                for j in range(i+1, n):
+                    fare_maps[i][j] = min(fare_maps[i][j], fare_maps[i][z] + fare_maps[z][j])
 
-    # 0.49168040200000007
-    dij_maps = [dijkstra(i) for i in range(n)]
-    ans = dij_maps[s-1][a-1] + dij_maps[s-1][b-1]
+        return fare_maps
+
+    # # dijkstra - 0.49168040200000007
+    # fare_dict = {k:[] for k in range(n)}
+    # for data in fares:
+    #     start, end, fare = data
+    #     fare_dict[start-1].append([end-1, fare]) # 1~n시작을 0~n-1로 변환
+    #     fare_dict[end-1].append([start-1, fare])
+    # fare_maps = [dijkstra(i) for i in range(n)]
+
+    # floyd warshall - 1455.153891
+    fare_maps = [[float("inf")] * n for _ in range(n)]
+    for i, j, fare in fares:
+        fare_maps[i-1][j-1] = fare_maps[j-1][i-1] = fare
     for i in range(n):
-        fare_way = dij_maps[s-1][i] + dij_maps[i][a-1] + dij_maps[i][b-1]
+        fare_maps[i][i] = 0
+    fare_maps = floyd_warshall()
+
+    ans = fare_maps[s-1][a-1] + fare_maps[s-1][b-1]
+    for i in range(n):
+        fare_way = fare_maps[s-1][i] + fare_maps[i][a-1] + fare_maps[i][b-1]
         if fare_way < ans:
             ans = fare_way
     return ans
@@ -48,7 +67,7 @@ if __name__ == "__main__":
         [6,4,6,2,[[4,1,10], [3,5,24], [5,6,2], [3,1,41], [5,1,24], [4,6,50], [2,4,66], [2,3,22], [1,6,25]]],
         [7,3,4,1,[[5,7,9], [4,6,4], [3,6,1], [3,2,3], [2,1,6]]],
         [6,4,5,6,[[2,6,6], [6,3,7], [4,6,7], [6,5,11], [2,5,12], [5,3,20], [2,4,8], [4,3,9]]],
-        [200,1,100,150,[[199,200,100],[2,6,6], [6,3,7], [4,6,7], [6,5,11], [2,5,12], [5,3,20], [20,40,8], [40,30,9],[50,70,9], [40,60,4], [30,60,1], [30,20,3], [20,10,6],[41,11,10], [31,51,24], [51,61,2], [31,11,41], [51,10,24], [41,60,50], [21,41,66], [21,31,22], [10,61,25],[1,100,100],[100,150,100],[100,10,1000]]]
+        [200, 1, 100, 150, [[199, 200, 100], [2, 6, 6], [6, 3, 7], [4, 6, 7], [6, 5, 11], [2, 5, 12], [5, 3, 20], [20, 40, 8],[40, 30, 9], [50, 70, 9], [40, 60, 4], [30, 60, 1], [30, 20, 3], [20, 10, 6], [41, 11, 10], [31, 51, 24],[51, 61, 2], [31, 11, 41], [51, 10, 24], [41, 60, 50], [21, 41, 66], [21, 31, 22], [10, 61, 25],[1, 100, 100], [100, 150, 100], [100, 10, 1000]]]
     ]
     t = Timer(f"for t in {query}: solution(*t)", "from __main__ import solution")
-    print(t.timeit(number=1000))
+    print(t.timeit(number=1))
