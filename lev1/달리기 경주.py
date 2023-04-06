4b9242505c7abb891bdbfa23a356f6f96275096d
daewoong lee <dw.lee832@gmail.com>
2023-04-06 17:56:18 +0900

    달리기 경주 문제풀이 01 - code refactoring

--- lev1/달리기 경주.py
+++ lev1/달리기 경주.py
@@ -1,35 +1,39 @@
 def solution(players, callings):
-    player_to_idx = dict()
-    idx_to_player = dict()
-    for idx, player in enumerate(players):
-        player_to_idx[player] = idx
-        idx_to_player[idx] = player
+    # # 0.102966658
+    # player_to_idx = dict()
+    # idx_to_player = list()
+    # for idx, player in enumerate(players):
+    #     player_to_idx[player] = idx
+    #     idx_to_player.append(player)
+    #
+    # for tracker in callings:
+    #     idx = player_to_idx[tracker]
+    #     lead_player = idx_to_player[idx-1]
+    #     player_to_idx[lead_player], player_to_idx[tracker] = idx, idx - 1
+    #     idx_to_player[idx], idx_to_player[idx - 1] = lead_player, tracker
+    # return idx_to_player
 
-    for calling_player in callings:
-        idx = player_to_idx[calling_player]
-        lead_player = idx_to_player[idx-1]
-        player_to_idx[lead_player] = idx
-        player_to_idx[calling_player] = idx - 1
-        idx_to_player[idx] = lead_player
-        idx_to_player[idx - 1] = calling_player
-
-    return list(idx_to_player.values())
-
-
-"""
-callings에 있는 명칭이 있을 때 players 인덱스 -1, 1등은 불리지 않음 (조건 고려X)
-
-player_to_idx={player: idx}
-idx_to_player={idx: player}
-for callings 돌면서
-    idx = player_to_idx[calling_player]
-    lead_player = idx_to_player[idx-1]
-    player_to_idx[lead_player] = idx # 선두 idx:이름 update
-    player_to_idx[calling_player] = idx-1 # 후발 idx:이름 update
-    idx_to_player[idx] = lead_player # 선두 이름:idx update
-    idx_to_player[idx-1] = calling_player # 후발 이름:idx update
-"""
+    # 0.07918229900000001
+    player_to_idx = {player: i for i, player in enumerate(players)}
+    for tracker in callings:
+        idx = player_to_idx[tracker]
+        lead_player = players[idx-1]
+        player_to_idx[lead_player], player_to_idx[tracker] = idx, idx-1
+        players[idx-1], players[idx] = tracker, lead_player
+    return players
 
 
 print(solution(["mumu", "soe", "poe", "kai", "mine"], ["kai", "kai", "mine", "mine"])) #["mumu", "kai", "mine", "soe", "poe"]
 print(solution(["aaa","bbb","ccc","ddd","eee"], ["eee","ddd","ccc","bbb"])) #["aaa","bbb","ccc","ddd","eee"]
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [
+        [["mumu", "soe", "poe", "kai", "mine"], ["kai", "kai", "mine", "mine"]],
+        [["aaa","bbb","ccc","ddd","eee"], ["eee","ddd","ccc","bbb"]],
+        [["aaa","bbb","ccc","ddd","eee"], ["eee","eee","eee","eee","ddd","ddd","ddd","ddd","ccc","ccc","ccc","ccc","bbb","bbb","bbb","bbb"]],
+        [["aaa","bbb","ccc","ddd","eee"], ["bbb","ccc"]]
+    ]
+    t = Timer(f"for t in {query}: solution(*t)", "from __main__ import solution")
+    print(t.timeit(number=10000))
