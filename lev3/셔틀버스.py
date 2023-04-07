7030da817238e9c35c3c7747b87b4f6d78e64752
daewoong lee <dw.lee832@gmail.com>
2023-04-07 22:36:02 +0900

    셔틀버스 문제풀이 01 - code refactoring

--- lev3/셔틀버스.py
+++ lev3/셔틀버스.py
@@ -9,36 +9,18 @@ def min_to_datetime(time_min):
 
 
 def solution(n, t, m, timetable):
+    # 0.55701053
     timetable.sort(reverse=True)
-    timetable = [datetime_to_min(tt) for tt in timetable]
 
-    bus_time = 9 * 60
-    for _ in range(n): # 0 < n이기 때문에 for loop는 반드시 구동 (waiting_line 변수는 반드시 존재하게 됨)
+    for i in range(n): # 0 < n이기 때문에 for loop는 반드시 구동 (waiting_line, bus_time 변수는 반드시 존재)
         cnt = 0
         waiting_line = []
-        while timetable and timetable[-1] <= bus_time and cnt < m:
+        bus_time = 9 * 60 + i * t
+        while timetable and datetime_to_min(timetable[-1]) <= bus_time and cnt < m:
             waiting_line.append(timetable.pop())
             cnt += 1
-        bus_time += t
 
-    print(waiting_line)
-    return min_to_datetime(9 * 60 + (n-1) * t) if len(waiting_line) < m else min_to_datetime(waiting_line[-1]-1)
-
-
-"""
-가장 늦게 출발하는 셔틀 타기
-
-1. 09:00 기준부터 셔틀 출발하니깐 주어진 N & t 기준으로 갈 수 있는 가장 늦은 셔틀 시간대 계산
-    - last_shuttle_bus = 09 * 60 + (n-1) * t (계산 편의를 위해 분 단위로 환산)
-2. sort timetable
-3. 막차 시간까지 대기열 & 현재 시간 갱신
-    - loop n만큼
-        while timetable[i] < t시 & len(list) < m -> list append timetable[i]
-        else -> t += 1 & init list    
-4. 막차 시간에 대기열 탑승 가능 인원
-    if 탑승 인원 < m -> 막차 시간 반환
-    else -> 탑승 인원 리스트[m-1]-1 시간 반환
-"""
+    return min_to_datetime(bus_time) if len(waiting_line) < m else min_to_datetime(datetime_to_min(waiting_line[-1])-1)
 
 
 # print(solution(1,1,5,["08:00", "08:01", "08:02", "08:03"])) #"09:00"
@@ -48,3 +30,19 @@ def solution(n, t, m, timetable):
 # print(solution(1,1,1,["23:59"])) #"09:00"
 # print(solution(10,60,45,["23:59","23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59"])) #"18:00"
 print(solution(3,10,3,["09:00", "09:00", "09:00", "09:10", "09:10", "09:10", "09:20", "09:20", "09:20", "09:20"])) #"09:19"
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [
+        [1,1,5,["08:00", "08:01", "08:02", "08:03"]],
+        [2,10,2,["09:10", "09:09", "08:00"]],
+        [2,1,2,["09:00", "09:00", "09:00", "09:00"]],
+        [1,1,5,["00:01", "00:01", "00:01", "00:01", "00:01"]],
+        [1,1,1,["23:59"]],
+        [10,60,45,["23:59","23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59"]],
+        [3,10,3,["09:00", "09:00", "09:00", "09:10", "09:10", "09:10", "09:20", "09:20", "09:20", "09:20"]],
+        [10,60,45,["09:00"]*2000]
+    ]
+    t = Timer(f"for t in {query}: solution(*t)", "from __main__ import solution")
+    print(t.timeit(number=1000))
