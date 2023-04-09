83dd76bc1a95bfb99346c4a011ecda3a4a1e6d2f
daewoong lee <dw.lee832@gmail.com>
2023-04-09 23:15:58 +0900

    연속된 부분 수열의 합 문제풀이 01 - code refactoring

--- lev2/연속된 부분 수열의 합.py
+++ lev2/연속된 부분 수열의 합.py
@@ -1,39 +1,39 @@
 def solution(sequence, k):
-    ans = []
+    # # 0.244592551
+    # ans = [[], float("inf")]
+    # N = len(sequence)
+    # i = 0
+    # j = 1
+    # total = sequence[0]
+    # while i < N:
+    #     if total < k and j < N:
+    #         total += sequence[j]
+    #         j += 1
+    #     else:
+    #         if total == k and j - i < ans[1]:
+    #             ans = [[i, j - 1], j-i]
+    #         total -= sequence[i]
+    #         i += 1
+    #         if i >= j and j < N:
+    #             if total == k and j - i < ans[1]:
+    #                 ans = [[i, j - 1], j-i]
+    #             total += sequence[j]
+    #             j += 1
+    # return ans[0]
+
+    # code refactoring - 0.21695578499999998
+    ans = [[], float("inf")]
     N = len(sequence)
-    i = 0
-    j = 1
-    total = sequence[0]
-    while i < N:
-        print(f"i: {i}, j: {j}, sum: {total}")
-        if total < k and j < N:
+    j = 0
+    total = 0
+    for i in range(N):
+        while total < k and j < N:
             total += sequence[j]
             j += 1
-        else:
-            if total == k:
-                ans.append([j-i, [i, j-1]])
-            total -= sequence[i]
-            i += 1
-            if i >= j and j < N:
-                if total == k:
-                    ans.append([j-i, [i, j-1]])
-            #     print(f"jj: {j}")
-                total += sequence[j]
-                j += 1
-    ans.sort(key=lambda x: (x[0], x[1][0]))
-    print(ans)
-    return ans[0][1]
-
-"""
-max k에 해당하는 가장 짧은 구간 구하기
-
-1. k에 해당하는 모든 구간 탐색
-    1-1. sum(i ~ j) < k라면 -> j += 1
-    1-2. sum(i ~ j) > k라면 -> i -= 1
-    1-3. sum(i ~ j) == k라면 -> append ans 
-2. 1번에서 구한 모든 구간 중 길이가 가장 짧은 경우 반환
-    2-1 return min len <- ans 
-"""
+        if total == k and j - i < ans[1]:
+            ans = [[i, j - 1], j-i]
+        total -= sequence[i]
+    return ans[0]
 
 
 # print(solution([1, 2, 3, 4, 5], 7)) #[2, 3]
@@ -42,3 +42,18 @@ max k에 해당하는 가장 짧은 구간 구하기
 # print(solution([1,5,2,3,4,5,1,2,3,4,5], 6)) #[0 ,1]
 # print(solution([5,4,3,2,1,2,3,4,5], 6)) #[2,4]
 print(solution([7,8,9,10,1,2,3,4,5], 6)) #[4,6]
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [
+        [[1, 2, 3, 4, 5], 7],
+        [[1, 1, 1, 2, 3, 4, 5], 5],
+        [[2, 2, 2, 2, 2], 6],
+        [[1,5,2,3,4,5,1,2,3,4,5], 6],
+        [[5,4,3,2,1,2,3,4,5], 6],
+        [[7,8,9,10,1,2,3,4,5], 6],
+        [[1,2,3,4,1,5,6,1,7,8,9,10,9,8,7,6,1,5,1,4,1,3,2,1,1,1,1,1,1,1],5]
+    ]
+    t = Timer(f"for t in {query}: solution(*t)", "from __main__ import solution")
+    print(t.timeit(number=10000))
