8394052a30dc1e7e6cec0edf7b22b0db97eea097
daewoong lee <dw.lee832@gmail.com>
2023-04-11 17:08:57 +0900

    경주로 건설 문제풀이 01 - code refactoring

--- lev3/경주로 건설.py
+++ lev3/경주로 건설.py
@@ -3,40 +3,62 @@ from collections import deque
 
 def solution(board):
     def bfs(x, y):
+        ans = float("inf")
         search = deque([[x, y, None]])
-        for i in range(4):
+        for i in range(len(move)):
             cost_map[x][y][i] = 0
 
         while search:
             x, y, d = search.popleft()
-            print(f"x: {x}, y: {y}, d: {d}")
             if x == N and y == N:
+                if ans > cost_map[x][y][d]:
+                    ans = cost_map[x][y][d]
                 continue
 
             for z, (mx, my) in enumerate(move):
-                nx, ny = x+mx, y+my
-                if 0 <= nx <= N and 0 <= ny <= N and not visited_map[nx][ny][z]:
-                    if d in [0, 1]: # R, L
-                        move_cost = 100 if z in [0, 1] else 600
-                    elif d in [2, 3]: # U, D
-                        move_cost = 100 if z in [2, 3] else 600
-                    else:
+                nx, ny = x + mx, y + my
+                if 0 <= nx <= N and 0 <= ny <= N and not board[nx][ny]:
+                    if d is None:
                         move_cost = 100
+                    else:
+                        move_cost = cost_map[x][y][d] + 100 if (d-z) % 2 == 0 else cost_map[x][y][d] + 600
 
-                    if cost_map[nx][ny][z] > cost_map[x][y][d]+move_cost if d is not None else 100:
+                    if cost_map[nx][ny][z] > move_cost:
                         search.append([nx, ny, z])
-                        cost_map[nx][ny][z] = min(cost_map[nx][ny][z], cost_map[x][y][d]+move_cost if d is not None else 100)
+                        cost_map[nx][ny][z] = move_cost
+        return ans
 
-        # for cm in cost_map:
-        #     print(cm)
+    def bfs1(x, y):
+        search = deque([[x, y, -1, 0]]) # x, y, dir, cost
+        ans = float("inf")
+        while search:
+            x, y, d, c = search.popleft()
+            for z, (mx, my) in enumerate(move):
+                nx, ny = x + mx, y + my
+                if 0 <= nx <= N and 0 <= ny <= N and not board[nx][ny]:
+                    cost = c + 100 if d == -1 or (d - z) % 2 == 0 else c + 600
+
+                    if nx == N and ny == N and ans > cost:
+                        ans = cost
+                    elif cost_map[nx][ny][z] > cost:
+                        cost_map[nx][ny][z] = cost
+                        search.append([nx, ny, z, cost])
+
+        return ans
 
     N = len(board)-1
-    move = [[0,1], [0,-1], [1,0], [-1,0]] # R L D U
-    visited_map = [[[0 for _ in range(len(move))] if r == 0 else [1 for _ in range(len(move))] for r in row] for row in board]
+    move = [[-1, 0], [0, -1], [1, 0], [0, 1]]  # U L D R
     cost_map = [[[float("inf")] * len(move) for _ in row] for row in board]
-    bfs(0, 0)
 
-    return min(cost_map[N][N][i] for i in range(len(move)))
+    # # code refactoring 01 - 0.872127471
+    # return bfs(0, 0)
+
+    # code refactoring 02 - 0.899766382
+    return bfs1(0, 0)
+
+
+
+
 
 
 """
@@ -53,6 +75,21 @@ dfs 길찾기, 출발점에서 도착점 경로는 항상 가능하도록 주어
 """
 
 # print(solution([[0,0,0],[0,0,0],[0,0,0]])) #900
-print(solution([[0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,0],[0,0,0,1,0,0,0,1],[0,0,1,0,0,0,1,0],[0,1,0,0,0,1,0,0],[1,0,0,0,0,0,0,0]])) #3800
+# print(solution([[0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,0],[0,0,0,1,0,0,0,1],[0,0,1,0,0,0,1,0],[0,1,0,0,0,1,0,0],[1,0,0,0,0,0,0,0]])) #3800
 # print(solution([[0,0,1,0],[0,0,0,0],[0,1,0,1],[1,0,0,0]])) #2100
 # print(solution([[0,0,0,0,0,0],[0,1,1,1,1,0],[0,0,1,0,0,0],[1,0,0,1,0,1],[0,1,0,0,0,1],[0,0,0,0,0,0]])) #3200
+print(solution([[0,0,0,0,0,0,0,0],[1,0,1,1,1,1,1,0],[1,0,0,1,0,0,0,0],[1,1,0,0,0,1,1,1],[1,1,1,1,0,0,0,0],[1,1,1,1,1,1,1,0],[1,1,1,1,1,1,1,0],[1,1,1,1,1,1,1,0]])) #4500
+
+
+if __name__ == "__main__":
+    from timeit import Timer
+    query = [
+        [[0,0,0],[0,0,0],[0,0,0]],
+        [[0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,0],[0,0,0,1,0,0,0,1],[0,0,1,0,0,0,1,0],[0,1,0,0,0,1,0,0],[1,0,0,0,0,0,0,0]],
+        [[0,0,1,0],[0,0,0,0],[0,1,0,1],[1,0,0,0]],
+        [[0,0,0,0,0,0],[0,1,1,1,1,0],[0,0,1,0,0,0],[1,0,0,1,0,1],[0,1,0,0,0,1],[0,0,0,0,0,0]],
+        [[0,0,0,0,0,0,0,0],[1,0,1,1,1,1,1,0],[1,0,0,1,0,0,0,0],[1,1,0,0,0,1,1,1],[1,1,1,1,0,0,0,0],[1,1,1,1,1,1,1,0],[1,1,1,1,1,1,1,0],[1,1,1,1,1,1,1,0]],
+        [[0]*25 for _ in range(25)]
+    ]
+    t = Timer(f"for t in {query}: solution(t)", "from __main__ import solution")
+    print(t.timeit(number=100))
