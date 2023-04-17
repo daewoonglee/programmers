b1ceb4226db9490f9a7b1cc43b5a286cc0f5318e
daewoong lee <dw.lee832@gmail.com>
2023-04-17 16:34:47 +0900

    두 원 사이의 정수 쌍 문제풀이 02 - 풀이X (2/10)

--- lev2/두 원 사이의 정수 쌍.py
+++ lev2/두 원 사이의 정수 쌍.py
@@ -1,8 +1,22 @@
 def solution(r1, r2):
     def cnt_coords(r):
-        return r**2 if ((r-1)**2)*2 <= r**2 else r**2-1
-    coords = cnt_coords(r2)-cnt_coords(r1)-(r2-r1)*2 # (x,0), (0,y) 제외
-    return coords*4 + (r2-r1+1)*4
+        total = r_square = r*r
+        for x in range(r-1, -1, -1):
+            cnt = 0
+            y = r-1
+            while r_square < x**2 + y**2:
+                cnt += 1
+                y -= 1
+            if cnt == 0:
+                break
+            total -= cnt
+        return total
+
+    # r2-r1+1 = 1사분면에 해당하는 좌표 수, 4사분면에 대해 모두 카운트하여 *4하면 중복 계산
+    # 중복 계산을 없애고자 1사분면에 해당하는 좌표를 제외하고 *4 진행
+    print(cnt_coords(r2))
+    print(cnt_coords(r1))
+    return (cnt_coords(r2)+2-cnt_coords(r1)-(r2-r1+1))*4
 
 
 """
@@ -17,4 +31,8 @@ def solution(r1, r2):
 # print(solution(1, 3)) # 28
 # print(solution(2, 4)) # 40
 # print(solution(3, 4)) # 24
-print(solution(2, 5)) # 72
+# print(solution(2, 5)) # 72
+# print(solution(2, 6)) # 104
+print(solution(6, 7)) # 40
+
+
