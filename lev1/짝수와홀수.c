120d7c4a3671c2aa926117115a211be131131d60
daewoong lee <dw.lee832@gmail.com>
2023-04-25 14:26:05 +0900

    짝수와 홀수 문제풀이01 - code refactoring

--- lev1/짝수와홀수.c
+++ lev1/짝수와홀수.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <time.h>
 
 char* solution(int num) {
-    char* answer = (char*)malloc(4);
-    if (num % 2 == 0)
-        answer = "Even";
-    else
-        answer = "Odd";
+    // // 0.017163
+    // char* answer = (char*)malloc(4);
+    // if (num % 2 == 0)
+    //     answer = "Even";
+    // else
+    //     answer = "Odd";
+
+    // code refactoring - 0.014269
+    int d = num%2;
+    char* answer = (char*)malloc(sizeof(char)*(5-d));
+    answer = d ? "Odd" : "Even";
     return answer;
     
-}
+};
 
 int main(){
-    printf("%s\n", solution(3));
-    printf("%s\n", solution(2147483647));
+    clock_t tic = clock();
+    for (int i=0; i<100000; i++){
+        solution(3);
+        solution(2147483647);
+        solution(2147483646);
+    }
+    clock_t toc = clock();
+    printf("running time: %f\n", (double)(toc-tic) / CLOCKS_PER_SEC);
     return 0;
 }
 
