7a048c74642152c54c061f4cc22fc62f38a436dc
daewoong lee <dw.lee832@gmail.com>
2023-04-29 04:17:20 +0900

    나머지가 1이 되는 수 찾기 문제풀이 01 - code refactoring

--- lev1/나머지가_1이_되는_수_찾기.c
+++ lev1/나머지가_1이_되는_수_찾기.c
@@ -1,32 +1,51 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <time.h>
 
 int solution(int n) {
-    // get prime number using Eratosthenes algorithm.
-    bool prime[n+2]; // 소수2를 인덱스 시작 2로 설정하기 위해 +2
-    memset(prime, true, sizeof(prime));
+    // // 0.721268
+    // // get prime number using Eratosthenes algorithm.
+    // bool prime[n+2]; // 소수2를 인덱스 시작 2로 설정하기 위해 +2
+    // memset(prime, true, sizeof(prime));
 
-    for(int i=2; i*i<n; i++){
-        if(prime[i]){
-            for(int j=i*i; j<n; j+=i)
-                prime[j] = false;
+    // for(int i=2; i*i<n; i++){
+    //     if(prime[i]){
+    //         for(int j=i*i; j<n; j+=i)
+    //             prime[j] = false;
+    //     }
+    // }
+
+    // for(int i=2; i<n; i++){
+    //     if(prime[i]){
+    //         if(n%i==1)
+    //             return i;
+    //     }
+    // }
+    
+    // return 0;
+
+    // 0.000011
+    // n이 작아 소수를 찾는 행위보다 탐색이 더 빠름
+    int ans = 0;
+    for(int i=2;i<n;i++){
+        if(n%i==1){
+            return i;
         }
     }
-
-    for(int i=2; i<n; i++){
-        if(prime[i]){
-            if(n%i==1)
-                return i;
-        }
-    }
-    
-    
-    return 0;
+    return ans;
 }
 
 int main(){
-    printf("%d\n", solution(10)); //3
-    printf("%d\n", solution(12)); //11
+    clock_t tic = clock();
+    for (int i=0; i<100; i++){
+        solution(3);
+        solution(10);
+        solution(12);
+        solution(1000000);
+        solution(999999);
+    }
+    clock_t toc = clock();
+    printf("running time: %f\n", (double)(toc-tic) / CLOCKS_PER_SEC);
     return 0;
 }
