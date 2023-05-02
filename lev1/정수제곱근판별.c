bf40b94a287ccd6a0aac04f6fa315ba83b7dae42
daewoong lee <dw.lee832@gmail.com>
2023-05-03 07:23:56 +0900

    정수 제곱근 판별 문제풀이 01 - code refactoring

--- lev1/정수제곱근판별.c
+++ lev1/정수제곱근판별.c
@@ -2,17 +2,30 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
 long long solution(long long n) {
-    double N = sqrt(n);
-    return (N == (int)N) ? (long long)pow(N+1, 2) : -1;
+    // // 0.010661
+    // double N = sqrt(n);
+    // return (N == (int)N) ? (long long)pow(N+1, 2) : -1;
+
+    // 0.009822
+    long long N = sqrtl(n); // return long long
+    return (N*N == n) ? (long long)(N+1)*(N+1) : -1;
 }
 
 
 int main(){
-    printf("%lld\n", solution(121)); //144
-    printf("%lld\n", solution(3)); //-1
-    printf("%lld\n", solution(4000000)); //4004001
-    printf("%lld\n", solution(7)); //-1
+    clock_t tic = clock();
+    for (int i=0; i<100000; i++){
+        solution(121);
+        solution(3);
+        solution(4000000);
+        solution(7);
+        solution(2147483646);
+        solution(50000000000000);
+    }
+    clock_t toc = clock();
+    printf("running time: %f\n", (double)(toc-tic) / CLOCKS_PER_SEC);
     return 0;
 }
