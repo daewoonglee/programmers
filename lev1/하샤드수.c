5fd90a3e6df41d0a2c01ee59491092720e5b0d32
daewoong lee <dw.lee832@gmail.com>
2023-05-07 22:42:10 +0900

    하샤드 수 문제풀이 01 - code refactoring

--- lev1/하샤드수.c
+++ lev1/하샤드수.c
@@ -1,25 +1,43 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <time.h>
+
+
+int sum(int x) {
+    if(x<10) return x%10;   
+    else return x%10 + sum(x/10); 
+}
 
 bool solution(int x) {
-    int n = x;
-    int harshad_n = 0;
-    while(n>0){
-        harshad_n += n%10;
-        n /= 10;
-        // printf("x: %d, h: %d, n: %d\n", x, harshad_n, n);
-    }
-    return x % harshad_n == 0 ? true : false;
+    // // 0.001444
+    // int n = x;
+    // int harshad_n = 0;
+    // while(n>0){
+    //     harshad_n += n%10;
+    //     n /= 10;
+    //     // printf("x: %d, h: %d, n: %d\n", x, harshad_n, n);
+    // }
+    // return x % harshad_n == 0 ? true : false;
+
+    // code refactoring - recursive, 0.001838
+    return (x%sum(x) == 0);
 }
 
 
+
+
 int main(){
-    printf("%d\n", solution(10)); //true
-    printf("%d\n", solution(12)); //true
-    printf("%d\n", solution(11)); //false
-    printf("%d\n", solution(13)); //false
-    printf("%d\n", solution(10000)); //true
-    printf("%d\n", solution(9999)); //false
+    clock_t tic = clock();
+    for (int i=0; i<10000; i++){
+        solution(10);
+        solution(12);
+        solution(11);
+        solution(13);
+        solution(10000);
+        solution(9999);
+    }
+    clock_t toc = clock();
+    printf("running time: %f\n", (double)(toc-tic) / CLOCKS_PER_SEC);
     return 0;
 }
