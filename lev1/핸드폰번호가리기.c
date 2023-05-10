6e71bd7e674bb8f13380716382486f5498db12c7
daewoong lee <dw.lee832@gmail.com>
2023-05-11 07:51:15 +0900

    핸드폰 번호 가리기 문제풀이 01 - code refactoring

--- lev1/핸드폰번호가리기.c
+++ lev1/핸드폰번호가리기.c
@@ -2,20 +2,36 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 char* solution(const char* phone_number) {
-    int N = strlen(phone_number); 
+    
+    const int N = strlen(phone_number);
     char* ans = (char*)malloc(sizeof(char) * (N+1)); // \0
-    char* hide_number = "****************";
     strcpy(ans, phone_number);
-    strncpy(ans, hide_number, N-4);
+
+    // // 0.002742
+    // char* hide_number = "****************";
+    // strncpy(ans, hide_number, N-4);
+
+    // // 0.002520
+    for(int i=0; i<N-4; i++)
+        ans[i] = '*';
+
     return ans;
 }
 
 
 int main(){
-    printf("%s\n", solution("01033334444")); //"*******4444"
-    printf("%s\n", solution("027778888")); //"*****8888"
+    clock_t tic = clock();
+    for (int i=0; i<10000; i++){
+        solution("01033334444");
+        solution("027778888");
+        solution("1234");
+        solution("11111111111111111111");
+    }
+    clock_t toc = clock();
+    printf("running time: %f\n", (double)(toc-tic) / CLOCKS_PER_SEC);
     return 0;
 }
