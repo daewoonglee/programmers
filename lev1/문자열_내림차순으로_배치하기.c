e5659745a1ef65c7a11b1c607d71ae5550fed472
daewoong lee <dw.lee832@gmail.com>
2023-05-22 07:37:30 +0900

    문자열 내림차순으로 배치하기 문제풀이 01 - code refactoring

--- lev1/문자열_내림차순으로_배치하기.c
+++ lev1/문자열_내림차순으로_배치하기.c
@@ -2,29 +2,80 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+int compare(const void *a, const void *b){
+    return -(strcmp((char *)a, (char *)b));
+};
+
+void quick_sort(char arr[], int L, int R){
+    int left = L;
+    int right = R;
+    int pivot = arr[(L + R) / 2];
+
+    while(left <= right){
+        while(arr[left] > pivot)
+            left++;
+        while(arr[right] < pivot)
+            right--;
+        if(left <= right){
+            char tmp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = tmp;
+
+            left++;
+            right--;
+        }
+    }
+
+    if(L < right)
+        quick_sort(arr, L, right);
+    if(left < R)
+        quick_sort(arr, left, R);
+}
 
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 char* solution(const char* s) {
+    // // 3.919274
+    // const int N = strlen(s);
+    // char* ans = (char*)malloc(sizeof(char)*(N+1)); //'\0'
+    // strcpy(ans, s);
+    // for(int i=0; i<N; i++){
+    //     for(int j=i+1; j<N; j++){
+    //         if(ans[i] < ans[j]){
+    //             char t = ans[i];
+    //             ans[i] = ans[j];
+    //             ans[j] = t;
+    //         }
+    //     } 
+    // }
+    // return ans;
+
+    // // code refactoring - 0.191321(quicksort)
+    // const int N = strlen(s);
+    // char* ans = (char*)malloc(sizeof(char)*(N+1));
+    // strcpy(ans, s);
+    // quick_sort(ans, 0, N-1);
+
+    // code refactoring - 0.629741(stdlib function)
     const int N = strlen(s);
-    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    char* ans = (char*)malloc(sizeof(char)*(N+1)); //'\0'
+    char* ans = (char*)malloc(sizeof(char)*(N+1));
     strcpy(ans, s);
-    for(int i=0; i<N; i++){
-        for(int j=i+1; j<N; j++){
-            if(ans[i] < ans[j]){
-                char t = ans[i];
-                ans[i] = ans[j];
-                ans[j] = t;
-            }
-        } 
-    }
+    qsort(ans, N, sizeof(char), compare);
     return ans;
 }
 
 
 int main(){
-    // printf("%s\n", solution("Zbcdefg")); //"gfedcbZ"
-    printf("%s\n", solution("cdbefZg")); //"gfedcbZ"
-
+    clock_t tic = clock();
+    for (int i=0; i<10000; i++){
+        solution("cdbefZg");
+        solution("Zbcdefg");
+        solution("ZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefg");
+        solution("cdbefZgcdbefZgcdbefZgcdbefZgcdbefZgcdbefZgcdbefZgcdbefZgcdbefZg");
+        solution("ZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefgZbcdefg");
+    }
+    clock_t toc = clock();
+    printf("running time: %f\n", (double)(toc-tic) / CLOCKS_PER_SEC);
     return 0;
 }
