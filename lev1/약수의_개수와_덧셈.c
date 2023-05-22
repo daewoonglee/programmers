42e94dc25e1c56f956b0d85b70cd8bf5d9facc92
daewoong lee <dw.lee832@gmail.com>
2023-05-23 07:18:51 +0900

    약수의 개수와 덧셈 문제풀이 01 - code refactoring

--- lev1/약수의_개수와_덧셈.c
+++ lev1/약수의_개수와_덧셈.c
@@ -2,27 +2,51 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
+
+int sum_natural_number(int x){
+    return (x * (x + 1) / 2);
+}
+
+int sum_square_number(int x){
+    return (x * (x + 1) * (2 * x + 1) / 6);
+}
 
 int solution(int left, int right) {
+    // int ans = 0;
+    // for(int i=left; i<=right; i++){
+    //     bool is_div = false;
+    //     // 2.643107
+    //     // for(int j=1; j<=(int)sqrt(i)+1; j++){
+    //     // code refactoring - 1.376362(without callback function (sqrt))
+    //     for(int j=1; j*j<=i; j++){
+    //         if(i%j==0 & j*j==i){
+    //             is_div = true;
+    //             break;
+    //         }
+    //     }
+    //     ans += is_div ? -i : i;
+    // }
+    // return ans;
+    
+    // code refactoring - 0.001346
     int ans = 0;
-    for(int i=left; i<=right; i++){
-        bool is_div = false;
-        for(int j=1; j<(int)sqrt(i)+1; j++){
-            if(i%j==0 & j*j==i){
-                is_div = true;
-                break;
-            }
-        }
-        // printf("i: %d, div: %d\n", i, div);
-        ans += is_div ? -i : i;
-    }
+    ans += sum_natural_number(right) - sum_natural_number(left - 1);
+    ans -= 2 * (sum_square_number(sqrt(right)) - sum_square_number(sqrt(left - 1)));
     return ans;
 }
 
 
 int main(){
-    printf("%d\n", solution(13, 17)); //43
-    printf("%d\n", solution(24, 27)); //52
-    printf("%d\n", solution(1, 1)); //-1
+    clock_t tic = clock();
+    for (int i=0; i<10000; i++){
+        solution(13, 17);
+        solution(24, 27);
+        solution(1, 1);
+        solution(1,1000);
+        solution(1,997);
+    }
+    clock_t toc = clock();
+    printf("running time: %f\n", (double)(toc-tic) / CLOCKS_PER_SEC);
     return 0;
 }
