1cfbaf2352f98c7630c4d26c0e386d24ff20a3b8
daewoong lee <dw.lee832@gmail.com>
2023-05-25 07:31:29 +0900

    부족한 금액 계산하기 문제풀이 01 - code refactoring

--- lev1/부족한_금액_계산하기.c
+++ lev1/부족한_금액_계산하기.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <time.h>
 
 long long solution(int price, int money, int count) {
-    long long s = count*(price+price * count)/2;
-    long long ans = money-s;
-    return ans < 0 ? llabs(ans) : 0;
+    // // 0.000415
+    // // long long sn = money-(count*(price+price * count)/2); //memory error
+    // long long n = (price+price*count);
+    // long long sn = count*n/2;
+    // long long ans = money-sn;
+
+    // code refactoring - 0.000184
+    // long long ans = money-((count + 1) * count / 2 * price); // memory error
+    long long n = (count+1)*count/2;
+    long long sn = price*n;
+    long long ans = money-sn;
+
+    return ans < 0? ans*-1 : 0;
 }
 
 
 int main(){
     printf("%lld\n", solution(3, 20, 4)); //10
-    printf("%lld\n", solution(1, 30, 2500)); //10
+    clock_t tic = clock();
+    for (int i=0; i<10000; i++){
+        solution(3, 20, 4);
+        solution(2500, 100, 2500);
+        solution(2500, 1000000000, 2500);
+    }
+    clock_t toc = clock();
+    printf("running time: %f\n", (double)(toc-tic) / CLOCKS_PER_SEC);
     return 0;
 }
