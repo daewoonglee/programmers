dda2f9c30596ec4310e309baefd37e6dc743a3a8
daewoong lee <dw.lee832@gmail.com>
2023-05-30 07:15:00 +0900

    문자열 다루기 기본 문제풀이 01 - code refactoring

--- lev1/문자열_다루기_기본.c
+++ lev1/문자열_다루기_기본.c
@@ -2,15 +2,29 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <ctype.h>
 
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 bool solution(const char* s) {
-    int N = strlen(s);
-    int zero = (int)'0';
+    // // 0.000830
+    // int N = strlen(s);
+    // int zero = (int)'0';
 
+    // if(N == 4 || N == 6){
+    //     for(int i=0; i<N; i++){
+    //         if(zero>s[i] || zero+9<s[i])
+    //             return false;
+    //     }
+    //     return true;
+    // }
+    // return false;
+
+    // using ctype-digit function - 0.001284
+    int N = strlen(s);
     if(N == 4 || N == 6){
         for(int i=0; i<N; i++){
-            if(zero>s[i] || zero+9<s[i])
+            if(!isdigit(s[i]))
                 return false;
         }
         return true;
@@ -20,8 +34,17 @@ bool solution(const char* s) {
 
 
 int main(){
-    printf("%d\n", solution("a234")); //false
-    printf("%d\n", solution("1234")); //true
-    printf("%d\n", solution("01234567")); //false
+    clock_t tic = clock();
+    for (int i=0; i<10000; i++){
+        solution("a234");
+        solution("1234");
+        solution("01234567");
+        solution("012347");
+        solution("0ZZZ");
+        solution("0123Z7");
+        solution("654321");
+    }
+    clock_t toc = clock();
+    printf("running time: %f\n", (double)(toc-tic) / CLOCKS_PER_SEC);
     return 0;
 }
