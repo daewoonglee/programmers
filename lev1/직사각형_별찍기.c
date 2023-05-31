d1e6127483b4448aec5503f8e4beb9dbb8909499
daewoong lee <dw.lee832@gmail.com>
2023-06-01 06:45:01 +0900

    직사각형 별찍기 문제풀이 01 - code refactoring

--- lev1/직사각형_별찍기.c
+++ lev1/직사각형_별찍기.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
+#include <time.h>
+
+void solution(int a, int b){
+    // // 0.123384
+    // for(int i=0; i<b; i++){
+    //     for(int j=0; j<a; j++)
+    //         printf("*");
+    //     printf("\n");
+    // }
+
+    // 0.124000 - O(n+m)
+    for(int i=0; i<a*b; i++){
+        printf("*");
+        if((i+1)%a==0)
+            printf("\n");
+    }
+}
 
 int main(void) {
-    int a;
-    int b;
-    scanf("%d %d", &a, &b);
-    for(int i=0; i<b; i++){
-        for(int j=0; j<a; j++)
-            printf("*");
-        printf("\n");
-    }
+    clock_t tic = clock();
+    solution(5, 3);
+    solution(1000, 3);
+    solution(1000, 1000);
+    clock_t toc = clock();
+    printf("running time: %f\n", (double)(toc-tic) / CLOCKS_PER_SEC);
     return 0;
 }
