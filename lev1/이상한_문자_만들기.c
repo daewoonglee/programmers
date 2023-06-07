5e92aff3b8e6aec2a11203bcdbab2e9a725b8756
daewoong lee <dw.lee832@gmail.com>
2023-06-07 22:02:27 +0900

    이상한 문자 만들기 문제풀이 01 - code refactoring

--- lev1/이상한_문자_만들기.c
+++ lev1/이상한_문자_만들기.c
@@ -2,39 +2,67 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 char* solution(const char* s) {
-    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     int N = strlen(s);
     char* ans = (char*)malloc(sizeof(char)*(N+1)); // add '\0'
+    
+    // // 0.002256
+    // int start = 'a';
+    // int end = 'z';
+    // int diff = 'a'-'A';
+    // int space = ' ';
+    // int j = 0;
+    // for(int i=0; i<N; i++){
+    //     if(s[i]==space){
+    //         j=0;
+    //         ans[i] = s[i];
+    //     }
+    //     else{
+    //         if(j++%2==0)
+    //             ans[i] = (start<=s[i] && s[i]<=end) ? s[i]-diff : s[i];
+    //         else
+    //             ans[i] = (start<=s[i] && s[i]<=end) ? s[i] : s[i]+diff;
+    //     }
+    // }
 
-    int start = 'a';
-    int end = 'z';
-    int diff = 'a'-'A';
-    int space = ' ';
+    // code refactoring - 0.002050
     int j = 0;
     for(int i=0; i<N; i++){
-        if(s[i]==space){
+        if(s[i]==' '){
             j=0;
             ans[i] = s[i];
         }
         else{
             if(j++%2==0)
-                ans[i] = (start<=s[i] & s[i]<=end) ? s[i]-diff : s[i];
+                ans[i] = (s[i]&32) ? s[i]^32 : s[i];
             else
-                ans[i] = (start<=s[i] & s[i]<=end) ? s[i] : s[i]+diff;
+                ans[i] = (s[i]&32) ? s[i] : s[i]|32;
         }
     }
+
     ans[N]='\0';
     return ans;
 }
 
 int main(){
-    // solution("try hello world"); //"TrY HeLlO WoRlD"
-    solution("t r y"); //"T R Y"
-    solution("T R Y"); //"t r y"
-    solution("TT RR YY"); //"Tt Rr Yy"
-    solution("tt rr yy"); //"Tt Rr Yy"
+    clock_t tic = clock();
+    char q[7][60]={
+        {"try hello world"},
+        {"t r y"},
+        {"T R Y"},
+        {"TT RR YY"},
+        {"tt rr yy"},
+        {"ttttttttttttttttttttttttttttttttttttttttttttttttttttttt"},
+        {"TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT"}
+    };
+    for (int i=0; i<10000; i++){
+        char* a = solution(q[i%7]);
+        free(a);
+    }
+    clock_t toc = clock();
+    printf("running time: %f\n", (double)(toc-tic) / CLOCKS_PER_SEC);
     return 0;
 }
