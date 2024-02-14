ce2bb2f1490f85fd0c4e082367375065f8af9421
dwlee <dw.lee832@gmail.com>
2024-02-14 21:49:40 +0900

    이웃한 칸 문제풀이

---
+++ lev1/이웃한_칸.py
@@ -0,0 +1,18 @@
+def solution(board, h, w):
+    ans = 0
+    N = len(board)
+
+    if h+1 < N and board[h][w] == board[h+1][w]:
+        ans += 1
+    if h-1 >= 0 and board[h][w] == board[h-1][w]:
+        ans += 1
+    if w+1 < N and board[h][w] == board[h][w+1]:
+        ans += 1
+    if w-1 >= 0 and board[h][w] == board[h][w-1]:
+        ans += 1
+
+    return ans
+
+
+print(solution([["blue", "red", "orange", "red"], ["red", "red", "blue", "orange"], ["blue", "orange", "red", "red"], ["orange", "orange", "red", "blue"]], 1, 1)) # 2
+print(solution([["yellow", "green", "blue"], ["blue", "green", "yellow"], ["yellow", "blue", "blue"]], 0, 1)) # 1
