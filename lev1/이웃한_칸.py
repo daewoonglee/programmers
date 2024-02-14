1cf8a1819489b25645b112c6d0dd8977b0e93d29
dwlee <dw.lee832@gmail.com>
2024-02-14 22:01:07 +0900

    이웃한 칸 문제풀이 01 - code refactoring

--- lev1/이웃한_칸.py
+++ lev1/이웃한_칸.py
@@ -2,14 +2,21 @@ def solution(board, h, w):
     ans = 0
     N = len(board)
 
-    if h+1 < N and board[h][w] == board[h+1][w]:
-        ans += 1
-    if h-1 >= 0 and board[h][w] == board[h-1][w]:
-        ans += 1
-    if w+1 < N and board[h][w] == board[h][w+1]:
-        ans += 1
-    if w-1 >= 0 and board[h][w] == board[h][w-1]:
-        ans += 1
+    # if h+1 < N and board[h][w] == board[h+1][w]:
+    #     ans += 1
+    # if h-1 >= 0 and board[h][w] == board[h-1][w]:
+    #     ans += 1
+    # if w+1 < N and board[h][w] == board[h][w+1]:
+    #     ans += 1
+    # if w-1 >= 0 and board[h][w] == board[h][w-1]:
+    #     ans += 1
+
+    yh, xw = [0, 1, -1, 0], [1, 0, 0, -1]
+    for i in range(4):
+        dh, dw = h + yh[i], w + xw[i]
+        if 0 <= dh < N and 0 <= dw < N:
+            if board[h][w] == board[dh][dw]:
+                ans += 1
 
     return ans
 
