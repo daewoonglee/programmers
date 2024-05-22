14a08eb6144013336cc3571214a7ab097854bd64
dw.lee <dw.lee832@gmail.com>
2024-05-22 22:27:37 +0900

    석유 시추 문제풀이 03 - 풀이X (테스트 9/9, 효율성 4/6)

--- lev2/석유 시추.py
+++ lev2/석유 시추.py
@@ -1,15 +1,18 @@
+from collections import deque
+
+
 def solution(land):
     def bfs(xy, oil_id):
         log_xy = []
         oil = 1
         while xy:
-            x, y = xy.pop(0)
+            x, y = xy.popleft()
             log_xy.append([x, y])
             for mx, my in move:
                 nx, ny = x+mx, y+my
-                if 0 <= nx < N and 0 <= ny < M and land[nx][ny] == 1:
+                if 0 <= nx < N and 0 <= ny < M and land[nx][ny][0] == 1:
                     xy.append([nx, ny])
-                    land[nx][ny] = [-1, oil_id]
+                    land[nx][ny][0] = [-1, oil_id]
                     oil += 1
         for x, y in log_xy:
             land[x][y] = [oil, oil_id]
@@ -17,32 +20,44 @@ def solution(land):
     # get oil size
     move = [[1, 0], [-1, 0], [0, 1], [0, -1]]
     N, M = len(land), len(land[0])
+    oil_group = -1
     for i in range(N):
         for j in range(M):
-            if land[i][j] == 1:
-                land[i][j] = -1
-                bfs([[i, j]], f"{i}_{j}")
+            land[i][j] = [land[i][j], oil_group]
+    oil_group += 1
+
+    for i in range(N):
+        for j in range(M):
+            if land[i][j][0] == 1:
+                land[i][j] = [-1, oil_group]
+                bfs(deque([[i, j]]), oil_group)
+                oil_group += 1
 
     # oil drilling
     max_oil = 0
     for j in range(M):
+        i = 0
         local_oil = []
-        for i in range(N):
-            if isinstance(land[i][j], list) and (not local_oil or all([1 if land[i][j][-1] != loc_oil[-1] else 0 for loc_oil in local_oil])):
+        for _ in range(N):
+            if land[i][j][0]:
                 local_oil.append(land[i][j])
-        sum_oil = sum([oil for oil, _ in local_oil]) if local_oil else 0
-        max_oil = sum_oil if sum_oil > max_oil else max_oil
+                break
+            i += 1
+        for z in range(i+1, N):
+            if land[z][j][0] and all([1 if land[z][j][-1] != loc_oil[-1] else 0 for loc_oil in local_oil]):
+                local_oil.append(land[z][j])
+        max_oil = max(sum([oil for oil, _ in local_oil]), max_oil)
     return max_oil
 
 
-# print(solution([[0, 0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 1, 1, 0, 0], [1, 1, 0, 0, 0, 1, 1, 0], [1, 1, 1, 0, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0, 1, 1]])) # 9
-# print(solution([[1, 0, 1, 0, 1, 1], [1, 0, 1, 0, 0, 0], [1, 0, 1, 0, 0, 1], [1, 0, 0, 1, 0, 0], [1, 0, 0, 1, 0, 1], [1, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 1]])) # 16
-# print(solution([[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]])) # 0
-# print(solution([[1]])) # 1
-# print(solution([[0]])) # 0
-# print(solution([[0, 0, 0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0, 0, 1], [1, 1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 0, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0, 1, 1]])) # 22
-# print(solution([[1, 0, 1, 0, 1, 1], [1, 0, 1, 0, 0, 1], [1, 0, 1, 1, 1, 1], [0, 1, 1, 0, 0, 1], [1, 1, 1, 0, 1, 1]])) # 20
-# print(solution([[1, 0, 0, 0, 1], [1, 0, 0, 0, 1], [1, 0, 0, 0, 1], [1, 1, 1, 1, 1], [1, 0, 0, 0, 0], [1, 1, 1, 1, 1]])) # 17
+print(solution([[0, 0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 1, 1, 0, 0], [1, 1, 0, 0, 0, 1, 1, 0], [1, 1, 1, 0, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0, 1, 1]])) # 9
+print(solution([[1, 0, 1, 0, 1, 1], [1, 0, 1, 0, 0, 0], [1, 0, 1, 0, 0, 1], [1, 0, 0, 1, 0, 0], [1, 0, 0, 1, 0, 1], [1, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 1]])) # 16
+print(solution([[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]])) # 0
+print(solution([[1]])) # 1
+print(solution([[0]])) # 0
+print(solution([[0, 0, 0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0, 0, 1], [1, 1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 0, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0, 1, 1]])) # 22
+print(solution([[1, 0, 1, 0, 1, 1], [1, 0, 1, 0, 0, 1], [1, 0, 1, 1, 1, 1], [0, 1, 1, 0, 0, 1], [1, 1, 1, 0, 1, 1]])) # 20
+print(solution([[1, 0, 0, 0, 1], [1, 0, 0, 0, 1], [1, 0, 0, 0, 1], [1, 1, 1, 1, 1], [1, 0, 0, 0, 0], [1, 1, 1, 1, 1]])) # 17
 print(solution([[1,1,1,1], [1,0,0,0], [1,0,1,0], [1,0,0,0],[1,1,1,1]])) # 12
 """
 1 1 1 1 
