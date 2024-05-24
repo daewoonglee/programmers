0ae5ef131940dad538fb7e0b9ba1d2ef2f502c68
dw.lee <dw.lee832@gmail.com>
2024-05-24 20:49:49 +0900

    석유 시추 문제풀이 04 - 풀이O (테스트 9/9, 효율성 6/6)

--- lev2/석유 시추.py
+++ lev2/석유 시추.py
@@ -10,9 +10,9 @@ def solution(land):
             log_xy.append([x, y])
             for mx, my in move:
                 nx, ny = x+mx, y+my
-                if 0 <= nx < N and 0 <= ny < M and land[nx][ny][0] == 1:
+                if 0 <= nx < N and 0 <= ny < M and land[nx][ny][0]:
                     xy.append([nx, ny])
-                    land[nx][ny][0] = [-1, oil_id]
+                    land[nx][ny][0] = 0
                     oil += 1
         for x, y in log_xy:
             land[x][y] = [oil, oil_id]
@@ -20,33 +20,33 @@ def solution(land):
     # get oil size
     move = [[1, 0], [-1, 0], [0, 1], [0, -1]]
     N, M = len(land), len(land[0])
-    oil_group = -1
     for i in range(N):
         for j in range(M):
-            land[i][j] = [land[i][j], oil_group]
-    oil_group += 1
+            land[i][j] = [land[i][j], -1]
 
+    oil_group = 0
     for i in range(N):
         for j in range(M):
             if land[i][j][0] == 1:
-                land[i][j] = [-1, oil_group]
+                land[i][j] = [0, oil_group]
                 bfs(deque([[i, j]]), oil_group)
                 oil_group += 1
 
     # oil drilling
     max_oil = 0
     for j in range(M):
-        i = 0
-        local_oil = []
-        for _ in range(N):
+        local_sum = 0
+        local_oil_log = []
+        for i in range(N):
             if land[i][j][0]:
-                local_oil.append(land[i][j])
-                break
-            i += 1
-        for z in range(i+1, N):
-            if land[z][j][0] and all([1 if land[z][j][-1] != loc_oil[-1] else 0 for loc_oil in local_oil]):
-                local_oil.append(land[z][j])
-        max_oil = max(sum([oil for oil, _ in local_oil]), max_oil)
+                land_id = land[i][j][1]
+                for gid in local_oil_log:
+                    if land_id == gid: break
+                else:
+                    local_oil_log.append(land_id)
+                    local_sum += land[i][j][0]
+        max_oil = local_sum if local_sum > max_oil else max_oil
+
     return max_oil
 
 
@@ -59,10 +59,3 @@ print(solution([[0, 0, 0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0, 0, 1], [1, 1, 1, 1,
 print(solution([[1, 0, 1, 0, 1, 1], [1, 0, 1, 0, 0, 1], [1, 0, 1, 1, 1, 1], [0, 1, 1, 0, 0, 1], [1, 1, 1, 0, 1, 1]])) # 20
 print(solution([[1, 0, 0, 0, 1], [1, 0, 0, 0, 1], [1, 0, 0, 0, 1], [1, 1, 1, 1, 1], [1, 0, 0, 0, 0], [1, 1, 1, 1, 1]])) # 17
 print(solution([[1,1,1,1], [1,0,0,0], [1,0,1,0], [1,0,0,0],[1,1,1,1]])) # 12
-"""
-1 1 1 1 
-1 0 0 0
-1 0 1 0
-1 0 0 0
-1 1 1 1
-"""
