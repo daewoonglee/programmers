cb78884c17e88538a70fee23a790200eb53245f8
dw.lee <dw.lee832@gmail.com>
2024-05-29 20:41:45 +0900

    가장 많이 받은 선물 C version - code refactoring

--- lev1/가장_많이_받은_선물.c
+++ lev1/가장_많이_받은_선물.c
@@ -4,79 +4,52 @@
 #include <string.h>
 
 
-typedef struct{
-    char names[11];
-    int index;
-} Friend;
-
 // friends_len은 배열 friends의 길이입니다.
 // gifts_len은 배열 gifts의 길이입니다.
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 int solution(const char* friends[], size_t friends_len, const char* gifts[], size_t gifts_len) {
-    Friend friend_name_idx[friends_len];
-    for (int i=0; i<friends_len; i++){
-        strcpy(friend_name_idx[i].names, friends[i]);
-        friend_name_idx[i].index = i;
-    }
-    
-    int **gift_log = (int**)malloc(sizeof(int*)*friends_len);
-    for (int i=0; i<friends_len; i++){
-        gift_log[i] = (int*)malloc(sizeof(int)*friends_len);
-        for (int j=0; j<friends_len; j++)
-            gift_log[i][j] = 0;
-    }
+    int gift_log[friends_len][friends_len];
+    memset(gift_log, 0, sizeof(int)*friends_len*friends_len);
     for (int i=0; i<gifts_len; i++){
         char gift_copy[22];
         strncpy(gift_copy, gifts[i], sizeof(gift_copy)-1);
-        gift_copy[sizeof(gift_copy) - 1] = '\0'; 
+        gift_copy[sizeof(gift_copy) - 1] = '\0';
 
         char *friend1 = strtok(gift_copy, " ");
         char *friend2 = strtok(NULL, " ");
 
-        int friend1_idx = 0;
-        int friend2_idx = 0;
-
+        int row = 0, col = 0;
         for (int j=0; j<friends_len; j++){
-            if (!strcmp(friend_name_idx[j].names, friend1))
-                friend1_idx = friend_name_idx[j].index;
-            if (!strcmp(friend_name_idx[j].names, friend2))
-                friend2_idx = friend_name_idx[j].index;
+            if (strcmp(friend1, friends[j]) == 0)
+                row = j;
+            if (strcmp(friend2, friends[j]) == 0)
+                col = j;
         }
-        gift_log[friend1_idx][friend2_idx]++;
+        gift_log[row][col]++;
     }
 
     int gift_index[friends_len];
     for (int i=0; i<friends_len; i++){
-        int row_sum = 0;
-        for (int j=0; j<friends_len; j++)
+        int row_sum = 0, col_sum = 0;
+        for (int j=0; j<friends_len; j++){
             row_sum += gift_log[i][j];
-        int col_sum = 0;
-        for (int j=0; j<friends_len; j++)
             col_sum += gift_log[j][i];
+        }
+
         gift_index[i] = row_sum - col_sum;
     }
     
-    int ans_arr[friends_len];
-    for (int i=0; i<friends_len; i++)
-        ans_arr[i] = 0;
-    for (int i=0; i<friends_len; i++){
-        for (int j=i+1; j<friends_len; j++){
-            int gift_cnt1 = gift_log[i][j];
-            int gift_cnt2 = gift_log[j][i];
-            if (gift_cnt1 == gift_cnt2){
-                if (gift_index[i] > gift_index[j])
-                    ans_arr[i] += 1;
-                else if (gift_index[i] < gift_index[j])
-                    ans_arr[j] += 1;
-            }
-            else 
-                ans_arr[gift_cnt1 > gift_cnt2 ? i : j] += 1;
-        }
-    }
     int ans = 0;
     for (int i=0; i<friends_len; i++){
-        if (ans < ans_arr[i])
-            ans = ans_arr[i];
+        int tmp = 0;
+        for (int j=0; j<friends_len; j++){
+            int gift_cnt1 = gift_log[i][j];
+            int gift_cnt2 = gift_log[j][i];
+            if (i == j) continue;
+            if (gift_cnt1 > gift_cnt2) tmp++;
+            else if (gift_cnt1 == gift_cnt2 && gift_index[i] > gift_index[j]) tmp++;
+        }
+        ans = ans < tmp ? tmp : ans;
     }
     return ans;
 }
