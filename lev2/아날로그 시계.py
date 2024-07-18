0c11bd2d312b17cb513fb404a9ac837b8cedcca0
dw.lee <dw.lee832@gmail.com>
2024-07-19 00:55:19 +0900

    아날로그 시계 문제풀이 03 - code refactoring 02

--- lev2/아날로그 시계.py
+++ lev2/아날로그 시계.py
@@ -11,30 +11,39 @@ def solution(h1, m1, s1, h2, m2, s2):
     hms2 = time2sec(h2, m2, s2)
 
     ans = 0
-    if hms1 == 0 * 3600 or hms1 == 12 * 3600:
-        ans += 1
+    # if hms1 == 0 or hms1 == 12 * 3600:
+    #     ans += 1
+    #
+    # for sec in range(hms2-hms1):
+    #     cur_s = s1 + sec  # 1초씩 증가
+    #     cur_m = (m1 + cur_s / 60)  # 1초 증가할 때 0.x분 증가
+    #     cur_h = (h1 + cur_m / 60) % 12 * 5  # 1초 증가할 때 0.x시간 증가
+    #
+    #     cur_s = cur_s % 60
+    #     cur_m = cur_m % 60
+    #     cur_h = cur_h % 60
+    #
+    #     next_s = (s1 + sec+1) # 1초씩 증가
+    #     next_m = (m1 + (s1 + sec+1) / 60)  # 1초 증가할 때 0.x분 증가
+    #     next_h = (h1 + next_m / 60) % 12 * 5  # 1초 증가할 때 0.x시간 증가
+    #
+    #     next_s = next_s % 60 if next_s % 60 != 0 else 60
+    #     next_m = next_m % 60 if next_m % 60 != 0 else 60
+    #     next_h = next_h if next_h % 60 != 0 else 60
+    #
+    #     if cur_s < cur_h and next_s >= next_h: ans += 1
+    #     if cur_s < cur_m and next_s >= next_m: ans += 1
+    #     if next_s == next_m == next_h: ans -= 1
+    #     print(f"sec: {sec}, next h: {next_h}, m: {next_m}, s: {next_s}, cur h: {cur_h}, m: {cur_m}, s: {cur_s}, ans: {ans}")
 
-    for sec in range(hms2-hms1):
-        cur_s = s1 + sec  # 1초씩 증가
-        cur_m = (m1 + cur_s / 60)  # 1초 증가할 때 0.x분 증가
-        cur_h = (h1 + cur_m / 60) % 12 * 5  # 1초 증가할 때 0.x시간 증가
 
-        cur_s = cur_s % 60
-        cur_m = cur_m % 60
-        cur_h = cur_h % 60
-
-        next_s = (s1 + sec+1) # 1초씩 증가
-        next_m = (m1 + (s1 + sec+1) / 60)  # 1초 증가할 때 0.x분 증가
-        next_h = (h1 + next_m / 60) % 12 * 5  # 1초 증가할 때 0.x시간 증가
-
-        next_s = next_s % 60 if next_s % 60 != 0 else 60
-        next_m = next_m % 60 if next_m % 60 != 0 else 60
-        next_h = next_h if next_h % 60 != 0 else 60
-
-        if cur_s < cur_h and next_s >= next_h: ans += 1
-        if cur_s < cur_m and next_s >= next_m: ans += 1
-        if next_s == next_m == next_h: ans -= 1
-        print(f"sec: {sec}, next h: {next_h}, m: {next_m}, s: {next_s}, cur h: {cur_h}, m: {cur_m}, s: {cur_s}, ans: {ans}")
+    # code refactoring 02
+    ans += int(hms2 / 3600 * 59)
+    ans -= hms1 // (3600 / 59)
+    ans += hms2 // (360 * 120 / 719)
+    ans -= hms1 // (360 * 120 / 719)
+    if hms1 <= 12 * 60 * 60 <= hms2: ans -= 1
+    if hms1 == 0 or hms1 == 12 * 60 * 60: ans += 1
 
     return ans
 
