62d5ebbcb8e8820697b7f2fbc9379a680fb2e0a7
dw.lee <dw.lee832@gmail.com>
2024-07-23 14:03:09 +0900

    도넛과 막대 그래프 문제풀이 03 - 풀이O (테스트 35/35) - 접근방법 참고

--- lev2/도넛과 막대 그래프.py
+++ lev2/도넛과 막대 그래프.py
@@ -1,5 +1,5 @@
 import sys
-sys.setrecursionlimit(500000)
+sys.setrecursionlimit(499999)
 
 
 def solution(edges):
@@ -8,19 +8,23 @@ def solution(edges):
     막대: 정점 n, 간선 n-1
     8자: 정점 n, 간선 n+1
     """
-    def dfs(cur_node, visited_node, edge_cnt):
-        # 방문할 수 있는 노드가 없는 경우(빈 리스트)
+    def dfs(cur_node, node_cnt, edge_cnt):
+        # 방문할 수 있는 노드가 없는 경우는 막대 그래프
         if cur_node not in edge_dict:
-            visited_node.append("NONE")
-            return visited_node, edge_cnt
+            return node_cnt, edge_cnt
+        # 방출 간선이 2개면 8자 그래프
+        elif len(edge_dict[cur_node]) == 2:
+            print(f"88 cur: {cur_node}, cnt: {node_cnt}, {edge_cnt}")
+            return node_cnt, edge_cnt+2
+
         # 노드 방문하며 서치
-        for i, next_node in enumerate(edge_dict[cur_node]):
-            if not edge_flag[cur_node][i]:
-                if cur_node not in visited_node:
-                    visited_node.append(cur_node)
-                edge_flag[cur_node][i] = True
-                visited_node, edge_cnt = dfs(next_node, visited_node, edge_cnt+1)
-        return visited_node, edge_cnt
+        if not edge_flag[cur_node][0]:
+            edge_flag[cur_node][0] = True
+            node_cnt, edge_cnt = dfs(edge_dict[cur_node][0], node_cnt+1, edge_cnt+1)
+        # 이미 방문한 노드 재방문 -> 도넛
+        else: edge_cnt += 1
+
+        return node_cnt, edge_cnt
 
     # edge 별 방문 리스트 생성
     edge_dict = dict()
@@ -31,35 +35,34 @@ def solution(edges):
             edge_flag[a] = []
         edge_dict[a].append(b)
         edge_flag[a].append(False)
-    print(edge_dict)
+    # print(edge_dict)
 
     # 무관한 정점 서치
     max_len = max(len(v) for v in edge_dict.values())
     max_keys = [k for k, v in edge_dict.items() if len(v) == max_len]
-    all_max_values = {val for k in max_keys for val in edge_dict[k]}
-    random_node_key = [k for k in max_keys if k not in all_max_values][0]
-    # print(random_node_key)
+    all_values = [v for values in edge_dict.values() for v in values]
+    random_node_key = [k for k in max_keys if k not in all_values][0]
 
     # 무관한 정점에서 edge dfs로 방문, 무관한 정점은 edge가 가장 많은 노드
     ans = [random_node_key, 0, 0, 0]
     for n in edge_dict[random_node_key]:
-        node, edge = dfs(n, [], 0)
-        print(f"n: {n}, node cnt: {node}, edge cnt: {edge}")
-        if len(node) == edge: ans[1] += 1 # 도넛
-        elif len(node)-1 == edge: ans[2] += 1 # 막대
+        node, edge = dfs(n, 1, 0)
+        # print(f"n: {n}, node cnt: {node}, edge cnt: {edge}")
+        if node == edge: ans[1] += 1 # 도넛
+        elif node-1 == edge: ans[2] += 1 # 막대
         else: ans[3] += 1 # 8자
     return ans
 
 
 # print(solution([[2, 3], [4, 3], [1, 1], [2, 1]])) # [2, 1, 1, 0]
 # print(solution([[4, 11], [1, 12], [8, 3], [12, 7], [4, 2], [7, 11], [4, 8], [9, 6], [10, 11], [6, 10], [3, 5], [11, 1], [5, 3], [11, 9], [3, 8]])) # [4, 0, 1, 2]
-print(solution([[1,4], [3,2],[4,3],[5,4],[6,5],[4,6],[2,4],
-                [7,8],[8,9],[9,8],[8,7],[1,9],
-                [10,11],[11,13],[13,15],[15,16],[16,14],[14,13],[13,12],[12,10],[1,16],
-                [17,18],[18,19],[19,20],[20,21], [1,21],
-                [22,24],[24,26],[26,27],[27,29],[29,30],[30,28],[28,26],[26,25],[25,23],[23,22], [1,30],
-                [36,31],[31,32],[32,33],[33,34],[34,35],[35,36], [1,36]])) # [2, 1, 0, 0]
-print(solution([[3,4],[4,5],[5,6],[6,4],[4,2],[2,3],[1,4],
-                [1,7],[7,8],[8,9],[9,8],[8,7]]))
+# print(solution([
+#     [2,3],[3,4],[4,5],[5,1],[1,2], [7,2], # 도넛
+#     [10,11],[11,12],[12,13],[13,14], [7,14], # 막대
+#     [32,27],[27,31],[31,21],[21,32],[31,29],[29,26],[26,30],[30,31], [7,32] # 8자
+# ]))
+print(solution([[32,27],[27,31],[31,21],[21,32],[31,29],[29,26],[26,30],[30,31], [7,32],
+                [7,100]])) # [7, 0, 1, 1]
+
 
 
