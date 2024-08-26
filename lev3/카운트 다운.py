84bea3ffd4bab470d44a287c9344544f9f783a59
dw.lee <dw.lee832@gmail.com>
2024-08-26 21:50:19 +0900

    카운트 다운 문제풀이 02 - 풀이X (테스트 11/25)

--- lev3/카운트 다운.py
+++ lev3/카운트 다운.py
@@ -14,20 +14,22 @@ def solution(target):
             else:
                 if x == 50 and xd == 0 and xq-1 == q:
                     return [xq, xq]
-    elif 50 <= target <= 70:
+    elif 50 < target <= 70:
         return [2, 2]
+    elif target == 50:
+        return [1, 1]
 
     if d <= 20:
         ans[0] += 1
         ans[1] += 1
-    elif d % 3 == 0 or d % 2 == 0:
+    elif d % 3 == 0 or (d <= 40 and d % 2 == 0):
         ans[0] += 1
-    elif d <= 40:
-        ans[0] += 2
-        ans[1] += 2
-    else:
+    elif d > 40:
         ans[0] += 2
         ans[1] += 1
+    else:
+        ans[0] += 2
+        ans[1] += 2
     return ans
 
 
@@ -35,6 +37,12 @@ def solution(target):
 # print(solution(58)) # [2,2]
 # print(solution(100000)) # [1667, 0]
 # print(solution(1)) # [1,1]
-# print(solution(35)) #[2,2]
+print(solution(35)) #[2,2]
+print(solution(49)) #[2,1]
+print(solution(40)) #[1,0]
+print(solution(41)) #[2,1]
+print(solution(47)) #[2,1]
 # print(solution(180)) #[3,0]
-print(solution(250))
+# print(solution(250))
+# print(solution(50)) # [1,1]
+print(solution(37)) # [2,2]
