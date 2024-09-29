2b0523f065f9c245b552332d0a90fb6ae7e1a683
dw.lee <dw.lee832@gmail.com>
2024-09-30 08:00:42 +0900

    카운트 다운 문제풀이 03 - 풀이X (테스트 11/25)

--- lev3/카운트 다운.py
+++ lev3/카운트 다운.py
@@ -1,48 +1,31 @@
 def solution(target):
-    ans = [0, 0]
-    q,d = 1668, target
-    if target > 70:
-        # 17(51), 18(54), 19(57), 20(60), 50(bull)
-        for x in [60, 57, 54, 51, 50]:
-            xq, xd = divmod(target, x)
-            print(x, xq, xd)
-            if xq < q:
-                q = xq
-                d = xd
-                ans = [q, q] if x == 50 else [q, 0]
-                if d == 0: return ans
-            else:
-                if x == 50 and xd == 0 and xq-1 == q:
-                    return [xq, xq]
-    elif 50 < target <= 70:
-        return [2, 2]
-    elif target == 50:
-        return [1, 1]
+    def countdown(n, q):
+        if n == 0: return [q, 0] if x != 50 else [q, q]
+        elif n > 20 and n % 3 == 0 or (n <= 40 and n % 2 == 0): return [q+1, 0] if x != 50 else [q+1, q]
+        elif n == 50: return [q+1, 1]
+        elif n > 50: return [q+2, 2]
+        elif n < 20: return [q+1, 1]
+        elif n < 40: return [q+2, 2]
+        else: return [q+2, 1]
 
-    if d <= 20:
-        ans[0] += 1
-        ans[1] += 1
-    elif d % 3 == 0 or (d <= 40 and d % 2 == 0):
-        ans[0] += 1
-    elif d > 40:
-        ans[0] += 2
-        ans[1] += 1
-    else:
-        ans[0] += 2
-        ans[1] += 2
-    return ans
+    ans = [[] for _ in range(5)]
+    for i, x in enumerate([60, 57, 54, 51, 50]): # 20(60), 19(57), 18(54), 17(51), 50(bull)
+        xq, xd = divmod(target, x)
+        ans[i] = countdown(xd, xq)
+
+    return min(ans, key=lambda x: (x[0], -x[1]))
 
 
-# print(solution(21)) # [1,0]
-# print(solution(58)) # [2,2]
-# print(solution(100000)) # [1667, 0]
+print(solution(21)) # [1,0]
+print(solution(58)) # [2,2]
+print(solution(100000)) # [1667, 0]
 # print(solution(1)) # [1,1]
-print(solution(35)) #[2,2]
-print(solution(49)) #[2,1]
-print(solution(40)) #[1,0]
-print(solution(41)) #[2,1]
-print(solution(47)) #[2,1]
+# print(solution(35)) #[2,2]
+# print(solution(49)) #[2,1]
+# print(solution(40)) #[1,0]
+# print(solution(41)) #[2,1]
+# print(solution(47)) #[2,1]
 # print(solution(180)) #[3,0]
-# print(solution(250))
+# print(solution(250)) #[5,5]
 # print(solution(50)) # [1,1]
-print(solution(37)) # [2,2]
+# print(solution(37)) # [2,2]
