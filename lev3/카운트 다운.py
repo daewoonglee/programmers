6a8cb71a823eef0b4720be5ca6d2544bc7e05001
dw.lee <dw.lee832@gmail.com>
2024-10-04 00:23:16 +0900

    카운트 다운 문제풀이 05 - 풀이X (테스트 6/25), 시간초과

--- lev3/카운트 다운.py
+++ lev3/카운트 다운.py
@@ -1,19 +1,37 @@
+import sys
+sys.setrecursionlimit(10000)
+
+
 def solution(target):
     def countdown(n, q, m):
-        print(f"n: {n}, q: {q}, m: {m}")
         if n == 0: return [q, m]
         elif n > 20 and (n % 3 == 0 or (n <= 40 and n % 2 == 0)): return [q+1, m]
         elif n == 50 or n <= 20: return [q+1, m+1]
         elif n > 50 or n < 40: return [q+2, m+2]
         else: return [q+2, m+1]
 
-    ans = [[] for _ in range(5)]
-    for i, x in enumerate([60, 57, 54, 51, 50]): # 20(60), 19(57), 18(54), 17(51), 50(bull)
-        xq, xd = divmod(target, x)
-        ans[i] = countdown(xd, xq, xq if x == 50 else 0)
-    print(ans)
+    def search(depth, search_list):
+        global ans
+        for x in [51, 54, 57, 60]:
+            search_list[depth] = x
+            d = target - sum(search_list)
+            darts, min_times = countdown(d, q, q-(depth+1))
+            # print(f"x: {x}, d: {d}, cnt: {darts}, {min_times}, list: {search_list}, ans: {ans}")
+            if ans[0] >= darts:
+                if ans[0] == darts and ans[1] < min_times:
+                    ans = [darts, min_times]
+                elif ans[0] > darts:
+                    ans = [darts, min_times]
+            if depth+1 < q:
+                search(depth+1, search_list[:])
 
-    return min(ans, key=lambda x: (x[0], -x[1]))
+    global ans
+    q, d = divmod(target, 50)
+    target_list = [50] * q
+    ans = countdown(d, q, q)
+    if q:
+        search(0, target_list[:])
+    return ans
 
 
 # print(solution(21)) # [1,0]
@@ -30,9 +48,11 @@ def solution(target):
 # print(solution(103)) # [3,3]
 # print(solution(102)) # [2,0]
 # print(solution(101)) # [2,1]
-print(solution(173)) # [4,3]
-# print(solution(191)) # [2,1]
-# print(solution(197)) # [2,1]
+# print(solution(117)) # [2,0]
+print(solution(160)) # [3,2]
+# print(solution(147)) # [4,3]
+# print(solution(191)) # [4,2]
+# print(solution(247)) # [4,2]
 # 141, 147 -> 3, 1
 # 191, 197 -> 4, 2
 # 241, 247 -> 5, 3
