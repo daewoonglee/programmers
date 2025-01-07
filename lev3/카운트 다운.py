eaad82f8a5bef28b918117f9897d9521fd9a2080
dw.lee <dw.lee832@gmail.com>
2025-01-08 08:07:52 +0900

    카운트 다운 문제풀이 06 - 풀이X (테스트 14/25)

--- lev3/카운트 다운.py
+++ lev3/카운트 다운.py
@@ -3,35 +3,84 @@ sys.setrecursionlimit(10000)
 
 
 def solution(target):
-    def countdown(n, q, m):
-        if n == 0: return [q, m]
-        elif n > 20 and (n % 3 == 0 or (n <= 40 and n % 2 == 0)): return [q+1, m]
-        elif n == 50 or n <= 20: return [q+1, m+1]
-        elif n > 50 or n < 40: return [q+2, m+2]
-        else: return [q+2, m+1]
 
-    def search(depth, search_list):
-        global ans
-        for x in [51, 54, 57, 60]:
-            search_list[depth] = x
-            d = target - sum(search_list)
-            darts, min_times = countdown(d, q, q-(depth+1))
-            # print(f"x: {x}, d: {d}, cnt: {darts}, {min_times}, list: {search_list}, ans: {ans}")
-            if ans[0] >= darts:
-                if ans[0] == darts and ans[1] < min_times:
-                    ans = [darts, min_times]
-                elif ans[0] > darts:
-                    ans = [darts, min_times]
-            if depth+1 < q:
-                search(depth+1, search_list[:])
+    def countdown(q, d, t):
+        nonlocal dart_cnt, single_bull
+        if t < 0 or dart_cnt < q: return
+        if t==0:
+            print(f"q: {q}, dart_cnt: {dart_cnt}, d: {d}, singlebull: {single_bull}")
+        if t == 0 and dart_cnt >= q:
+            if dart_cnt == q and single_bull < d:
+                single_bull = d
+            elif dart_cnt != q:
+                dart_cnt = q
+                single_bull = d
+            return
 
-    global ans
-    q, d = divmod(target, 50)
-    target_list = [50] * q
-    ans = countdown(d, q, q)
-    if q:
-        search(0, target_list[:])
-    return ans
+        range_li = [20, 19, 18, 17, 50, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+        for n in range_li:
+            if n != 50:
+                if n * 3 <= t:
+                    countdown(q+1, d, t - n*3)
+                elif n * 2 <= t:
+                    countdown(q+1, d, t - n*2)
+                elif n <= t:
+                    countdown(q+1, d+1, t - n)
+            elif n <= t:
+                countdown(q+1, d+1, t - n)
+
+    dart_cnt, single_bull = target, 0
+    q, target = divmod(target, 300)
+    print(q*5, target)
+    countdown(q*5, 0, target)
+    return [dart_cnt, single_bull]
+
+
+    """
+    shot = target
+    ball_n_single = 0
+
+    single = list(range(1, 21))
+    
+    def dfs(score, cnt, bs):
+        '''
+        score: 현재 점수
+        cnt: 총 던진 횟수
+        bs: 볼 또는 싱글을 맞춘 횟수
+        '''
+        nonlocal shot, ball_n_single
+
+        # answer보다 더 많이 쐈으면 return
+        if cnt > shot:
+            return
+
+        # 0점 밑으로 내려가면 return
+        if score < 0:
+            return
+
+        # 0점이면 볼 또는 싱글 횟수 갱신
+        if cnt <= shot and score == 0:
+            if cnt == shot:
+                ball_n_single = max(bs, ball_n_single)
+            else:
+                shot = cnt
+                ball_n_single = bs
+
+            return
+
+        dfs(score - 50, cnt + 1, bs + 1)
+
+        for s in single:
+            if score - s < 0:
+                break
+            dfs(score - s, cnt + 1, bs + 1)
+            dfs(score - s * 2, cnt + 1, bs)
+            dfs(score - s * 3, cnt + 1, bs)
+
+    dfs(target, 0, 0)
+    
+    return [shot, ball_n_single]
+"""
 
 
 # print(solution(21)) # [1,0]
@@ -44,12 +93,12 @@ def solution(target):
 # print(solution(41)) #[2,1]
 # print(solution(47)) #[2,1]
 # print(solution(180)) #[3,0]
-# print(solution(250)) #[5,5]
+print(solution(250)) #[5,5]
 # print(solution(103)) # [3,3]
 # print(solution(102)) # [2,0]
 # print(solution(101)) # [2,1]
 # print(solution(117)) # [2,0]
-print(solution(160)) # [3,2]
+# print(solution(160)) # [3,2]
 # print(solution(147)) # [4,3]
 # print(solution(191)) # [4,2]
 # print(solution(247)) # [4,2]
