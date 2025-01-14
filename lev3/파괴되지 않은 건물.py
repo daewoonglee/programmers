2d3db30baeba9564d59975b244b97def3489c5e5
dw.lee <dw.lee832@gmail.com>
2025-01-14 18:33:06 +0900

    파괴되지 않는 건물 문제풀이 03 - 풀이X (정확성 10/10, 효율성 0/7)

--- lev3/파괴되지 않은 건물.py
+++ lev3/파괴되지 않은 건물.py
@@ -1,5 +1,5 @@
 def solution(board, skill):
-    # 효율성 (0/7)
+    # # 효율성 (0/7)
     # for s in skill:
     #     t, x1, y1, x2, y2, d = s
     #     d = d if t == 2 else -d
@@ -17,25 +17,22 @@ def solution(board, skill):
     # return ans
 
     # 효율성 (0/7)
-    ans = len(board) * len(board[0])
-    flag = [[1]*len(board[0]) for _ in board]
+    N, M = len(board), len(board[0])
+    prefix_sum_li = [0] * (N * M)
     for s in skill:
         t, x1, y1, x2, y2, d = s
         d = d if t == 2 else -d
-        row = x2-x1+1
-        col = y2-y1+1
+        row = x2 - x1 + 1
+        col = y2 - y1 + 1
         for i in range(row*col):
-            x, y = x1+i//col, y1+i%col
-            board[x][y] += d
-            if board[x][y] <= 0 and flag[x][y]:
-                ans -= 1
-                flag[x][y] = 0
-            elif board[x][y] > 0 and not flag[x][y]:
-                ans += 1
-                flag[x][y] = 1
+            prefix_sum_li[(x1+i//col) * M + y1+i%col] += d
+
+    ans = 0
+    for i, n in enumerate(prefix_sum_li):
+        x, y = i // M, i % M
+        if board[x][y] + n > 0:
+            ans += 1
     return ans
-
-
 
 
 print(solution([[5,5,5,5,5],[5,5,5,5,5],[5,5,5,5,5],[5,5,5,5,5]], [[1,0,0,3,4,4],[1,2,0,2,3,2],[2,1,0,3,1,2],[1,0,1,3,3,1]])) # 10
