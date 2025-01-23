51b0204e4c56dccf337ded254d764177b7bff5eb
dw.lee <dw.lee832@gmail.com>
2025-01-24 08:25:46 +0900

    PCCP_기출문제_1번_동영상_재생기 - code refactoring

--- lev1/PCCP_기출문제_1번_동영상_재생기.py
+++ lev1/PCCP_기출문제_1번_동영상_재생기.py
@@ -1,22 +1,22 @@
-import datetime
-
-
 def solution(video_len, pos, op_start, op_end, commands):
-    vid_start = datetime.datetime.strptime("1900-01-01 00:00:00", '%Y-%m-%d %H:%M:%S')
-    video_len, pose, op_start, op_end = [datetime.datetime.strptime(t, "%M:%S") for t in [video_len, pos, op_start, op_end]]
+    def time_to_sec(txt):
+        h, m = txt.split(":")
+        return int(h) * 60 + int(m)
 
+    def sec_to_time(s):
+        h, m = s // 60, s % 60
+        return f"{h:02d}:{m:02d}"
+
+    vid_start = 0
+    vid_end, pos, op_start, op_end = [time_to_sec(t) for t in [video_len, pos, op_start, op_end]]
     for command in commands:
-        if op_start <= pose <= op_end:
-            pose = op_end
-        pose += datetime.timedelta(seconds=10) * (-1 if command.startswith("p") else 1)
-        if pose <= vid_start:
-            pose = vid_start
-        elif pose >= video_len:
-            pose = video_len
+        if op_start <= pos <= op_end:
+            pos = op_end
+        pos = (pos-10 if pos-10 > vid_start else vid_start) if command.startswith("p") else (pos+10 if pos+10 < vid_end else vid_end)
 
-    if op_start <= pose <= op_end:
-        pose = op_end
-    return pose.strftime("%M:%S")
+    if op_start <= pos <= op_end:
+        pos = op_end
+    return sec_to_time(pos)
 
 
 # print(solution("34:33","13:00","00:55","02:55",["next", "prev"])) #	"13:00"
