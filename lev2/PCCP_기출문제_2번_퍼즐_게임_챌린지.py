6cfff5e9c829a46dfddac95dafb5247c3d4fe96e
dw.lee <dw.lee832@gmail.com>
2025-02-04 21:31:03 +0900

    PCCP 기출문제 2번, 퍼즐 게임 챌린지 문제풀이 03 - 풀이O (21/21)

--- lev2/PCCP_기출문제_2번_퍼즐_게임_챌린지.py
+++ lev2/PCCP_기출문제_2번_퍼즐_게임_챌린지.py
@@ -1,20 +1,19 @@
 def solution(diffs, times, limit):
-    left = 0
+    left = 1
     right = max(diffs)
     ans = []
     while left < right:
-        level = left + (right - left) // 2
+        level = (right + left) // 2
         pre_time = 0
         play_time = 0
         for i, d in enumerate(diffs):
             cur_time = times[i]
             play_time += (cur_time + pre_time) * (d-level) + cur_time if d > level else cur_time
             pre_time = cur_time
-        print(f"level: {level}, play time: {play_time}, left: {left}, right: {right}")
 
         if play_time <= limit:
             ans.append(level)
-            right = level-1
+            right = level
         else:
             if left == level:
                 break
@@ -25,4 +24,7 @@ def solution(diffs, times, limit):
 # print(solution([1, 5, 3], [2, 4, 7], 30)) # 3
 # print(solution([1, 4, 4, 2], [6, 3, 8, 2], 59)) # 2
 # print(solution([1, 328, 467, 209, 54], [2, 7, 1, 4, 3], 1723)) # 294
-print(solution([1, 99999, 100000, 99995], [9999, 9001, 9999, 9001], 3456789012)) # 39354
+# print(solution([1, 99999, 100000, 99995], [9999, 9001, 9999, 9001], 3456789012)) # 39354
+# print(solution([1], [1], 10)) # 1
+# print(solution([10], [1], 1)) # 10
+print(solution([1, 5], [10000, 10000], 20000)) # 5
