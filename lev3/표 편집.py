9f1fb7fe84dd605c0c7afe8f4ca07eee151e5698
dw.lee <dw.lee832@gmail.com>
2025-04-12 21:13:06 +0900

    표 편집 문제풀이 04 - 풀이O (테스트 30/30, 효율성 10/10)

--- lev3/표 편집.py
+++ lev3/표 편집.py
@@ -9,11 +9,19 @@ def solution(n, k, cmd):
         logs[i] = 0
         delete_list.append(i)
 
-    def update_node(i):
-        if i-1 >= 0 and node_data[i][0] is not None:
-            node_data[node_data[i][0]][1] = node_data[i][1]
-        if i+1 < n and node_data[i][1] is not None:
-            node_data[node_data[i][1]][0] = node_data[i][0]
+    def update_node(i, c):
+        if c == "C":
+            if node_data[i][0] is not None:
+                # 노드를 지우고 앞, 뒤를 연결할 때는 제대로 동작
+                # 다만, 지운 노드를 다시 추가할 때 제대로 동작 X
+                node_data[node_data[i][0]][1] = node_data[i][1]
+            if node_data[i][1] is not None:
+                node_data[node_data[i][1]][0] = node_data[i][0]
+        else:
+            if node_data[i][0] is not None:
+                node_data[node_data[i][0]][1] = i
+            if node_data[i][1] is not None:
+                node_data[node_data[i][1]][0] = i
         return node_data[i][1] if node_data[i][1] else node_data[i][0]
 
     logs = [1] * n
@@ -25,18 +33,15 @@ def solution(n, k, cmd):
     move_cnt = 0
     for cmd_line in cmd:
         if len(cmd_line) == 1:
-            idx = move_idx(move_cnt, idx) # 명령어 실행 전 index move
-            print(f"MOVE: {idx}, CMD: {cmd_line}, move cnt: {move_cnt}")
+            idx = move_idx(move_cnt, idx) # 명령어 실행 전 index move & 표의 범위를 벗어난 이동은 입력으로 주어지지 않음
             move_cnt = 0
-            if cmd_line == "C":
-                print(f"cmd: {cmd_line}, idx: {idx}, node: {node_data}")
+            if cmd_line == "C": # 표의 모든 행을 제거하여, 행이 하나도 남지 않는 경우는 입력으로 주어지지 않음
                 del_row(idx)
-                idx = update_node(idx)
-                print(f"cmd: {cmd_line}, idx: {idx}, node: {node_data}\n")
+                idx = update_node(idx, cmd_line)
             else: # delete_list가 null인 경우는 주어지지 않음
                 del_idx = delete_list.pop()
                 logs[del_idx] = 1
-                _ = update_node(del_idx)
+                _ = update_node(del_idx, cmd_line)
                 if idx is None:
                     idx = del_idx
         else:
@@ -50,4 +55,5 @@ def solution(n, k, cmd):
 # print(solution(5, 4, ["U 4", "C", "C", "C", "C", "C", "Z"])) # XXXXO
 # print(solution(11, 0, ["D 10", "C"])) # OOOOOOOOOOX
 # print(solution(5, 2, ["C", "C", "U 1", "C"])) # OXXXO
-print(solution(5, 2, ["C", "C", "C", "C", "C", "Z", "Z", "C"])) # XOXXX
+# print(solution(5, 2, ["C", "C", "C", "C", "C", "Z", "Z", "C"])) # XOXXX
+print(solution(5, 4, ["C", "Z", "U 2", "C", "C", "U 1", "C", "Z", "U 1", "C"])) # XXOOO
