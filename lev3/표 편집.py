4d3e7197b736105e8701f81ca40c79ea17f3b330
dw.lee <dw.lee832@gmail.com>
2025-04-13 21:00:30 +0900

    표 편집 문제풀이 04 - code refactoring

--- lev3/표 편집.py
+++ lev3/표 편집.py
@@ -5,24 +5,22 @@ def solution(n, k, cmd):
             i = node_data[i][step]
         return i
 
+    def update_node(i, c):
+        prev_node, next_node = node_data[i]
+        if prev_node is not None:
+            node_data[prev_node][1] = next_node if c == "C" else i
+        if next_node is not None:
+            node_data[next_node][0] = prev_node if c == "C" else i
+        return next_node if next_node else prev_node
+
     def del_row(i):
         logs[i] = 0
         delete_list.append(i)
+        return update_node(i, "C")
 
-    def update_node(i, c):
-        if c == "C":
-            if node_data[i][0] is not None:
-                # 노드를 지우고 앞, 뒤를 연결할 때는 제대로 동작
-                # 다만, 지운 노드를 다시 추가할 때 제대로 동작 X
-                node_data[node_data[i][0]][1] = node_data[i][1]
-            if node_data[i][1] is not None:
-                node_data[node_data[i][1]][0] = node_data[i][0]
-        else:
-            if node_data[i][0] is not None:
-                node_data[node_data[i][0]][1] = i
-            if node_data[i][1] is not None:
-                node_data[node_data[i][1]][0] = i
-        return node_data[i][1] if node_data[i][1] else node_data[i][0]
+    def restore_row(i):
+        logs[i] = 1
+        update_node(i, "Z")
 
     logs = [1] * n
     idx = k
@@ -36,24 +34,19 @@ def solution(n, k, cmd):
             idx = move_idx(move_cnt, idx) # 명령어 실행 전 index move & 표의 범위를 벗어난 이동은 입력으로 주어지지 않음
             move_cnt = 0
             if cmd_line == "C": # 표의 모든 행을 제거하여, 행이 하나도 남지 않는 경우는 입력으로 주어지지 않음
-                del_row(idx)
-                idx = update_node(idx, cmd_line)
+                idx = del_row(idx)
             else: # delete_list가 null인 경우는 주어지지 않음
-                del_idx = delete_list.pop()
-                logs[del_idx] = 1
-                _ = update_node(del_idx, cmd_line)
-                if idx is None:
-                    idx = del_idx
+                restore_row(delete_list.pop())
         else:
             c, m = cmd_line.split()
             move_cnt += int(m) if c == "D" else int(m)*-1 # 이동 값 누적하여 계산
     return "".join(["O" if l else "X" for l in logs])
 
 
-# print(solution(8, 2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z"])) # OOOOXOOO
+print(solution(8, 2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z"])) # OOOOXOOO
 # print(solution(8, 2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"])) # OOXOXOOO
 # print(solution(5, 4, ["U 4", "C", "C", "C", "C", "C", "Z"])) # XXXXO
 # print(solution(11, 0, ["D 10", "C"])) # OOOOOOOOOOX
 # print(solution(5, 2, ["C", "C", "U 1", "C"])) # OXXXO
 # print(solution(5, 2, ["C", "C", "C", "C", "C", "Z", "Z", "C"])) # XOXXX
-print(solution(5, 4, ["C", "Z", "U 2", "C", "C", "U 1", "C", "Z", "U 1", "C"])) # XXOOO
+# print(solution(5, 4, ["C", "Z", "U 2", "C", "C", "U 1", "C", "Z", "U 1", "C"])) # XXOOO
